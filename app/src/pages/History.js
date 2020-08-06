0f0f1321c8df13659b06be007930f70217663adc
muzakparov <muzakparov@gmail.com>
2020-08-06 03:26:13 -0400

    start History

--- app/src/pages/History.js
+++ app/src/pages/History.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react';
+import { Paper, Container, makeStyles } from '@material-ui/core';
+import { ContractContext } from '../App';
 
-export default function History() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: theme.spacing(2),
+  },
+  paper: {
+    padding: theme.spacing(4),
+  },
+}));
+
+export default function History(props) {
+  const { count } = props;
+  const classes = useStyles();
+  const contract = useContext(ContractContext);
+  const { loanContract, account } = contract;
+
+  useEffect(() => {
+    (async () => {
+      if (loanContract) {
+        try {
+          const loan = await loanContract.methods.getOneBook(account).call();
+          console.log('hhh', loan);
+        } catch (error) {
+          console.log('hhh err')
+        }
+      }
+    })();
+
+    return () => {};
+  }, [loanContract, count]);
+
   return (
-    <div>
-      History
-    </div>
-  )
+    <Container className={classes.root}>
+      <Paper className={classes.paper}>HH</Paper>
+    </Container>
+  );
 }
