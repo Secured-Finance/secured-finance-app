10c8e58fe6dcc715c56707096c790da388e002f8
muzakparov <muzakparov@gmail.com>
2020-08-06 16:36:26 -0400

    add chat and pinata

---
+++ app/src/pages/Pinata.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { SvgIcon } from '@material-ui/core';
+import pina from './pinatacloud.png'
+
+export default function Pinata() {
+  return (
+     <img src={pina} style={{width:24}}/>
+  );
+}
