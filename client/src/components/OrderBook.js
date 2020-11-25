72d5b354e5261fd4cacb24c0d14d8df275496b10
muzakparov <muzakparov@gmail.com>
2020-11-24 23:33:26 -0500

    rm extra content

---
+++ client/src/components/OrderBook.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function OrderBook() {
+  return (
+    <div className='orderbook'>
+      <div className="orderbook__title">Order book</div>
+    </div>
+  )
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './main.css';
+import './styles/main.css';
 import './assets/index.css'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
