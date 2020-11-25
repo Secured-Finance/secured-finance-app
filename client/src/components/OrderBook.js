ba5ae2941b201d084430846a9494e4863763ef1e
muzakparov <muzakparov@gmail.com>
2020-11-25 12:34:07 -0500

    add orderbook lenders

--- client/src/components/OrderBook.js
+++ client/src/components/OrderBook.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import OrderType from './OrderType'
+
+
 
 export default function OrderBook() {
   return (
     <div className='orderbook'>
-      <div className="orderbook__title">Order book</div>
+      <h6 className="orderbook__title">Order book</h6>
+      <div className="orderbook__heads">
+        <div className="orderbook__head">Price</div>
+        <div className="orderbook__head">Amount</div>
+      </div>
+      <OrderType
+      type={'lenders'}
+      orders={[
+        {price:{value:100, label:'eth'},amount:{value:200, label:'fil'}},
+        {price:{value:100, label:'eth'},amount:{value:200, label:'fil'}},
+        {price:{value:100, label:'eth'},amount:{value:200, label:'fil'}},
+        {price:{value:100, label:'eth'},amount:{value:200, label:'fil'}},
+        {price:{value:100, label:'eth'},amount:{value:200, label:'fil'}},
+        {price:{value:100, label:'eth'},amount:{value:200, label:'fil'}},
+        {price:{value:100, label:'eth'},amount:{value:200, label:'fil'}},
+      ]}
+      />
     </div>
   )
 }
