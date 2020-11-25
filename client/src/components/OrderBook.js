ad0d56a0bd338367d2b2c9b3961f8f7c4a173302
muzakparov <muzakparov@gmail.com>
2020-11-25 13:29:55 -0500

    orderbook update

--- client/src/components/OrderBook.js
+++ client/src/components/OrderBook.js
@@ -1,28 +1,29 @@
-import React from 'react'
-import OrderType from './OrderType'
+import React from "react";
+import OrderType from "./OrderType";
+
 
 
 
 export default function OrderBook() {
+  const data = [...Array(6)].map((d, i) => {
+    const amountval=2000 - i * 100
+  
+    return {
+      price: { value: 0.1, label: "eth" },
+      amount: { value:amountval , label: "fil" },
+    };
+  });
+console.log("data", data);
+
   return (
-    <div className='orderbook'>
+    <div className="orderbook">
       <h6 className="orderbook__title">Order book</h6>
       <div className="orderbook__heads">
         <div className="orderbook__head">Price</div>
         <div className="orderbook__head">Amount</div>
       </div>
-      <OrderType
-      type={'lenders'}
-      orders={[
-        {price:{value:100, label:'eth'},amount:{value:200, label:'fil'}},
-        {price:{value:100, label:'eth'},amount:{value:200, label:'fil'}},
-        {price:{value:100, label:'eth'},amount:{value:200, label:'fil'}},
-        {price:{value:100, label:'eth'},amount:{value:200, label:'fil'}},
-        {price:{value:100, label:'eth'},amount:{value:200, label:'fil'}},
-        {price:{value:100, label:'eth'},amount:{value:200, label:'fil'}},
-        {price:{value:100, label:'eth'},amount:{value:200, label:'fil'}},
-      ]}
-      />
+      <OrderType type={"l"} orders={data} />
+      <OrderType type={"b"} orders={data.reverse()} />
     </div>
-  )
+  );
 }
--- client/src/components/OrderType.js
+++ client/src/components/OrderType.js
@@ -1,25 +1,39 @@
-import React from 'react'
+import React from "react";
 
-export default function OrderType({type,orders}) {
-  const rows= orders.map(({price,amount},i)=>{
+export default function OrderType({ type, orders }) {
+  const rows = orders.map(({ price, amount }, i) => {
+    const w = type === "l" ? `${100 - i * 10}%` : `${(i + 1) * 10}%`;
+    const bgColor = type === "l" ? "#3A80AB" : "#E46D53";
+
     return (
-      <div className="order_row" key={i}>         
-        <div className="order_price">{price.value} {price.label}</div>
-        <div className="order_amount">{amount.value} {amount.label}</div>
-        <div className="order_progress" style={{width:`${100-i*10}%`}}></div>
+      <div className="order_row" key={i}>
+        <div className="order_price">
+          {price.value} {price.label}
+        </div>
+        <div className="order_amount">
+          {amount.value} {amount.label}
+        </div>
+        <div
+          className="order_progress"
+          style={{ width: w, backgroundColor: bgColor }}
+        ></div>
       </div>
-    )
-  })
+    );
+  });
+
+  let title;
+
+  if (type.toLowerCase() === "l") {
+    title = <div className="order-type_title order-type_title--l">Lenders</div>;
+  } else {
+    title = <div className="order-type_title order-type_title--b">Borrowers</div>;
+
+  }
 
   return (
     <div className="order-type">
-      <h6 className="order-type_title">{type}</h6>
-      <div className="order_rows">
-      {
-       rows
-      }
-      </div>
-      
+      {title}
+      <div className="order_rows">{rows}</div>
     </div>
-  )
+  );
 }
