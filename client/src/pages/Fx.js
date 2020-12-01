46d02bcc8eafab78527c4c122b9ba811ee814320
muzakparov <muzakparov@gmail.com>
2020-12-01 05:00:06 -0500

    fx lay

--- client/src/pages/Fx.js
+++ client/src/pages/Fx.js
@@ -39,63 +39,19 @@ export default function Fx() {
   const classes = useStyles();
 
   return (
-    <Grid container spacing={2} className={classes.gridItem}>
-      <Grid item xs={8}>
-        <Paper className={classes.paper}>
-          <div id="apex">
-            <ApexChart
-              options={{
-                colors: ["#ff0000", "#ff0000"],
-              }}
-            />
-          </div>
-        </Paper>
-      </Grid>
-      <Grid item xs={4}>
-        <Paper className={classes.paper}>
-          <div>
-            <ReactVirtualizedTable
-              rows={asks}
-              columns={[
-                {
-                  width: 100,
-                  label: "Amount",
-                  dataKey: "size",
-                  flexGrow: 1,
-                },
-                {
-                  width: 120,
-                  label: "Rate %",
-                  dataKey: "rate",
-                  numeric: true,
-                },
-              ]}
-            />
-          </div>
-          <div className={classes.midPrice}>
-            ${Math.round((asks[asks.length - 1].rate + bids[0].rate) / 2)}
-          </div>
-          <div>
-            <ReactVirtualizedTable
-              rows={bids}
-              columns={[
-                {
-                  width: 100,
-                  label: "Amount",
-                  dataKey: "size",
-                  flexGrow: 1,
-                },
-                {
-                  width: 120,
-                  label: "Rate %",
-                  dataKey: "rate",
-                  numeric: true,
-                },
-              ]}
-            />
-          </div>
-        </Paper>
-      </Grid>
-    </Grid>
+    <div className="fx-container">
+      <div className="fx-left">
+        <RateList></RateList>
+      </div>
+      <div className="fx-mid">
+        <div className="fx-graph">
+          <YieldCurve></YieldCurve>
+        </div>
+        <Actions></Actions>
+      </div>
+      <div className="fx-right">
+        <OrderBook></OrderBook>
+      </div>
+    </div>
   );
 }
