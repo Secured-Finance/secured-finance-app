beb5d3512daf9fe258efa73c6ff325b7c94bd63d
muzakparov <muzakparov@gmail.com>
2020-12-01 04:53:48 -0500

    action adjust

--- client/src/components/Action/Action.js
+++ client/src/components/Action/Action.js
@@ -7,34 +7,41 @@ const Type = styled.div`
   text-transform: uppercase;
   font-weight: bold;
   text-align: center;
+  font-size: 14px;
 `;
 
-export default function Action({ children, btn,bgColor }) {
-  let btnBgColor
+const useStyles = makeStyles((theme) => ({
+  textField:{
+    height:10,
+    background:'red'
+  }
+}));
 
-  if(bgColor==="primary"){
-    btnBgColor="blue"
+export default function Action({ children, btn, bgColor }) {
+  const classes=useStyles()
+  let btnBgColor;
+
+  if (bgColor === "primary") {
+    btnBgColor = "blue";
   } else {
-    btnBgColor="red"
-    
+    btnBgColor = "red";
   }
   return (
     <div className="action">
       <Type>{children}</Type>
       <div className="rates">
-        <TextField
-          variant="outlined"
-          size="small"
-          color="primary"
-          label="Rates"
-          fullWidth
+        <TextField variant="outlined" size="small" label="Rates" fullWidth 
+          classes={classes.textField}
+          margin="dense"
         />
       </div>
       <div className="amount">
-        <TextField variant="outlined" size="small" fullWidth label="Amount" />
+        <TextField variant="outlined" size="small" fullWidth label="Amount"  margin="dense"/>
       </div>
       <div>
-        <button className={`bg-${btnBgColor}-600 text-white rounded-sm px-5 py-2 w-full `}>
+        <button
+          className={`bg-${btnBgColor}-600 text-white rounded-sm px-5 py-2 w-full `}
+        >
           {btn}
         </button>
       </div>
