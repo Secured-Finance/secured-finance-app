3ecd8929a0ea6d5089ae751472e0704178ffc06d
Adrian Ciura <aciura@gmail.com>
2021-03-15 20:29:09 +0100

    Display Styled-components names in development mode

--- config-overrides.js
+++ config-overrides.js
@@ -1,25 +1,30 @@
-const path = require('path');
+const path = require('path')
+const rewireStyledComponents = require('react-app-rewire-styled-components')
 
 module.exports = function override(config, env) {
-  const wasmExtensionRegExp = /\.wasm$/;
+  const wasmExtensionRegExp = /\.wasm$/
 
-  config.resolve.extensions.push('.wasm');
+  config.resolve.extensions.push('.wasm')
 
   config.module.rules.forEach(rule => {
-    (rule.oneOf || []).forEach(oneOf => {
+    ;(rule.oneOf || []).forEach(oneOf => {
       if (oneOf.loader && oneOf.loader.indexOf('file-loader') >= 0) {
         // make file-loader ignore WASM files
-        oneOf.exclude.push(wasmExtensionRegExp);
+        oneOf.exclude.push(wasmExtensionRegExp)
       }
-    });
-  });
+    })
+  })
 
   // add a dedicated loader for WASM
   config.module.rules.push({
     test: wasmExtensionRegExp,
     include: path.resolve(__dirname, 'src'),
-    use: [{ loader: require.resolve('wasm-loader'), options: {} }]
-  });
+    use: [{ loader: require.resolve('wasm-loader'), options: {} }],
+  })
 
-  return config;
+  // For more config options check:
+  // https://github.com/withspectrum/react-app-rewire-styled-components
+  config = rewireStyledComponents(config, env)
+
+  return config
 }
