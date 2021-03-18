26db4b7bc6e42cac9730f5e9efe2d2e464714a0b
Alla Panchenko <evetrueivory@gmail.com>
2021-03-18 04:10:03 +0300

    created common button

--- src/views/Exchange/components/Balances/index.tsx
+++ src/views/Exchange/components/Balances/index.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 import styled from "styled-components";
 import { useWallet } from "use-wallet";
-import Button from "../../../../components/Button";
 import CollateralModal from "../../../../components/CollateralModal";
 import useCollateralBook from "../../../../hooks/useCollateralBook";
 import { useEthereumWalletStore } from "../../../../hooks/useEthWallet";
@@ -12,6 +11,7 @@ import theme from "../../../../theme";
 import { ordinaryFormat, usdFormat } from "../../../../utils";
 import { Subheader } from "../../../../components/common/Subheader"
 import { Table, Cell, CellKey, CellValue } from "../../../../components/common/Table";
+import { Button } from "../../../../components/common/Buttons";
 
 export const Balances: React.FC = () => {
     const totalUSDBalance = useTotalUSDBalance()
@@ -53,18 +53,7 @@ export const Balances: React.FC = () => {
                     <CellValue>10 000 FIL</CellValue>
                 </Cell>
             </Table>
-            <Button
-                onClick={onPresentCollateralModal}
-                text={"Manage Collateral"}
-                style={{
-                    background: 'transparent',
-                    fontSize: theme.sizes.subhead,
-                    fontWeight: 500,
-                    color: theme.colors.primaryBlue,
-                    borderColor: theme.colors.primaryBlue,
-                    borderRadius: 4,
-                }}
-            />
+            <Button onClick={onPresentCollateralModal} outline>Manage Collateral</Button>
         </BalanceContainer>
     );
 }
