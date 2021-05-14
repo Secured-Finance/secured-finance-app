6cd90747835e521a72963f82cf9350f3f5382c40
Alla Panchenko <evetrueivory@gmail.com>
2021-05-14 21:20:01 +0300

    eliminated compile errors

--- src/services/ledger/connectLedger.ts
+++ src/services/ledger/connectLedger.ts
@@ -1,17 +1,21 @@
-import { clearError, resetLedgerState } from './state'
+import { clearError, resetLedgerState } from './state';
 import {
     checkLedgerConfiguration,
     setLedgerProvider,
-} from './setLedgerProvider'
+} from './setLedgerProvider';
+import { Dispatch } from 'react';
 
-const connectWithLedger = async (dispatch, LedgerProvider) => {
-    dispatch(clearError())
-    dispatch(resetLedgerState())
-    const provider = await setLedgerProvider(dispatch, LedgerProvider)
-    if (!provider) return null
-    const configured = await checkLedgerConfiguration(dispatch, provider)
-    if (!configured) return null
-    return provider
-}
+const connectWithLedger = async (
+    dispatch: Dispatch<{ type: string }>,
+    LedgerProvider: any
+) => {
+    dispatch(clearError());
+    dispatch(resetLedgerState());
+    const provider = await setLedgerProvider(dispatch, LedgerProvider);
+    if (!provider) return null;
+    const configured = await checkLedgerConfiguration(dispatch, provider);
+    if (!configured) return null;
+    return provider;
+};
 
-export default connectWithLedger
+export default connectWithLedger;
--- src/services/ledger/ledgerStateManagement.ts
+++ src/services/ledger/ledgerStateManagement.ts
@@ -1,21 +1,35 @@
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 /* VALID ACTION TYPES */
-export const LEDGER_USER_INITIATED_IMPORT = 'USER_INITIATED_IMPORT'
-export const LEDGER_NOT_FOUND = 'LEDGER_NOT_FOUND'
-export const LEDGER_RESET_STATE = 'LEDGER_RESET_STATE'
-export const LEDGER_CONNECTED = 'LEDGER_CONNECTED'
+export const LEDGER_USER_INITIATED_IMPORT = 'USER_INITIATED_IMPORT';
+export const LEDGER_NOT_FOUND = 'LEDGER_NOT_FOUND';
+export const LEDGER_RESET_STATE = 'LEDGER_RESET_STATE';
+export const LEDGER_CONNECTED = 'LEDGER_CONNECTED';
 export const LEDGER_ESTABLISHING_CONNECTION_W_FILECOIN_APP =
-    'ESTABLISHING_CONNECTION_W_FILECOIN_APP'
-export const LEDGER_FILECOIN_APP_NOT_OPEN = 'FILECOIN_APP_NOT_OPEN'
-export const LEDGER_FILECOIN_APP_OPEN = 'FILECOIN_APP_OPEN'
-export const LEDGER_LOCKED = 'LEDGER_LOCKED'
-export const LEDGER_UNLOCKED = 'LEDGER_UNLOCKED'
-export const LEDGER_REPLUG = 'LEDGER_REPLUG'
-export const LEDGER_BUSY = 'LEDGER_BUSY'
-export const LEDGER_USED_BY_ANOTHER_APP = 'LEDGER_USED_BY_ANOTHER_APP'
-export const LEDGER_BAD_VERSION = 'LEDGER_BAD_VERSION'
-export const WEBUSB_UNSUPPORTED = 'WEBUSB_UNSUPPORTED'
+    'ESTABLISHING_CONNECTION_W_FILECOIN_APP';
+export const LEDGER_FILECOIN_APP_NOT_OPEN = 'FILECOIN_APP_NOT_OPEN';
+export const LEDGER_FILECOIN_APP_OPEN = 'FILECOIN_APP_OPEN';
+export const LEDGER_LOCKED = 'LEDGER_LOCKED';
+export const LEDGER_UNLOCKED = 'LEDGER_UNLOCKED';
+export const LEDGER_REPLUG = 'LEDGER_REPLUG';
+export const LEDGER_BUSY = 'LEDGER_BUSY';
+export const LEDGER_USED_BY_ANOTHER_APP = 'LEDGER_USED_BY_ANOTHER_APP';
+export const LEDGER_BAD_VERSION = 'LEDGER_BAD_VERSION';
+export const WEBUSB_UNSUPPORTED = 'WEBUSB_UNSUPPORTED';
+
+interface ILedger {
+    userImportFailure: boolean;
+    connecting: boolean;
+    connectedFailure: boolean;
+    locked: boolean;
+    unlocked: boolean;
+    busy: boolean;
+    filecoinAppNotOpen: boolean;
+    replug: boolean;
+    inUseByAnotherApp: boolean;
+    badVersion: boolean;
+    webUSBSupported: boolean;
+}
 
 export const initialLedgerState = {
     userImportFailure: false,
@@ -30,7 +44,7 @@ export const initialLedgerState = {
     badVersion: false,
     // true until proven otherwise
     webUSBSupported: true,
-}
+};
 
 export const LEDGER_STATE_PROPTYPES = {
     userImportFailure: PropTypes.bool.isRequired,
@@ -42,4 +56,4 @@ export const LEDGER_STATE_PROPTYPES = {
     filecoinAppNotOpen: PropTypes.bool.isRequired,
     badVersion: PropTypes.bool.isRequired,
     webUSBSupported: PropTypes.bool.isRequired,
-}
+};
