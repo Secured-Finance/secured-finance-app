1742a7e69e08a7af8e532ed97bb0c504bef43d9b
Alla Panchenko <evetrueivory@gmail.com>
2021-05-25 20:56:36 +0300

    fixed Eth wallet sign out from store and localStorage

--- src/components/WalletAccountModal/components/EthWallet.tsx
+++ src/components/WalletAccountModal/components/EthWallet.tsx
@@ -1,91 +1,89 @@
-import React, { useCallback } from 'react'
-import styled from 'styled-components'
-import { useWallet } from 'use-wallet'
-import theme from '../../../theme'
-import Button from '../../Button'
-import Label from '../../Label'
-import { ModalProps } from '../../Modal'
-import Spacer from '../../Spacer'
+import React, { useCallback } from 'react';
+import styled from 'styled-components';
+import { useWallet } from 'use-wallet';
+import theme from '../../../theme';
+import Button from '../../Button';
+import Label from '../../Label';
+import { ModalProps } from '../../Modal';
+import Spacer from '../../Spacer';
+import { CACHED_PROVIDER_KEY } from '../../../contexts/FilecoinWalletProvider';
 
-const EthWallet: React.FC<ModalProps> = ({ onDismiss }) => {
-	const { account, reset } = useWallet()
+const EthWallet: React.FC<ModalProps> = ({ onDismiss, ...props }) => {
+    const { account, reset } = useWallet();
 
-	const handleSignOutClick = useCallback(() => {
-        onDismiss!()
-		reset()
-	}, [onDismiss, reset])
+    const handleSignOutClick = useCallback(() => {
+        reset();
+        localStorage.removeItem(CACHED_PROVIDER_KEY);
+        onDismiss();
+    }, [onDismiss, reset]);
 
     return (
-        <StyledWalletContainer>
+        <div>
             <div style={{ display: 'flex' }}>
-            <StyledAccountContainer>
-                <StyledAccount>
+                <StyledAccountContainer>
+                    <StyledAccount>
                         <StyledAccountText>{account}</StyledAccountText>
-                        <Label 
-                            style={{marginTop: 5}}
-                            text="Ethereum address" 
+                        <Label
+                            style={{ marginTop: 5 }}
+                            text='Ethereum address'
                         />
-                </StyledAccount>
-            </StyledAccountContainer>
+                    </StyledAccount>
+                </StyledAccountContainer>
+            </div>
+            <StyledButtonContainer>
+                <Button
+                    href={`https://etherscan.io/address/${account}`}
+                    text='View on Etherscan'
+                    style={{
+                        background: theme.colors.buttonBlue,
+                        fontSize: theme.sizes.callout,
+                        fontWeight: 500,
+                        color: theme.colors.white,
+                    }}
+                />
+                <Spacer />
+                <Button
+                    onClick={handleSignOutClick}
+                    text='Sign out'
+                    style={{
+                        background: theme.colors.buttonBlue,
+                        fontSize: theme.sizes.callout,
+                        fontWeight: 500,
+                        color: theme.colors.white,
+                    }}
+                />
+            </StyledButtonContainer>
         </div>
-        <StyledButtonContainer>
-            <Button
-                href={`https://etherscan.io/address/${account}`}
-                text="View on Etherscan"
-                style={{
-                    background: theme.colors.buttonBlue,
-                    fontSize: theme.sizes.callout, 
-                    fontWeight: 500,	
-                    color: theme.colors.white
-                }}
-            />
-            <Spacer />
-            <Button
-                onClick={handleSignOutClick}
-                text="Sign out"
-                style={{
-                    background: theme.colors.buttonBlue,
-                    fontSize: theme.sizes.callout, 
-                    fontWeight: 500,	
-                    color: theme.colors.white
-                }}
-            />
-        </StyledButtonContainer>
-    </StyledWalletContainer>
-    )
-}
-
-const StyledWalletContainer = styled.div`
-
-`
+    );
+};
 
 const StyledAccount = styled.div`
-	align-items: center;
-	display: flex;
-	flex-direction: column;
-`
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+`;
 
 const StyledAccountContainer = styled.div`
-	align-items: center;
-	display: flex;
-	flex: 1;
-	flex-direction: column;
-	margin-bottom: ${(props) => props.theme.spacing[4]}px;
-`
+    align-items: center;
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+    margin-bottom: ${props => props.theme.spacing[4]}px;
+`;
 
 const StyledAccountText = styled.p`
-	margin: 0;
-	color: ${props => props.theme.colors.lightText};
-	font-size: ${props => props.theme.sizes.callout};
-`
+    margin: 0;
+    color: ${props => props.theme.colors.lightText};
+    font-size: ${props => props.theme.sizes.callout};
+`;
 
 const StyledButtonContainer = styled.div`
-	margin: 0;
-	padding: 0;
-	display: flex;
-	flex-direction: row;
-	justify-content: space-between;
-	align-items: center;
-`
+    margin: 0;
+    padding: 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`;
 
-export default EthWallet
+export default EthWallet;
--- src/hooks/useEthWallet.ts
+++ src/hooks/useEthWallet.ts
@@ -1,74 +1,131 @@
-import BigNumber from 'bignumber.js'
-import { useCallback, useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { useWallet } from 'use-wallet'
-import WalletAccountModal from '../components/WalletAccountModal'
-import { RootState } from '../store/types'
-import { updateEthWalletActions, updateEthWalletAddress, updateEthWalletAssetPrice, updateEthWalletBalance, updateEthWalletDailyChange, updateEthWalletPortfolioShare, updateEthWalletUSDBalance } from '../store/wallets'
-import { useEthereumUsd } from './useAssetPrices'
+import BigNumber from 'bignumber.js';
+import { useCallback, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useWallet } from 'use-wallet';
+import WalletAccountModal from '../components/WalletAccountModal';
+import { RootState } from '../store/types';
+import {
+    updateEthWalletActions,
+    updateEthWalletAddress,
+    updateEthWalletAssetPrice,
+    updateEthWalletBalance,
+    updateEthWalletDailyChange,
+    updateEthWalletPortfolioShare,
+    updateEthWalletUSDBalance,
+    resetEthWallet,
+} from '../store/wallets';
+import { useEthereumUsd } from './useAssetPrices';
 
-import useBlock from './useBlock'
-import useModal from './useModal'
+import useBlock from './useBlock';
+import useModal from './useModal';
 
 export const useEthereumWalletStore = () => {
-    const ethWallet = useSelector((state: RootState) => state.wallets.ethereum);
-    const block = useBlock()
-    const dispatch = useDispatch()
-    const { account, balance, reset } = useWallet()
-    const { price, change } = useEthereumUsd()
-    const totalUSDBalance = useSelector((state: RootState) => state.wallets.totalUSDBalance);
-    const [onPresentAccountModal] = useModal(WalletAccountModal)
+    const ethWallet = useSelector((state: RootState) => {
+        return state.wallets.ethereum;
+    });
+    const block = useBlock();
+    const dispatch = useDispatch();
+    const { account, balance, reset } = useWallet();
+    const { price, change } = useEthereumUsd();
+    const totalUSDBalance = useSelector(
+        (state: RootState) => state.wallets.totalUSDBalance
+    );
+    const [onPresentAccountModal] = useModal(WalletAccountModal);
 
     const actObj = {
         send: onPresentAccountModal,
         placeCollateral: onPresentAccountModal,
         signOut: reset,
-    }
-    
-    const fetchEthStore = useCallback(async (isMounted: boolean) => {
-        const inEther = new BigNumber(balance).dividedBy(new BigNumber(10).pow(18)).toNumber()
-        const usdBalance = new BigNumber(inEther).times(new BigNumber(price)).toNumber()
-        const portfolioShare = new BigNumber(usdBalance).times(100).dividedBy(new BigNumber(totalUSDBalance)).toNumber()
-    
-        dispatch(updateEthWalletAddress(account))
-        dispatch(updateEthWalletBalance(inEther))
-        dispatch(updateEthWalletAssetPrice(price))
-        dispatch(updateEthWalletDailyChange(change))
-        dispatch(updateEthWalletUSDBalance(usdBalance))
-        if (portfolioShare != (null || Infinity)) {
-            dispatch(updateEthWalletPortfolioShare(portfolioShare))
-        }
-        dispatch(updateEthWalletActions(actObj))
-	}, [dispatch, account, balance, reset, totalUSDBalance, price, change])
-    
-	useEffect(() => {
-        let isMounted = true;
-        if ( account && balance && reset && totalUSDBalance != 0 && price != 0 && change != 0) {
-            fetchEthStore(isMounted)
-        }
-        return () => { isMounted = false };
-    }, [block, dispatch, account, balance, reset, totalUSDBalance, price, change])
+    };
 
-    return ethWallet
-}
+    const fetchEthStore = useCallback(
+        async (isMounted: boolean) => {
+            const inEther = new BigNumber(balance)
+                .dividedBy(new BigNumber(10).pow(18))
+                .toNumber();
+            const usdBalance = new BigNumber(inEther)
+                .times(new BigNumber(price))
+                .toNumber();
+            const portfolioShare = new BigNumber(usdBalance)
+                .times(100)
+                .dividedBy(new BigNumber(totalUSDBalance))
+                .toNumber();
+
+            dispatch(updateEthWalletAddress(account));
+            dispatch(updateEthWalletBalance(inEther));
+            dispatch(updateEthWalletAssetPrice(price));
+            dispatch(updateEthWalletDailyChange(change));
+            dispatch(updateEthWalletUSDBalance(usdBalance));
+            if (portfolioShare != (null || Infinity)) {
+                dispatch(updateEthWalletPortfolioShare(portfolioShare));
+            }
+            dispatch(updateEthWalletActions(actObj));
+        },
+        [dispatch, account, balance, reset, totalUSDBalance, price, change]
+    );
+
+    useEffect(() => {
+        let isMounted = true;
+        if (
+            account &&
+            balance &&
+            reset &&
+            totalUSDBalance != 0 &&
+            price != 0 &&
+            change != 0
+        ) {
+            fetchEthStore(isMounted);
+        }
+
+        return () => {
+            isMounted = false;
+        };
+    }, [
+        block,
+        dispatch,
+        account,
+        balance,
+        reset,
+        totalUSDBalance,
+        price,
+        change,
+    ]);
+
+    useEffect(() => {
+        if (account === null) {
+            dispatch(resetEthWallet());
+        }
+    }, [account]);
+
+    return ethWallet;
+};
 
 export const useEthBalance = () => {
-    const { account, balance } = useWallet()
-    const dispatch = useDispatch()
-    const ethBalance = useSelector((state: RootState) => state.wallets.ethereum.balance);
+    const { account, balance } = useWallet();
+    const dispatch = useDispatch();
+    const ethBalance = useSelector(
+        (state: RootState) => state.wallets.ethereum.balance
+    );
 
-    const fetchEthStore = useCallback(async (isMounted: boolean) => {
-        const inEther = new BigNumber(balance).dividedBy(new BigNumber(10).pow(18)).toNumber()
-        dispatch(updateEthWalletBalance(inEther))
-	}, [dispatch, account, balance, ])
-    
-	useEffect(() => {
+    const fetchEthStore = useCallback(
+        async (isMounted: boolean) => {
+            const inEther = new BigNumber(balance)
+                .dividedBy(new BigNumber(10).pow(18))
+                .toNumber();
+            dispatch(updateEthWalletBalance(inEther));
+        },
+        [dispatch, account, balance]
+    );
+
+    useEffect(() => {
         let isMounted = true;
-        if ( account && balance) {
-            fetchEthStore(isMounted)
+        if (account && balance) {
+            fetchEthStore(isMounted);
         }
-        return () => { isMounted = false };
-    }, [dispatch, account, balance])
+        return () => {
+            isMounted = false;
+        };
+    }, [dispatch, account, balance]);
 
-    return ethBalance
-}
+    return ethBalance;
+};
