5046161b6f4692b53ecd99456e48e94ca968eb15
Alla Panchenko <evetrueivory@gmail.com>
2021-06-18 16:52:37 +0300

    inited Storybook with 4 button stories

--- src/components/new/Button/index.tsx
+++ src/components/new/Button/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import cm from './Button.module.scss';
 import cx from 'classnames';
 
-interface IButton extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+export interface IButton extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     outline?: boolean;
 }
 
--- src/components/new/TextButton/index.tsx
+++ src/components/new/TextButton/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import cm from './TextButton.module.scss';
-interface ITextButton extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+export interface ITextButton
+    extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
 export const TextButton: React.FC<ITextButton> = ({ children, ...props }) => {
     return (
         <button className={cm.textButton} {...props}>
-            {children}
+            <span>{children}</span>
             <span className={cm.icon} />
         </button>
     );
