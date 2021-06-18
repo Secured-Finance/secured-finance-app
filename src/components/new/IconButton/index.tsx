5624429dc78d6b944e0ebe2f9bbf0ac8d3dda82b
Alla Panchenko <evetrueivory@gmail.com>
2021-06-18 19:21:06 +0300

    added icon button + icons

---
+++ src/components/new/IconButton/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import cx from 'classnames';
+import cm from './IconButton.module.scss';
+import arrow from '../icons/arrow.svg';
+
+export interface IIconButton
+    extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    withBackground?: boolean;
+    size?: number;
+    icon?: string;
+}
+
+export const IconButton: React.FC<IIconButton> = ({
+    withBackground,
+    size = 18,
+    icon = arrow,
+    children,
+    ...props
+}) => {
+    return (
+        <button
+            className={cx(cm.iconButton, {
+                [cm.withBackground]: withBackground,
+            })}
+            {...props}
+        >
+            <img
+                src={icon}
+                width={size}
+                className={withBackground && cm.iconWithBackground}
+            />
+        </button>
+    );
+};
