04e7a92855cf2546133ceaab547051421022e58c
Alla Panchenko <evetrueivory@gmail.com>
2021-06-18 19:45:44 +0300

    handy icon exports

---
+++ src/components/new/icons/index.ts
@@ -0,0 +1,37 @@
+import arrow from './arrow.svg';
+import chevron from './chevron.svg';
+import close from './close.svg';
+import darkMode from './dark-mode.svg';
+import discord from './discord.svg';
+import ethereum from './ethereum.svg';
+import filecoin from './filecoin.svg';
+import filter from './filter.svg';
+import github from './github.svg';
+import lightMode from './light-mode.svg';
+import like from './like.svg';
+import medium from './medium.svg';
+import telegram from './telegram.svg';
+import twitter from './twitter.svg';
+import USD from './USD.svg';
+import verified from './verified.svg';
+import wallet from './wallet.svg';
+
+export {
+    arrow,
+    chevron,
+    close,
+    darkMode,
+    discord,
+    ethereum,
+    filecoin,
+    filter,
+    github,
+    lightMode,
+    like,
+    medium,
+    telegram,
+    twitter,
+    USD,
+    verified,
+    wallet,
+};
