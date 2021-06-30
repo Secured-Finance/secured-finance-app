93d3908de457d00f35ea54913b05ae8dacd9a77c
Alla Panchenko <evetrueivory@gmail.com>
2021-06-30 16:34:58 +0300

    gradient magic for chart axis

--- src/components/new/LineChart/constants.ts
+++ src/components/new/LineChart/constants.ts
@@ -1,5 +1,3 @@
-import cm from './LineChart.module.scss';
-
 const canvas: HTMLCanvasElement = document.createElement('canvas');
 let ctx: CanvasRenderingContext2D = canvas.getContext('2d');
 
@@ -14,6 +12,19 @@ greenGradient.addColorStop(1, 'rgba(15, 26, 34, 0.1)');
 const redGradient = ctx.createLinearGradient(0, 0, 0, 290);
 redGradient.addColorStop(0, 'rgba(222, 95, 66, 1)');
 redGradient.addColorStop(1, 'rgba(15, 26, 34, 0.1)');
+
+const yAxisGradient = ctx.createLinearGradient(0, 100, 600, 100);
+yAxisGradient.addColorStop(0, 'rgba(53, 54, 61, 0.3)');
+yAxisGradient.addColorStop(0.9, 'rgba(53, 54, 61, 0.1)');
+yAxisGradient.addColorStop(1, 'rgba(53, 54, 61, 0)');
+
+const xAxisGradient = ctx.createLinearGradient(0, 100, 600, 100);
+xAxisGradient.addColorStop(0, 'rgba(53, 54, 52, 0.3)');
+xAxisGradient.addColorStop(0.05, 'rgba(53, 54, 61, 0)');
+
+const xAxisZeroLineGradient = ctx.createLinearGradient(300, 200, 300, 0);
+xAxisZeroLineGradient.addColorStop(0.6, 'rgba(53, 54, 61, 0.3)');
+xAxisZeroLineGradient.addColorStop(1, 'rgba(53, 54, 61, 0)');
 
 export const commonDataset = {
     fill: true,
@@ -43,18 +54,32 @@ export const defaultDatasets = [
     },
 ];
 
+const getYAxisLinesColor = () => {
+    // hack for hiding the last yAxis line for charts of 20 ticks max
+    const colors: Array<string | CanvasGradient> = ['transparent'];
+    for (let i = 0; i < 20; i++) {
+        colors.push(yAxisGradient);
+    }
+
+    return colors;
+};
+
 export const options = {
     responsive: true,
     scales: {
         yAxes: [
             {
+                color: yAxisGradient,
                 ticks: {
                     color: '#5f616a',
                     fontSize: 13,
+                    padding: 8,
                 },
                 gridLines: {
-                    color: '#5f616a50',
+                    color: getYAxisLinesColor(),
                     drawBorder: false,
+                    zeroLineColor: yAxisGradient,
+                    drawTicks: false,
                 },
             },
         ],
@@ -63,10 +88,13 @@ export const options = {
                 ticks: {
                     color: '#5f616a',
                     fontSize: 13,
+                    padding: 8,
                 },
                 gridLines: {
-                    color: '#5f616a50',
+                    color: xAxisGradient, // hides xAxis grid except for zero line
                     drawBorder: false,
+                    zeroLineColor: xAxisZeroLineGradient,
+                    drawTicks: false,
                 },
             },
         ],
