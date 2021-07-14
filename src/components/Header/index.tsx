34dec1b786e09d3af12541e99077370d23062c65
Alla Panchenko <evetrueivory@gmail.com>
2021-07-14 20:19:45 +0300

    header width common with page width

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,42 +6,44 @@ import { WalletButton } from './WalletButton';
 
 export const Header: React.FC = () => {
     return (
-        <div className={cm.header}>
-            <NavLink className={cm.logo} to={'/'}>
-                <Logo fill={'#666cf3'} size={40} />
-                <span className={cm.logoText}>
-                    <span className={cm.logoTextBold}>Secured</span>&ensp;
-                    <span>Finance</span>
-                </span>
-            </NavLink>
-            <div className={cm.navigation}>
-                <NavLink
-                    exact
-                    className={cm.navLink}
-                    activeClassName={cm.active}
-                    to='/'
-                >
-                    Lending
+        <div className={cm.container}>
+            <div className={cm.header}>
+                <NavLink className={cm.logo} to={'/'}>
+                    <Logo fill={'#666cf3'} size={40} />
+                    <span className={cm.logoText}>
+                        <span className={cm.logoTextBold}>Secured</span>&ensp;
+                        <span>Finance</span>
+                    </span>
                 </NavLink>
-                <NavLink
-                    exact
-                    className={cm.navLink}
-                    activeClassName={cm.active}
-                    to='/exchange'
-                >
-                    Terminal
-                </NavLink>
-                <NavLink
-                    exact
-                    className={cm.navLink}
-                    activeClassName={cm.active}
-                    to='/history'
-                >
-                    History
-                </NavLink>
-                <WalletButton />
+                <div className={cm.navigation}>
+                    <NavLink
+                        exact
+                        className={cm.navLink}
+                        activeClassName={cm.active}
+                        to='/'
+                    >
+                        Lending
+                    </NavLink>
+                    <NavLink
+                        exact
+                        className={cm.navLink}
+                        activeClassName={cm.active}
+                        to='/exchange'
+                    >
+                        Terminal
+                    </NavLink>
+                    <NavLink
+                        exact
+                        className={cm.navLink}
+                        activeClassName={cm.active}
+                        to='/history'
+                    >
+                        History
+                    </NavLink>
+                    <WalletButton />
+                </div>
+                <div className={cm.themeSwitch} />
             </div>
-            <div className={cm.themeSwitch} />
         </div>
     );
 };
