c67032f66c5665a40fb79fbe20bce80ec9fbb27c
reuvab <reuven.aboulker@gmail.com>
2022-03-10 09:04:33 +0100

    [SF-5] Start migration of Lending page to Tailwind

--- src/views/Lending/index.tsx
+++ src/views/Lending/index.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Page } from 'src/components/new/Page';
 import { useRates } from 'src/hooks/useRates';
 import useSF from 'src/hooks/useSecuredFinance';
 import { getLendingControllerContract } from 'src/services/sdk/utils';
 import { LendingStore } from 'src/store/lending';
 import { RootState } from 'src/store/types';
-import YieldGraph from './components/Graph';
-import cm from './index.module.scss';
 import { ChartInfo } from './components/ChartInfo';
-import { Page } from 'src/components/new/Page';
+import YieldGraph from './components/Graph';
 import { PlaceOrder } from './components/PlaceOrder';
 // import PlaceOrderObsolete from './components/PlaceOrderObsolete';
 
-const Lending: React.FC<LendingStore> = ({ selectedCcy }) => {
+const Lending = ({ selectedCcy }: LendingStore) => {
     const securedFinance = useSF();
     const lendingController = getLendingControllerContract(securedFinance);
     const borrowRates = useRates(lendingController, 0, selectedCcy);
@@ -21,8 +20,8 @@ const Lending: React.FC<LendingStore> = ({ selectedCcy }) => {
 
     return (
         <Page>
-            <div className={cm.container}>
-                <div className={cm.chartAndInfo}>
+            <div className='flex flex-grow justify-between space-x-4 pt-8'>
+                <div className='flex flex-col'>
                     <ChartInfo />
                     <YieldGraph
                         borrowRates={borrowRates}
