3455d349322942aab76d591fce80c92a5ed7a5a5
reuvab <reuven.aboulker@gmail.com>
2022-04-08 18:02:01 +0200

    [SF-78] Add cypress for end to end testing

--- src/components/new/Page/index.tsx
+++ src/components/new/Page/index.tsx
@@ -1,11 +1,15 @@
-import cm from './Page.module.scss';
 import React, { HTMLAttributes } from 'react';
+import cm from './Page.module.scss';
 
 type IPage = HTMLAttributes<HTMLDivElement>;
 
-export const Page: React.FC<IPage> = ({ style, children }) => {
+export const Page: React.FC<IPage & { id: string }> = ({
+    style,
+    children,
+    id,
+}) => {
     return (
-        <div className={cm.page} style={style}>
+        <div id={id} data-cy={id} className={cm.page} style={style}>
             {children}
         </div>
     );
