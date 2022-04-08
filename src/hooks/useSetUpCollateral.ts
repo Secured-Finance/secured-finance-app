357e3fb252ffbdfdea08148e07bf3b8937bcc4de
akihiro.tanaka <big.a.0816@gmail.com>
2022-04-08 17:05:38 +0900

    [SF-72] Add .nvmrc & Update node version

--- src/hooks/useSetUpCollateral.ts
+++ src/hooks/useSetUpCollateral.ts
@@ -1,7 +1,6 @@
 import { useCallback } from 'react';
 import { toBytes32 } from 'src/utils/strings';
 import { useWallet } from 'use-wallet';
-import { provider } from 'web3-core';
 import { getCollateralContract, setUpCollateral } from '../services/sdk/utils';
 import useSF from './useSecuredFinance';
 
@@ -11,7 +10,7 @@ export const useSetUpCollateral = (
     filAddr: string
 ) => {
     const securedFinance = useSF();
-    const { account }: { account: string; ethereum: provider } = useWallet();
+    const { account } = useWallet();
     const collateralContract = getCollateralContract(securedFinance);
 
     const handleSetUpCollateral = useCallback(async () => {
