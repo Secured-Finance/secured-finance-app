9b15ad6a31f2afb49bd0c9ed1155a0d399d0077c
reuvab <reuven.aboulker@gmail.com>
2022-04-20 18:53:09 +0800

    [SF-87] Fix warning in filecoin store

--- src/services/filecoin/store/actions.ts
+++ src/services/filecoin/store/actions.ts
@@ -1,33 +1,34 @@
 import Filecoin from '@glif/filecoin-wallet-provider';
 import * as constants from './constants';
+import { FilWalletProviderAction } from './types';
 
-export function startFetchingFilWalletProvider() {
+export function startFetchingFilWalletProvider(): FilWalletProviderAction {
     return {
         type: constants.FETCHING_FILECOIN_WALLET_PROVIDER,
     };
 }
 
-export function failFetchingFilWalletProvider() {
+export function failFetchingFilWalletProvider(): FilWalletProviderAction {
     return {
         type: constants.FETCHING_FILECOIN_WALLET_PROVIDER_FAILURE,
     };
 }
 
-export function setFilWalletType(data: string) {
+export function setFilWalletType(data: string): FilWalletProviderAction {
     return {
         type: constants.UPDATE_WALLET_TYPE,
         data,
     };
 }
 
-export function setFilWalletProvider(data: Filecoin) {
+export function setFilWalletProvider(data: Filecoin): FilWalletProviderAction {
     return {
         type: constants.UPDATE_WALLET_PROVIDER,
         data,
     };
 }
 
-export function resetFilWalletProvider() {
+export function resetFilWalletProvider(): FilWalletProviderAction {
     return {
         type: constants.RESET_WALLET_PROVIDER,
     };
--- src/services/filecoin/store/types.ts
+++ src/services/filecoin/store/types.ts
@@ -1,7 +1,13 @@
 import Filecoin from '@glif/filecoin-wallet-provider';
+import { FileCoinActionType } from './constants';
 
 export interface FilWalletProvider {
     walletType: string;
     walletProvider: Filecoin;
     isLoading: boolean;
 }
+
+export type FilWalletProviderAction = {
+    type: FileCoinActionType;
+    data?: unknown;
+};
