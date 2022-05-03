78becc4746306e80fe19b5b0fb647e0067d6e426
reuvab <reuven.aboulker@gmail.com>
2022-05-03 12:56:29 +0800

    [SF-87] Fix filscan api url

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -15,9 +15,3 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands';
-
-Cypress.on('uncaught:exception', (err, runnable) => {
-    if (err.message.includes('Network Error')) {
-        return false;
-    }
-});
