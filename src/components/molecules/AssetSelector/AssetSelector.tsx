ba93e21a76ccad71ac7e4f44d99f84f4e605ac31
Reuven Aboulker <reuven.aboulker@gmail.com>
2022-06-20 19:40:00 +0800

    feat(design): add focus ring for selectors
    SF-156

--- src/components/molecules/AssetSelector/AssetSelector.tsx
+++ src/components/molecules/AssetSelector/AssetSelector.tsx
@@ -23,47 +23,45 @@ export const AssetSelector = ({
     }, [asset, amount, priceList]);
 
     return (
-        <div>
-            <div className='w-72 flex-col items-start justify-start space-y-2'>
-                <div className='flex flex-row items-start justify-between'>
-                    <label className='typography-caption ml-2 text-planetaryPurple'>
-                        Asset
-                    </label>
-                    <div
-                        className='typography-caption mr-1 text-white-60'
-                        data-testid='asset-selector-usd'
-                    >
-                        {`${amountInUsd} USD`}
-                    </div>
+        <div className='w-72 flex-col items-start justify-start space-y-2'>
+            <div className='flex flex-row items-start justify-between'>
+                <label className='typography-caption ml-2 text-planetaryPurple'>
+                    Asset
+                </label>
+                <div
+                    className='typography-caption mr-1 text-white-60'
+                    data-testid='asset-selector-usd'
+                >
+                    {`${amountInUsd} USD`}
                 </div>
-                <div className='flex h-14 w-72 flex-row items-center justify-between space-x-2 rounded-lg bg-black-20 py-2 pl-2 pr-4 focus:border-2 focus:border-horizonBlue'>
-                    <div>
-                        <DropdownSelector
-                            optionList={options}
-                            onChange={setAsset}
-                        />
-                    </div>
-                    <div>
-                        <input
-                            type='text'
-                            placeholder='0'
-                            className='typography-body-1 h-8 w-24 rounded-lg bg-transparent p-2 text-right font-bold text-white placeholder-opacity-50'
-                            onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                                const maybeNumber = e.target.value;
-                                isNaN(+maybeNumber)
-                                    ? setAmount(0)
-                                    : setAmount(+maybeNumber);
-                            }}
-                            value={amount}
-                        />
-                    </div>
+            </div>
+            <div className='flex h-14 w-72 flex-row items-center justify-between space-x-2 rounded-lg bg-black-20 py-2 pl-2 pr-4 focus-within:ring'>
+                <div>
+                    <DropdownSelector
+                        optionList={options}
+                        onChange={setAsset}
+                    />
+                </div>
+                <div>
+                    <input
+                        type='text'
+                        placeholder='0'
+                        className='typography-body-1 h-8 w-24 rounded-lg bg-transparent p-2 text-right font-bold text-white placeholder-opacity-50'
+                        onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                            const maybeNumber = e.target.value;
+                            isNaN(+maybeNumber)
+                                ? setAmount(0)
+                                : setAmount(+maybeNumber);
+                        }}
+                        value={amount}
+                    />
+                </div>
 
-                    <div
-                        className='typography-caption ml-2 text-white-60'
-                        data-testid='asset-selector-transformed-value'
-                    >
-                        {transform(asset)}
-                    </div>
+                <div
+                    className='typography-caption ml-2 text-white-60'
+                    data-testid='asset-selector-transformed-value'
+                >
+                    {transform(asset)}
                 </div>
             </div>
         </div>
