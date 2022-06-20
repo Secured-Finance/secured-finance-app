db1325332c87d282c6b92c9196aadd6e0cd9aae4
Reuven Aboulker <reuven.aboulker@gmail.com>
2022-06-20 19:40:00 +0800

    feat(design): add focus ring for selectors
    SF-156

--- src/components/molecules/TermSelector/TermSelector.tsx
+++ src/components/molecules/TermSelector/TermSelector.tsx
@@ -12,24 +12,19 @@ export const TermSelector = ({
     const [term, setTerm] = useState('');
 
     return (
-        <div>
-            <div className='flex flex-col items-start justify-start space-y-2'>
-                <label className='typography-caption ml-2 text-planetaryPurple'>
-                    Loan Term
-                </label>
-                <div className='flex h-14 w-72 flex-row items-center justify-between rounded-lg bg-black-20 py-2 pl-2 pr-4 focus:border-2 focus:border-horizonBlue'>
-                    <div>
-                        <DropdownSelector
-                            optionList={options}
-                            onChange={setTerm}
-                        />
-                    </div>
-                    <div
-                        className='typography-caption text-white-60'
-                        data-testid='term-selector-transformed-value'
-                    >
-                        {transform(term)}
-                    </div>
+        <div className='flex flex-col items-start justify-start space-y-2'>
+            <label className='typography-caption ml-2 text-planetaryPurple'>
+                Loan Term
+            </label>
+            <div className='flex h-14 w-72 flex-row items-center justify-between rounded-lg bg-black-20 py-2 pl-2 pr-4 focus-within:ring'>
+                <div>
+                    <DropdownSelector optionList={options} onChange={setTerm} />
+                </div>
+                <div
+                    className='typography-caption text-white-60'
+                    data-testid='term-selector-transformed-value'
+                >
+                    {transform(term)}
                 </div>
             </div>
         </div>
