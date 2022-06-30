e8ead97bb09a71fa4880f94fd35f3857015062ea
Reuven Aboulker <reuven.aboulker@gmail.com>
2022-06-30 14:05:45 +0800

    fix: remove the reactRemoveProperties
    SF-146

--- next.config.js
+++ next.config.js
@@ -3,11 +3,6 @@ const webpack = require('./webpack-config');
 
 module.exports = {
     reactStrictMode: true,
-    compiler: {
-        // The regexes defined here are processed in Rust so the syntax is different from
-        // JavaScript `RegExp`s. See https://docs.rs/regex.
-        reactRemoveProperties: { properties: ['^data-cy$', '^data-test$'] },
-    },
 
     webpack: (config, options) => webpack.webpackOverride(config),
 };
