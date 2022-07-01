9bd7aa93b18841a9c3e3b0f303fec74cf5ee90d0
Arpit Singh <arpit@secured-finance.com>
2022-07-01 19:34:12 +0530

    fix: add padding and aspect ratio in chart [SF-157]

--- src/components/molecules/LineChart/constants.ts
+++ src/components/molecules/LineChart/constants.ts
@@ -6,32 +6,21 @@ import {
     ChartOptions,
 } from 'chart.js';
 
-const canvas: HTMLCanvasElement = document.createElement('canvas');
-const ctx: CanvasRenderingContext2D = canvas.getContext('2d');
-
-const yieldCurveGradient = ctx.createLinearGradient(0, 0, 500, 0);
-yieldCurveGradient.addColorStop(0, 'rgba(255, 89, 248, 0)');
-yieldCurveGradient.addColorStop(0.2, 'rgba(174, 114, 255, 1)');
-yieldCurveGradient.addColorStop(0.5, 'rgba(144, 233, 237, 1');
-yieldCurveGradient.addColorStop(0.78, 'rgba(92, 209, 103, 1)');
-yieldCurveGradient.addColorStop(1, 'rgba(255, 238, 0, 0)');
-
-export const commonDataset = {
+export const defaultDatasets = {
     borderWidth: 3,
     radius: 0,
     lineTension: 0.4,
     pointRadius: 0.01,
 };
 
-export const defaultDatasets = [
-    {
-        ...commonDataset,
-        borderColor: yieldCurveGradient,
-    },
-];
-
 export const options: ChartOptions<'line'> = {
     responsive: true,
+    maintainAspectRatio: false,
+    layout: {
+        padding: {
+            top: 50,
+        },
+    },
     elements: {
         point: {
             hoverRadius: 12,
@@ -50,7 +39,7 @@ export const options: ChartOptions<'line'> = {
             beginAtZero: true,
             ticks: {
                 backdropColor: 'rgba(255, 255, 0, 0.6)',
-                padding: 0,
+                padding: 10,
             },
             grid: {
                 display: false,
@@ -63,7 +52,7 @@ export const options: ChartOptions<'line'> = {
     plugins: {
         tooltip: {
             yAlign: 'bottom',
-            caretPadding: 20,
+            caretPadding: 15,
             backgroundColor: 'rgba(47, 50, 65, 1)',
             borderWidth: 1,
             borderColor: 'rgba(52, 56, 76, 1)',
@@ -98,7 +87,7 @@ export const options: ChartOptions<'line'> = {
             const bottomY = this.chartArea.bottom;
             ctx.beginPath();
             ctx.moveTo(x, y + 12);
-            ctx.lineTo(x, bottomY + 6);
+            ctx.lineTo(x, bottomY + 15);
             ctx.closePath();
             ctx.lineWidth = 1;
             ctx.strokeStyle = '#FCFCFD';
