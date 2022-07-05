86f7a86d2a819d64d269a3236fd3063d3b756332
Arpit Singh <arpit@secured-finance.com>
2022-07-05 12:49:03 +0530

    test: add snap test on YieldChart [SF-157]

--- src/components/organisms/YieldChart/YieldChart.test.tsx
+++ src/components/organisms/YieldChart/YieldChart.test.tsx
@@ -1,11 +1,16 @@
 import { composeStories } from '@storybook/testing-react';
-import { render } from 'src/test-utils.js';
+import { queries, render, RenderResult, waitFor } from 'src/test-utils.js';
 import * as stories from './YieldChart.stories';
 
 const { Default } = composeStories(stories);
 
 describe('YieldChart Component', () => {
     it('should render YieldChart', async () => {
-        render(<Default />);
+        let ag: RenderResult<typeof queries, HTMLElement>;
+        await waitFor(() => {
+            ag = render(<Default />);
+        }).then(() => {
+            expect(ag.baseElement).toMatchSnapshot();
+        });
     });
 });
