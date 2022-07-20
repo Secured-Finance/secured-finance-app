b8efb1f9d84e1f35dd8d9e2511dabd86987bb51c
Arpit Singh <arpit@secured-finance.com>
2022-07-20 14:48:59 +0530

    refactor: add png file for sf-sphere logo [SF-161]

--- src/components/organisms/ConnectWalletCard/ConnectWalletCard.tsx
+++ src/components/organisms/ConnectWalletCard/ConnectWalletCard.tsx
@@ -1,6 +1,7 @@
+/* eslint-disable @next/next/no-img-element */
 import { useState } from 'react';
 import SFLogo from 'src/assets/img/logo.svg';
-import SFSphere from 'src/assets/img/sf-sphere.svg';
+import SFSphere from 'src/assets/img/sf-sphere.png';
 import { Button } from 'src/components/atoms';
 import { WalletDialog } from 'src/components/organisms';
 
@@ -12,7 +13,11 @@ export const ConnectWalletCard = () => {
             <div className='h-1 w-full bg-starBlue'></div>
             <div className='h-full w-full rounded-b-2xl border border-white-10 bg-gradient-to-b from-[rgba(106,118,177,0.1)] via-[rgba(106,118,177,0)] to-black-20 px-6'>
                 <div className='flex h-full flex-col items-center gap-4 pt-5 pb-6'>
-                    <SFSphere className='h-[188px] w-[210px]'></SFSphere>
+                    <img
+                        src={SFSphere.src}
+                        className='h-[188px] w-[210px]'
+                        alt='SF Logo'
+                    ></img>
                     <SFLogo className='h-5 w-[208px]'></SFLogo>
                     <div className='typography-caption h-[92px] w-[230px] pb-5 text-center text-[#718096]'>
                         Welcome to the future of DeFi. Secured Finance brings
