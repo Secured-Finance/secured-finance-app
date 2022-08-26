bd28a688c954a748796530451e3c94ef9b630a04
Arpit Singh <arpit@secured-finance.com>
2022-08-26 18:21:02 +0530

    refactor: refactor logic to produce list of wallet and balance [SF-148]

--- src/utils/currencies.tsx
+++ src/utils/currencies.tsx
@@ -1,6 +1,12 @@
 import ethereum from 'src/assets/coins/eth2.svg';
 import FilIcon from 'src/assets/coins/fil.svg';
+import { AssetDisclosureProps } from 'src/components/molecules';
 import { CurrencyInfo, currencyMap, CurrencySymbol } from './currencyList';
+
+export enum WalletSource {
+    METAMASK = 'METAMASK',
+    LEDGER = 'LEDGER',
+}
 
 export type CollateralInfo = {
     indexCcy: number;
@@ -43,4 +49,42 @@ export const collateralListDropdown = [
     },
 ];
 
+export const walletInformation = [
+    {
+        walletSource: WalletSource.METAMASK,
+        currencies: [CurrencySymbol.ETH],
+    },
+    {
+        walletSource: WalletSource.LEDGER,
+        currencies: [CurrencySymbol.FIL],
+    },
+];
+
 export const collateralList = [currencyMap.ETH] as Array<CurrencyInfo>;
+
+export const generateWalletInformation = (
+    accounts: Record<WalletSource, string>,
+    balance: Record<string, number>
+): AssetDisclosureProps[] => {
+    const collateralRecords = [];
+    for (let i = 0; i < walletInformation.length; i++) {
+        const wallet = walletInformation[i].walletSource;
+        const data = [];
+        if (accounts[wallet]) {
+            const currenciesArray = walletInformation[i].currencies;
+            for (let j = 0; j < currenciesArray.length; j++) {
+                const asset = currenciesArray[j] as CurrencySymbol;
+                data.push({
+                    asset: asset,
+                    quantity: balance[asset] ? balance[asset] : 0,
+                });
+            }
+            collateralRecords.push({
+                data: data,
+                walletSource: wallet,
+                account: accounts[wallet],
+            });
+        }
+    }
+    return collateralRecords;
+};
