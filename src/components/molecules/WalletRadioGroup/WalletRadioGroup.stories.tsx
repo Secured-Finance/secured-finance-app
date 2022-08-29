2ab25054d3b2a5286662dd4f06facb49e864e352
Reuven Aboulker <reuven.aboulker@gmail.com>
2022-08-29 19:07:40 +0800

    refactor: pass argument to the walletRadioGroup
    SF-178

--- src/components/molecules/WalletRadioGroup/WalletRadioGroup.stories.tsx
+++ src/components/molecules/WalletRadioGroup/WalletRadioGroup.stories.tsx
@@ -1,5 +1,7 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import React from 'react';
+import MetaMaskIcon from 'src/assets/img/metamask-fox.svg';
+import WalletConnectIcon from 'src/assets/img/wallet-connect.svg';
 import { WalletRadioGroup } from './WalletRadioGroup';
 
 export default {
@@ -12,7 +14,22 @@ export default {
 
 const Template: ComponentStory<typeof WalletRadioGroup> = () => {
     const [value, setValue] = React.useState('');
-    return <WalletRadioGroup value={value} onChange={setValue} />;
+    return (
+        <WalletRadioGroup
+            value={value}
+            onChange={setValue}
+            options={[
+                {
+                    name: 'Metamask',
+                    Icon: MetaMaskIcon,
+                },
+                {
+                    name: 'WalletConnect',
+                    Icon: WalletConnectIcon,
+                },
+            ]}
+        />
+    );
 };
 
 export const Default = Template.bind({});
