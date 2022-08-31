61042fe41d3c0b6fbd2eb75c0eb3cb6616dd25c1
Reuven Aboulker <reuven.aboulker@gmail.com>
2022-08-31 15:59:29 +0800

    test: add more test for the FilWalletDialog [SF-178]

--- src/components/organisms/FilWalletDialog/FilWalletDialog.test.tsx
+++ src/components/organisms/FilWalletDialog/FilWalletDialog.test.tsx
@@ -1,11 +1,35 @@
 import { composeStories } from '@storybook/testing-react';
-import { render } from 'src/test-utils.js';
+import { render, screen } from 'src/test-utils.js';
 import * as stories from './FilWalletDialog.stories';
+
+// @ts-expect-error: this is a mock for the IntersectionObserver.
+global.IntersectionObserver = class FakeIntersectionObserver {
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    observe() {}
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    disconnect() {}
+};
 
 const { Default } = composeStories(stories);
 
 describe('FilWalletDialog Component', () => {
     it('should render a FilWalletDialog', () => {
         render(<Default />);
+    });
+
+    it('should display three option', () => {
+        render(<Default />);
+        expect(screen.getAllByRole('radio')).toHaveLength(3);
+        expect(screen.getAllByRole('radio')[0]).toHaveTextContent('Ledger');
+        expect(screen.getAllByRole('radio')[1]).toHaveTextContent('Mnemonic');
+        expect(screen.getAllByRole('radio')[2]).toHaveTextContent('PrivateKey');
+    });
+
+    it('should close after selecting an option', () => {
+        const onClose = jest.fn();
+        render(<Default onClose={onClose} />);
+        screen.getByText('Mnemonic').click();
+        screen.getByText('Continue').click();
+        expect(onClose).toHaveBeenCalled();
     });
 });
