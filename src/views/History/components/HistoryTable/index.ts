ff33248a98a2f4c6c00c2fc89ea2ac4936d1e261
Reuven Aboulker <reuven.aboulker@gmail.com>
2022-09-05 12:46:08 +0800

    refactor: remove unused component [SF-149]

--- src/views/History/components/HistoryTable/index.ts
+++
@@ -1,5 +0,0 @@
-import HistoryTable from './HistoryTable';
-import { historyTableColumns, TableColumns } from './types';
-
-export { HistoryTable, historyTableColumns };
-export type { TableColumns };
