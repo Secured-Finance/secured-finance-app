289446b68c2d455de58cd07eb4cb0d9c6239d793
Reuven Aboulker <reuven.aboulker@gmail.com>
2022-09-07 08:31:38 +0800

    feat: add sort arrows icon [SF-149]

---
+++ src/components/atoms/SortArrows/SortArrows.tsx
@@ -0,0 +1,27 @@
+import { ChevronDownIcon } from '@heroicons/react/outline';
+import classNames from 'classnames';
+
+export const SortArrows = ({
+    isSorted,
+}: {
+    isSorted: boolean | 'asc' | 'desc';
+}) => {
+    return (
+        <div className='flex flex-col'>
+            <ChevronDownIcon
+                data-testid='up-sort-arrows'
+                className={classNames('-mb-0.5 h-3 w-3 rotate-180 ', {
+                    'text-white': isSorted === 'asc',
+                    'text-slateGray': isSorted === false || isSorted === 'desc',
+                })}
+            />
+            <ChevronDownIcon
+                data-testid='down-sort-arrows'
+                className={classNames('-mt-0.5 h-3 w-3', {
+                    'text-white': isSorted === 'desc',
+                    'text-slateGray': isSorted === false || isSorted === 'asc',
+                })}
+            />
+        </div>
+    );
+};
