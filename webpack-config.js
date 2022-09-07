5ceda259260cddd022b0d90c985070fbfca6aa72
Reuven Aboulker <reuven.aboulker@gmail.com>
2022-09-07 15:55:17 +0800

    feat: remove unused dependencies [SF-178]

--- webpack-config.js
+++ webpack-config.js
@@ -41,11 +41,5 @@ function webpackOverride(config) {
         })
     );
 
-    config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm';
-
-    config.experiments = {
-        asyncWebAssembly: true,
-    };
-
     return config;
 }
