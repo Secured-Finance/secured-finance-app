c0f035e3f3d88448282bdacee72fa64d51a525de
Reuven Aboulker <reuven.aboulker@gmail.com>
2022-09-17 16:07:56 +0800

    test: fix collateral amount in mock [SF-145]

--- src/components/pages/Landing/Landing.stories.tsx
+++ src/components/pages/Landing/Landing.stories.tsx
@@ -3,6 +3,7 @@ import { useEffect, useMemo } from 'react';
 import { useDispatch } from 'react-redux';
 import {
     WithAppLayout,
+    WithAssetPrice,
     WithWalletProvider,
 } from 'src/../.storybook/decorators';
 import { updateLendingMarketContract } from 'src/store/availableContracts';
@@ -13,7 +14,7 @@ export default {
     title: 'Pages/Landing',
     component: Landing,
     args: {},
-    decorators: [WithAppLayout, WithWalletProvider],
+    decorators: [WithAppLayout, WithAssetPrice, WithWalletProvider],
 } as ComponentMeta<typeof Landing>;
 
 const Template: ComponentStory<typeof Landing> = () => {
@@ -38,7 +39,7 @@ const Template: ComponentStory<typeof Landing> = () => {
             dispatch(
                 updateLendingMarketContract(maturities, CurrencySymbol.USDC)
             );
-        }, 100);
+        }, 200);
     }, [dispatch, maturities]);
     return <Landing />;
 };
