62d1c7a68575a1a935f707e8c1659548abeb3fa6
Reuven Aboulker <reuven.aboulker@gmail.com>
2022-10-06 08:37:38 +0200

    test: force garbage collection between tests [SF-220]
    using jest with storybook seems to keep in memory big object.
    Forcing garbage collection fixes the issue

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,19 +2,14 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import { loadEnvConfig } from '@next/env';
 import '@testing-library/jest-dom';
 import 'jest-canvas-mock';
 import failOnConsole from 'jest-fail-on-console';
-import { TextDecoder } from 'util';
 
-global.ResizeObserver = jest.fn().mockImplementation(() => ({
-    observe: jest.fn(),
-    unobserve: jest.fn(),
-    disconnect: jest.fn(),
-}));
-
-global.TextDecoder = TextDecoder;
-
-loadEnvConfig(process.cwd());
 failOnConsole();
+
+afterAll(() => {
+    global.gc && global.gc();
+    jest.clearAllMocks();
+    jest.clearAllTimers();
+});
