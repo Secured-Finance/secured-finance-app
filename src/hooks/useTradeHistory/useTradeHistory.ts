3af09fd400beed4993f538d091540973473e6b98
Arpit Singh <arpit@secured-finance.com>
2022-10-20 18:33:11 +0530

    feat: active trades refresh on ui [SF-226]

--- src/hooks/useTradeHistory/useTradeHistory.ts
+++ src/hooks/useTradeHistory/useTradeHistory.ts
@@ -6,21 +6,33 @@ import {
     BuyerTransactionsQuery,
     SellerTransactionsQuery,
 } from '@secured-finance/sf-graph-client/dist/graphclients';
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from 'src/store/types';
 
 export type TradeHistory =
     | SellerTransactionsQuery['transactions']
     | BuyerTransactionsQuery['transactions'];
 
 export const useTradeHistory = (account: string | null) => {
-    const { data: lendingHistory, error: lendingError } =
-        useSellerTransactionHistory({
-            account: account ?? '',
-        });
+    const block = useSelector(
+        (state: RootState) => state.blockchain.latestBlock
+    );
+    const {
+        data: lendingHistory,
+        error: lendingError,
+        refetch: refetchSeller,
+    } = useSellerTransactionHistory({
+        account: account ?? '',
+    });
 
-    const { data: borrowingHistory, error: borrowingError } =
-        useBuyerTransactionHistory({
-            account: account ?? '',
-        });
+    const {
+        data: borrowingHistory,
+        error: borrowingError,
+        refetch: refetchBuyer,
+    } = useBuyerTransactionHistory({
+        account: account ?? '',
+    });
 
     if (lendingError) {
         console.error(lendingError);
@@ -29,6 +41,11 @@ export const useTradeHistory = (account: string | null) => {
     if (borrowingError) {
         console.error(borrowingError);
     }
+
+    useEffect(() => {
+        refetchSeller?.();
+        refetchBuyer?.();
+    }, [block, refetchSeller, refetchBuyer]);
 
     return [
         ...(lendingHistory?.transactions ?? []),
