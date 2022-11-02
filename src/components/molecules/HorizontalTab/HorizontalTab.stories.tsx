fa62ce6c00a073c7d3815b17337bf98407cb024b
Reuven Aboulker <reuven.aboulker@gmail.com>
2022-11-02 13:37:00 +0000

    refactor: rename tab component [SF-240]

--- src/components/molecules/HorizontalTab/HorizontalTab.stories.tsx
+++ src/components/molecules/HorizontalTab/HorizontalTab.stories.tsx
@@ -1,18 +1,18 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-import { TradeHistoryTab } from './TradeHistoryTab';
+import { HorizontalTab } from './HorizontalTab';
 
 export default {
     title: 'Molecules/TradeHistoryTab',
-    component: TradeHistoryTab,
+    component: HorizontalTab,
     args: {},
-} as ComponentMeta<typeof TradeHistoryTab>;
+} as ComponentMeta<typeof HorizontalTab>;
 
-const Template: ComponentStory<typeof TradeHistoryTab> = () => (
+const Template: ComponentStory<typeof HorizontalTab> = () => (
     <div className='h-[400px] w-[600px] text-white-80'>
-        <TradeHistoryTab tabTitles={['Active Contracts', 'Trade History']}>
+        <HorizontalTab tabTitles={['Active Contracts', 'Trade History']}>
             <div>This is a Great Tab Content</div>
             <div className='py-12'>This is the content of the second tab</div>
-        </TradeHistoryTab>
+        </HorizontalTab>
     </div>
 );
 
