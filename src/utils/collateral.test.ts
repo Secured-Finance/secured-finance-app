accf4e6bb009cd24fb2f486eb40b6227b9a915a8
Reuven Aboulker <reuven.aboulker@gmail.com>
2022-11-15 09:41:01 +0800

    test: address PR review [SF-240]

--- src/utils/collateral.test.ts
+++ src/utils/collateral.test.ts
@@ -2,6 +2,7 @@ import { BigNumber } from 'ethers';
 import { calculatePercentage, computeAvailableToBorrow } from './collateral';
 
 const ONE_ETH = BigNumber.from('1000000000000000000');
+const TWO_ETH = BigNumber.from('2000000000000000000');
 
 describe('collateral.computeAvailableToBorrow', () => {
     it('should compute the available amount to borrow with a collateral of 150%', () => {
@@ -21,7 +22,7 @@ describe('collateral.computeAvailableToBorrow', () => {
 
 describe('collateral.calculatePercentage', () => {
     it('should compute the percentage', () => {
-        expect(calculatePercentage(ONE_ETH, ONE_ETH.mul(2))).toEqual(
+        expect(calculatePercentage(ONE_ETH, TWO_ETH)).toEqual(
             BigNumber.from(50)
         );
     });
