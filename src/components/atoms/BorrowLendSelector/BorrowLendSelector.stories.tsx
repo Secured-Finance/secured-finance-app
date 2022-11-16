66837a7c291ae5e240a7313aa9fc6594c86f6d68
Arpit Singh <arpit@secured-finance.com>
2022-11-17 01:35:43 +0530

    test: address review comments [SF-241]

--- src/components/atoms/BorrowLendSelector/BorrowLendSelector.stories.tsx
+++ src/components/atoms/BorrowLendSelector/BorrowLendSelector.stories.tsx
@@ -5,11 +5,16 @@ import { BorrowLendSelector } from '.';
 export default {
     title: 'Atoms/BorrowLendSelector',
     component: BorrowLendSelector,
-    args: { side: OrderSide.Borrow, variant: 'advanced' },
+    args: { side: OrderSide.Borrow, variant: 'simple' },
 } as ComponentMeta<typeof BorrowLendSelector>;
 
 const Template: ComponentStory<typeof BorrowLendSelector> = args => (
     <BorrowLendSelector {...args} />
 );
 
-export const Default = Template.bind({});
+export const Simple = Template.bind({});
+export const Advanced = Template.bind({});
+Advanced.args = {
+    side: OrderSide.Lend,
+    variant: 'advanced',
+};
