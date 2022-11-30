e6ea85aad64d45d4b0e1557a9fd5d6faec955528
Arpit Singh <arpit@secured-finance.com>
2022-11-30 21:50:48 +0530

    fix: fix some css [SF-244]

--- src/components/atoms/HorizontalListItem/HorizontalListItem.tsx
+++ src/components/atoms/HorizontalListItem/HorizontalListItem.tsx
@@ -6,9 +6,9 @@ export const HorizontalListItem = ({
     value: string;
 }) => {
     return (
-        <div className='typography-caption grid w-full grid-cols-2 justify-around font-bold'>
+        <div className='typography-caption grid w-full grid-cols-2 justify-around'>
             <span className='text-left text-planetaryPurple'>{label}</span>
-            <span className='text-right text-white'>{value}</span>
+            <span className='text-right text-neutral-8'>{value}</span>
         </div>
     );
 };
