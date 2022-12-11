39173891dd72c9c9e89c1fbf7155dd0c6ebb3779
Reuven Aboulker <reuven.aboulker@gmail.com>
2022-12-11 19:36:42 +0800

    refactor: create decorator for maturities [SF-247]

--- src/components/organisms/MarketOrganism/MarketOrganism.stories.tsx
+++ src/components/organisms/MarketOrganism/MarketOrganism.stories.tsx
@@ -1,10 +1,9 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-import { useEffect, useMemo } from 'react';
-import { useDispatch } from 'react-redux';
-import { WithWalletProvider } from 'src/../.storybook/decorators';
-import { updateLendingMarketContract } from 'src/store/availableContracts';
+import {
+    withMaturities,
+    WithWalletProvider,
+} from 'src/../.storybook/decorators';
 import { maturityOptions } from 'src/stories/mocks/fixtures';
-import { CurrencySymbol } from 'src/utils';
 import { MarketOrganism } from './MarketOrganism';
 
 export default {
@@ -13,35 +12,10 @@ export default {
     args: {
         maturitiesOptionList: maturityOptions,
     },
-    decorators: [WithWalletProvider],
+    decorators: [WithWalletProvider, withMaturities],
 } as ComponentMeta<typeof MarketOrganism>;
 
 const Template: ComponentStory<typeof MarketOrganism> = args => {
-    const maturities = useMemo(
-        () => ({
-            MAR22: 1616508800,
-            JUN22: 1625097600,
-            SEP22: 1633046400,
-            DEC22: 1640995200,
-        }),
-        []
-    );
-    const dispatch = useDispatch();
-    useEffect(() => {
-        const timerId = setTimeout(() => {
-            dispatch(
-                updateLendingMarketContract(maturities, CurrencySymbol.FIL)
-            );
-            dispatch(
-                updateLendingMarketContract(maturities, CurrencySymbol.ETH)
-            );
-            dispatch(
-                updateLendingMarketContract(maturities, CurrencySymbol.USDC)
-            );
-        }, 200);
-
-        return () => clearTimeout(timerId);
-    }, [dispatch, maturities]);
     return <MarketOrganism {...args} />;
 };
 
--- src/components/pages/MarketDashboard/MarketDashboard.stories.tsx
+++ src/components/pages/MarketDashboard/MarketDashboard.stories.tsx
@@ -1,48 +1,25 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-import { useEffect, useMemo } from 'react';
-import { useDispatch } from 'react-redux';
 import {
     WithAppLayout,
     WithAssetPrice,
+    withMaturities,
     WithWalletProvider,
 } from 'src/../.storybook/decorators';
-import { updateLendingMarketContract } from 'src/store/availableContracts';
-import { CurrencySymbol } from 'src/utils';
 import { MarketDashboard } from './MarketDashboard';
 
 export default {
     title: 'Pages/MarketDashboard',
     component: MarketDashboard,
     args: {},
-    decorators: [WithAppLayout, WithAssetPrice, WithWalletProvider],
+    decorators: [
+        WithAppLayout,
+        withMaturities,
+        WithAssetPrice,
+        WithWalletProvider,
+    ],
 } as ComponentMeta<typeof MarketDashboard>;
 
 const Template: ComponentStory<typeof MarketDashboard> = () => {
-    const maturities = useMemo(
-        () => ({
-            MAR22: 1616508800,
-            JUN22: 1625097600,
-            SEP22: 1633046400,
-            DEC22: 1640995200,
-        }),
-        []
-    );
-    const dispatch = useDispatch();
-    useEffect(() => {
-        const timerId = setTimeout(() => {
-            dispatch(
-                updateLendingMarketContract(maturities, CurrencySymbol.FIL)
-            );
-            dispatch(
-                updateLendingMarketContract(maturities, CurrencySymbol.ETH)
-            );
-            dispatch(
-                updateLendingMarketContract(maturities, CurrencySymbol.USDC)
-            );
-        }, 200);
-
-        return () => clearTimeout(timerId);
-    }, [dispatch, maturities]);
     return <MarketDashboard />;
 };
 
