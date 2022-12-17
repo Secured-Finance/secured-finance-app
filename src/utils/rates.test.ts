9c07dea39af0a3a8d980ecda6bd5a8849a0cfc81
Reuven Aboulker <reuven.aboulker@gmail.com>
2022-12-17 12:00:03 +0800

    fix: cap and floor rate
    we do not want to break the ui in case of dummy orders [SF-247]

--- src/utils/rates.test.ts
+++ src/utils/rates.test.ts
@@ -5,4 +5,12 @@ describe('Rate class', () => {
         const rate = new Rate(123);
         expect(rate.toNumber()).toBe(123);
     });
+
+    it('should always be between 0 and 1000%', () => {
+        const rate = new Rate(-1);
+        expect(rate.toNumber()).toBe(0);
+
+        const rate2 = new Rate(100000000);
+        expect(rate2.toNumber()).toBe(10000000);
+    });
 });
