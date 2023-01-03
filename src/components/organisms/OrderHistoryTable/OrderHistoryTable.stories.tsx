8e7392a9be0055dd205a7b4c7782ba92ea6509af
Reuven Aboulker <reuven.aboulker@gmail.com>
2023-01-02 19:39:35 -0600

    test: add mock queries for orders [SF-193]

--- src/components/organisms/OrderHistoryTable/OrderHistoryTable.stories.tsx
+++ src/components/organisms/OrderHistoryTable/OrderHistoryTable.stories.tsx
@@ -1,62 +1,12 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-import { BigNumber } from 'ethers';
-import { dec22Fixture } from 'src/stories/mocks/fixtures';
+import { orderHistoryList } from 'src/stories/mocks/fixtures';
 import { OrderHistoryTable } from './OrderHistoryTable';
 
 export default {
     title: 'Organism/OrderHistoryTable',
     component: OrderHistoryTable,
     args: {
-        data: [
-            {
-                id: '1',
-                orderId: BigNumber.from('1'),
-                originalOrderId: BigNumber.from('1'),
-                maker: '0x0000000000000000000000000000000000000000',
-                currency:
-                    '0x4554480000000000000000000000000000000000000000000000000000000000',
-                side: 1,
-                maturity: BigNumber.from(dec22Fixture.toString()),
-                unitPrice: BigNumber.from('9800'),
-                amount: BigNumber.from('1'),
-                status: 'Filled',
-                createdAt: BigNumber.from('1'),
-                blockNumber: BigNumber.from('1'),
-                txHash: '1',
-            },
-            {
-                id: '2',
-                orderId: BigNumber.from('1'),
-                originalOrderId: BigNumber.from('1'),
-                maker: '0x0000000000000000000000000000000000000000',
-                currency:
-                    '0x4554480000000000000000000000000000000000000000000000000000000000',
-                side: 1,
-                maturity: BigNumber.from(dec22Fixture.toString()),
-                unitPrice: BigNumber.from('9600'),
-                amount: BigNumber.from('1'),
-                status: 'Open',
-                createdAt: BigNumber.from('1'),
-                blockNumber: BigNumber.from('1'),
-                txHash: '1',
-            },
-            {
-                id: '3',
-                orderId: BigNumber.from('1'),
-                originalOrderId: BigNumber.from('1'),
-                maker: '0x0000000000000000000000000000000000000000',
-                currency:
-                    '0x4554480000000000000000000000000000000000000000000000000000000000',
-                side: 0,
-                maturity: BigNumber.from(dec22Fixture.toString()),
-                unitPrice: BigNumber.from('9800'),
-                amount: BigNumber.from('1'),
-                status: 'Filled',
-                createdAt: BigNumber.from('1'),
-                blockNumber: BigNumber.from('1'),
-                txHash: '1',
-            },
-        ],
+        data: orderHistoryList,
     },
 } as ComponentMeta<typeof OrderHistoryTable>;
 
--- src/components/pages/PortfolioManagement/PortfolioManagement.stories.tsx
+++ src/components/pages/PortfolioManagement/PortfolioManagement.stories.tsx
@@ -1,62 +1,14 @@
-import { TransactionHistoryDocument } from '@secured-finance/sf-graph-client/dist/graphclients';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-import { BigNumber, utils } from 'ethers';
 import {
     withAppLayout,
     withAssetPrice,
     withWalletProvider,
 } from 'src/../.storybook/decorators';
-import { TradeHistory } from 'src/types';
+import {
+    mockOrderHistory,
+    mockTransactionHistory,
+} from 'src/stories/mocks/queries';
 import { PortfolioManagement } from './PortfolioManagement';
-
-const fil = utils.formatBytes32String('FIL');
-const eth = utils.formatBytes32String('ETH');
-const btc = utils.formatBytes32String('BTC');
-
-const transactions: TradeHistory = [
-    {
-        id: '0x123',
-        amount: '1000000000000000000000',
-        averagePrice: BigNumber.from(8000),
-        side: 0,
-        orderPrice: 100000,
-        createdAt: 123,
-        blockNumber: 123,
-        taker: '0x123',
-        forwardValue: 100000,
-        txHash: '0x123',
-        currency: fil,
-        maturity: BigNumber.from(1733011200),
-    },
-    {
-        id: '0x123',
-        amount: '1000000000',
-        averagePrice: BigNumber.from(9000),
-        side: 1,
-        orderPrice: 100000,
-        createdAt: 123,
-        blockNumber: 123,
-        taker: '0x123',
-        forwardValue: 100000,
-        txHash: '0x123',
-        currency: btc,
-        maturity: BigNumber.from(1733011200),
-    },
-    {
-        id: '0x123',
-        amount: '1000000000',
-        averagePrice: BigNumber.from(9000),
-        side: 1,
-        orderPrice: 100000,
-        createdAt: 123,
-        blockNumber: 123,
-        taker: '0x123',
-        forwardValue: 100000,
-        txHash: '0x123',
-        currency: eth,
-        maturity: BigNumber.from(1733011200),
-    },
-];
 
 export default {
     title: 'Pages/PortfolioManagement',
@@ -65,52 +17,7 @@ export default {
     decorators: [withAssetPrice, withAppLayout, withWalletProvider],
     parameters: {
         apolloClient: {
-            mocks: [
-                {
-                    request: {
-                        query: TransactionHistoryDocument,
-                        variables: {
-                            address: '',
-                            awaitRefetchQueries: true,
-                        },
-                    },
-                    result: {
-                        data: {
-                            transactions: [],
-                        },
-                    },
-                    newData: () => {
-                        return {
-                            data: {
-                                transactions: [],
-                            },
-                        };
-                    },
-                },
-                {
-                    request: {
-                        query: TransactionHistoryDocument,
-                        variables: {
-                            address:
-                                '0xb98bd7c7f656290071e52d1aa617d9cb4467fd6d',
-                            awaitRefetchQueries: true,
-                        },
-                    },
-
-                    result: {
-                        data: {
-                            transactions: transactions,
-                        },
-                    },
-                    newData: () => {
-                        return {
-                            data: {
-                                transactions: transactions,
-                            },
-                        };
-                    },
-                },
-            ],
+            mocks: [...mockTransactionHistory, ...mockOrderHistory],
         },
     },
 } as ComponentMeta<typeof PortfolioManagement>;
