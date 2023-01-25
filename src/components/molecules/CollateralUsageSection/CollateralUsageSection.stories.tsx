b65baa612e6319b97162ad3a4c981354a961a271
Reuven Aboulker <reuven.aboulker@gmail.com>
2023-01-25 12:25:52 +0000

    build: make CollateralUsageSection a molecule [SF-230]

--- src/components/molecules/CollateralUsageSection/CollateralUsageSection.stories.tsx
+++ src/components/molecules/CollateralUsageSection/CollateralUsageSection.stories.tsx
@@ -2,10 +2,10 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { withAssetPrice } from 'src/../.storybook/decorators';
 import { collateralBook80 } from 'src/stories/mocks/fixtures';
 import { CurrencySymbol } from 'src/utils';
-import { CollateralUsageSection } from './';
+import { CollateralUsageSection } from '.';
 
 export default {
-    title: 'Atoms/CollateralUsageSection',
+    title: 'Molecules/CollateralUsageSection',
     component: CollateralUsageSection,
     args: {
         usdCollateral: collateralBook80.usdCollateral,
