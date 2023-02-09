f2aeea371bc6d6be1a342cbe07321fa28f57ce32
Arpit Singh <arpit@secured-finance.com>
2023-02-10 00:48:09 +0530

    fix: remove unused variables [SF-295]

--- src/components/pages/MarketDashboard/MarketDashboard.tsx
+++ src/components/pages/MarketDashboard/MarketDashboard.tsx
@@ -1,15 +1,10 @@
-import { useSelector } from 'react-redux';
 import { MarketDashboardTable } from 'src/components/molecules';
 import { ConnectWalletCard, WalletDialog } from 'src/components/organisms';
 import { Page } from 'src/components/templates';
-import { RootState } from 'src/store/types';
 import { useWallet } from 'use-wallet';
 
 export const MarketDashboard = () => {
     const { account } = useWallet();
-    const open = useSelector(
-        (state: RootState) => state.interactions.walletDialogOpen
-    );
 
     return (
         <Page title='Market Dashboard' name='exchange-page'>
