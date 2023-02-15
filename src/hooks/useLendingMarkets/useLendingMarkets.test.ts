b321c412e1fab19ce3187ed354c3b2cfbacb56c8
Reuven Aboulker <reuven.aboulker@gmail.com>
2023-02-15 09:37:24 +0000

    refactor: reduce number of hooks in SFProvider
    this should reduce the number of rerender of the webapp [SF-301]

--- src/hooks/useLendingMarkets/useLendingMarkets.test.ts
+++ src/hooks/useLendingMarkets/useLendingMarkets.test.ts
@@ -9,12 +9,12 @@ const mock = mockUseSF();
 describe('useLendingMarkets', () => {
     it('should insert the lending market contract into the store', async () => {
         const { store, waitFor } = renderHook(() =>
-            useLendingMarkets(
-                CurrencySymbol.ETH,
-                mock as unknown as SecuredFinanceClient
-            )
+            useLendingMarkets(mock as unknown as SecuredFinanceClient)
         );
         waitFor(() => {
+            expect(
+                Object.keys(store.getState().availableContracts.lendingMarkets)
+            ).toHaveLength(4);
             expect(
                 store.getState().availableContracts.lendingMarkets[
                     CurrencySymbol.ETH
@@ -27,10 +27,10 @@ describe('useLendingMarkets', () => {
     });
 
     it('should return the initial state of the store if the client is not defined', async () => {
-        const { store } = renderHook(() =>
-            useLendingMarkets(CurrencySymbol.ETH, undefined)
-        );
-
+        const { store } = renderHook(() => useLendingMarkets(undefined));
+        expect(
+            Object.keys(store.getState().availableContracts.lendingMarkets)
+        ).toHaveLength(4);
         expect(
             store.getState().availableContracts.lendingMarkets[
                 CurrencySymbol.ETH
