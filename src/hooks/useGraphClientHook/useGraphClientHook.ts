0fb724653ea385836e90ece0c9ca01eec5812121
Reuven Aboulker <reuven.aboulker@gmail.com>
2023-02-21 11:18:32 +0000

    feat: add high, low for the last 24 hours [SF-301]
    this is not tested in storybook since we need exact variables.
    time needs to tick though to display the graph.

--- src/hooks/useGraphClientHook/useGraphClientHook.ts
+++ src/hooks/useGraphClientHook/useGraphClientHook.ts
@@ -3,22 +3,25 @@ import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from 'src/store/types';
 
-export const useGraphClientHook = <T, K extends keyof T>(
-    variables: { [key: string]: unknown },
+export const useGraphClientHook = <T, TVariables, K extends keyof T>(
+    variables: TVariables,
     queryDocument: Parameters<typeof useQuery<T>>[0],
     entity: K,
     realTime = false,
     client?: GraphApolloClient
 ) => {
-    const { error, data, refetch, networkStatus } = useQuery<T>(queryDocument, {
-        client: client,
-        variables: {
-            ...variables,
-            awaitRefetchQueries: true,
-        },
-        fetchPolicy: 'network-only',
-        notifyOnNetworkStatusChange: true,
-    });
+    const { error, data, refetch, networkStatus } = useQuery<T, TVariables>(
+        queryDocument,
+        {
+            client: client,
+            variables: {
+                ...variables,
+                awaitRefetchQueries: true,
+            },
+            fetchPolicy: 'network-only',
+            notifyOnNetworkStatusChange: true,
+        }
+    );
 
     const block = useSelector(
         (state: RootState) => state.blockchain.latestBlock
