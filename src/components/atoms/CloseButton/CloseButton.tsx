8f0ead1d10d70096ab970703708752201bb2baa8
Arpit Singh <arpit@secured-finance.com>
2023-03-06 20:29:58 +0530

    feat: add contextual buttons to place order buttons [SF-287]

--- src/components/atoms/CloseButton/CloseButton.tsx
+++ src/components/atoms/CloseButton/CloseButton.tsx
@@ -3,7 +3,7 @@ import { XIcon } from '@heroicons/react/outline';
 export const CloseButton = ({ onClick }: { onClick: () => void }) => {
     return (
         <button
-            className='flex h-8 w-8 items-center justify-center rounded-full border-2 border-neutral-3'
+            className='flex h-8 w-8 items-center justify-center rounded-full border-2 border-neutral-3 hover:border-white-30'
             onClick={onClick}
             data-testid='close-button'
         >
