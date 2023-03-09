4d95ce5b31b5f8bf35c1d4073c728d0979e2260c
Reuven Aboulker <reuven.aboulker@gmail.com>
2023-03-09 08:41:27 +0000

    fix: fix decimal orders [SF-356]

--- src/components/molecules/AmountCard/AmountCard.tsx
+++ src/components/molecules/AmountCard/AmountCard.tsx
@@ -1,41 +1,35 @@
 import { CurrencyIcon } from 'src/components/atoms';
-import {
-    currencyMap,
-    CurrencySymbol,
-    ordinaryFormat,
-    usdFormatAppendUSD,
-} from 'src/utils';
+import { currencyMap, ordinaryFormat, usdFormatAppendUSD } from 'src/utils';
+import { Amount } from 'src/utils/entities';
 
 export const AmountCard = ({
-    ccy,
     amount,
     price,
 }: {
-    ccy: CurrencySymbol;
-    amount: number;
+    amount: Amount;
     price: number;
 }) => {
     return (
         <div className='grid w-full grid-cols-2 justify-around'>
             <div className='flex flex-row items-center justify-start gap-2'>
                 <span>
-                    <CurrencyIcon ccy={ccy} />
+                    <CurrencyIcon ccy={amount.currency} />
                 </span>
                 <span className='typography-caption font-bold text-white'>
-                    {currencyMap[ccy].name}
+                    {currencyMap[amount.currency].name}
                 </span>
             </div>
             <div className='flex h-6 flex-row items-center justify-end gap-2'>
                 <span className='typography-body-1 font-bold text-white'>
-                    {ordinaryFormat(amount)}
+                    {ordinaryFormat(amount.value)}
                 </span>
                 <span className='typography-caption h-5 text-white-60'>
-                    {ccy}
+                    {amount.currency}
                 </span>
             </div>
             <div></div>
             <div className='typography-caption-3 text-right text-white-60'>
-                ~ {usdFormatAppendUSD(price * amount)}
+                ~ {usdFormatAppendUSD(amount.toUSD(price))}
             </div>
         </div>
     );
