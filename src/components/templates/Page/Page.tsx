1c5e18be5a06b4d6b991f409138cca6051135e32
Reuven Aboulker <reuven.aboulker@gmail.com>
2023-03-11 18:44:46 +0100

    feat: apply min width to page [SF-329]

--- src/components/templates/Page/Page.tsx
+++ src/components/templates/Page/Page.tsx
@@ -11,16 +11,16 @@ export const Page = ({
 }) => {
     return (
         <div
-            className='mt-7 flex min-w-fit flex-col gap-6 px-40'
+            className='mt-7 flex min-w-[744px] flex-col gap-6 px-40'
             data-testid={name}
         >
-            <div className='flex h-16 justify-between border-b-[0.5px] border-panelStroke'>
+            <div className='flex h-16 min-w-[744px] justify-between border-b-[0.5px] border-panelStroke'>
                 <span className='font-secondary text-lg font-light leading-7 text-white'>
                     {title}
                 </span>
                 {titleComponent ? titleComponent : null}
             </div>
-            {children}
+            <div className='min-w-[744px]'>{children}</div>
         </div>
     );
 };
