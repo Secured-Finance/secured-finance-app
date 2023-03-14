801f1bf85a76e333ead9e8c7ed3a7165f4b5ce6c
Reuven Aboulker <reuven.aboulker@gmail.com>
2023-03-14 09:58:48 +0100

    refactor: simplify min width [SF-329]

--- src/components/templates/TwoColumns/TwoColumns.tsx
+++ src/components/templates/TwoColumns/TwoColumns.tsx
@@ -4,9 +4,9 @@ export const TwoColumns = ({
     children: [React.ReactNode, React.ReactNode];
 }) => {
     return (
-        <div className='flex flex-row justify-between gap-6 pt-4'>
-            <div className='w-2/3 min-w-[480px]'>{children[0]}</div>
-            <div className='w-1/3 min-w-[240px]'>{children[1]}</div>
+        <div className='flex min-w-[720px] flex-row justify-between gap-6 pt-4'>
+            <div className='w-2/3'>{children[0]}</div>
+            <div className='w-1/3'>{children[1]}</div>
         </div>
     );
 };
