feaa0e5d1f1f9b7dffb2c4551ebf08579a14bf03
Reuven Aboulker <reuven.aboulker@gmail.com>
2023-03-23 12:55:56 +0000

    fix: add back spacing [SF-391]

--- src/components/templates/Page/Page.tsx
+++ src/components/templates/Page/Page.tsx
@@ -12,7 +12,10 @@ export const Page = ({
     name?: string;
 }) => {
     return (
-        <div className='mt-7 min-w-[744px] flex-col px-40' data-testid={name}>
+        <div
+            className='mt-7 flex min-w-[744px] flex-col gap-6 px-40'
+            data-testid={name}
+        >
             <div className='flex h-16 min-w-[744px] justify-between border-b-[0.5px] border-panelStroke'>
                 <span className='font-secondary text-lg font-light leading-7 text-white'>
                     {title}
