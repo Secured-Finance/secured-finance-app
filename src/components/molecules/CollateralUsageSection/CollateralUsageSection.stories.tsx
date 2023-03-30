3d972a8610c3c98c062179b72d0ff03b4498de8b
Arpit Singh <arpit@secured-finance.com>
2023-03-30 16:40:59 +0530

    feat: move to 80 percent collateral threshold [SF-422]

--- src/components/molecules/CollateralUsageSection/CollateralUsageSection.stories.tsx
+++ src/components/molecules/CollateralUsageSection/CollateralUsageSection.stories.tsx
@@ -1,6 +1,6 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { withAssetPrice } from 'src/../.storybook/decorators';
-import { collateralBook80 } from 'src/stories/mocks/fixtures';
+import { collateralBook37 } from 'src/stories/mocks/fixtures';
 import { CurrencySymbol } from 'src/utils';
 import { CollateralUsageSection } from '.';
 
@@ -8,8 +8,8 @@ export default {
     title: 'Molecules/CollateralUsageSection',
     component: CollateralUsageSection,
     args: {
-        usdCollateral: collateralBook80.usdCollateral,
-        collateralCoverage: collateralBook80.coverage.toNumber(),
+        usdCollateral: collateralBook37.usdCollateral,
+        collateralCoverage: collateralBook37.coverage.toNumber(),
         currency: CurrencySymbol.FIL,
     },
     decorators: [withAssetPrice],
