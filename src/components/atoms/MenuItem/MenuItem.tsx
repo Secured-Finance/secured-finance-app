0ea4eb1cfba8bd0f2b5e6eb486dd8d754f563ec2
Riya Kaushik <haibara@Riyas-MacBook-Pro.local>
2023-04-09 12:45:35 +0530

    fix: minor code refactor [SF-409]

--- src/components/atoms/MenuItem/MenuItem.tsx
+++ src/components/atoms/MenuItem/MenuItem.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { formatDataCy } from 'src/utils';
 
 export const MenuItem = ({
@@ -17,25 +18,27 @@ export const MenuItem = ({
             data-cy={formatDataCy(text)}
             className='group relative flex w-full cursor-pointer rounded-md p-2 transition focus:outline-none'
         >
-            <a href={link} className='h-full w-full' aria-label='Menu Item'>
-                <div className='group relative flex w-full cursor-pointer items-center'>
-                    <div className='flex h-10 w-10 items-center justify-center'>
-                        {icon}
-                    </div>
-                    <p className='typography-caption flex w-[90%] capitalize text-white'>
-                        {text}
-                    </p>
-                    {badge && (
-                        <span className='group relative my-3 flex h-full py-2 align-top'>
-                            <span className='absolute top-0 right-0 -mt-5 transform opacity-0 transition-opacity group-hover:opacity-100'>
-                                <div className=' my-1 rounded-full py-1'>
-                                    {badge}
-                                </div>
+            <Link href={link} className='h-full w-full' passHref>
+                <a href={link} className='h-full w-full' aria-label='Menu Item'>
+                    <div className='group relative flex w-full cursor-pointer items-center'>
+                        <div className='flex h-10 w-10 items-center justify-center'>
+                            {icon}
+                        </div>
+                        <p className='typography-caption flex w-[90%] capitalize text-white'>
+                            {text}
+                        </p>
+                        {badge && (
+                            <span className='group relative my-3 flex h-full py-2 align-top'>
+                                <span className='absolute top-0 right-0 -mt-5 transform opacity-0 transition-opacity group-hover:opacity-100'>
+                                    <div className=' my-1 rounded-full py-1'>
+                                        {badge}
+                                    </div>
+                                </span>
                             </span>
-                        </span>
-                    )}
-                </div>
-            </a>
+                        )}
+                    </div>
+                </a>
+            </Link>
         </div>
     );
 };
