41abb62c98c2945af1c3c3d2f4a8e96258be2655
Riya Kaushik <haibara@Riyas-MacBook-Pro.local>
2023-04-10 15:52:30 +0530

    fix: suggested changes [SF-409]

--- src/components/atoms/MenuItem/MenuItem.tsx
+++ src/components/atoms/MenuItem/MenuItem.tsx
@@ -1,44 +1,53 @@
+import { ExternalLinkIcon } from '@heroicons/react/outline';
 import Link from 'next/link';
 import { formatDataCy } from 'src/utils';
 
 export const MenuItem = ({
     text,
     icon,
-    badge,
     link,
 }: {
-    label?: string;
     text: string;
     icon: React.ReactNode;
-    badge?: React.ReactNode;
     link: string;
 }) => {
     return (
         <div
             data-cy={formatDataCy(text)}
-            className='group relative flex w-full cursor-pointer rounded-md p-2 transition focus:outline-none'
+            className='group relative flex w-full cursor-pointer items-center rounded-md p-2 transition hover:bg-horizonBlue focus:outline-none'
         >
-            <Link href={link} className='h-full w-full' passHref>
-                <a href={link} className='h-full w-full' aria-label='Menu Item'>
-                    <div className='group relative flex w-full cursor-pointer items-center'>
+            <Link
+                href={link}
+                className='align-center flex h-full w-full '
+                passHref
+            >
+                <a
+                    href={link}
+                    className='h-full w-full'
+                    aria-label='Menu Item'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                >
+                    <div className=' group relative flex w-full cursor-pointer items-center'>
                         <div className='flex h-10 w-10 items-center justify-center'>
                             {icon}
                         </div>
-                        <p className='typography-caption flex w-[90%] capitalize text-white'>
-                            {text}
-                        </p>
-                        {badge && (
-                            <span className='group relative my-3 flex h-full py-2 align-top'>
-                                <span className='absolute top-0 right-0 -mt-5 transform opacity-0 transition-opacity group-hover:opacity-100'>
-                                    <div className=' my-1 rounded-full py-1'>
-                                        {badge}
-                                    </div>
-                                </span>
+                        <div className='flex w-full justify-between'>
+                            <p className='typography-caption flex w-[90%] capitalize text-white'>
+                                {text}
+                            </p>
+
+                            <span className='absolute right-0 my-1 h-full transform align-top transition-opacity group-hover:opacity-100'>
+                                <ExternalIcon />
                             </span>
-                        )}
+                        </div>
                     </div>
                 </a>
             </Link>
         </div>
     );
 };
+
+const ExternalIcon = () => (
+    <ExternalLinkIcon className='h-4 w-4 text-slateGray' />
+);
