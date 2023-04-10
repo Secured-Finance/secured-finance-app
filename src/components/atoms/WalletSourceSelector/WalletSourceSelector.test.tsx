ee5d2f1cd6727d5cfad73420cef80c74eb7eb382
Arpit Singh <arpit@secured-finance.com>
2023-04-11 03:01:47 +0530

    feat: improve wallet source selector [SF-304]

--- src/components/atoms/WalletSourceSelector/WalletSourceSelector.test.tsx
+++ src/components/atoms/WalletSourceSelector/WalletSourceSelector.test.tsx
@@ -1,9 +1,28 @@
 import { WalletSource } from '@secured-finance/sf-client/dist/secured-finance-client';
 import { composeStories } from '@storybook/testing-react';
+import SFLogoSmall from 'src/assets/img/logo-small.svg';
+import MetamaskIcon from 'src/assets/img/metamask-fox.svg';
 import { fireEvent, render, screen } from 'src/test-utils.js';
+import { CurrencySymbol } from 'src/utils';
+import { WalletSourceOption } from './WalletSourceSelector';
 import * as stories from './WalletSourceSelector.stories';
 
 const { Default } = composeStories(stories);
+
+const walletSourceList: WalletSourceOption[] = [
+    {
+        source: WalletSource.METAMASK,
+        available: 1000,
+        asset: CurrencySymbol.WBTC,
+        iconSVG: MetamaskIcon,
+    },
+    {
+        source: WalletSource.SF_VAULT,
+        available: 0,
+        asset: CurrencySymbol.WBTC,
+        iconSVG: SFLogoSmall,
+    },
+];
 
 describe('WalletSourceSelector component', () => {
     it('should render WalletSourceSelector', () => {
@@ -25,7 +44,7 @@ describe('WalletSourceSelector component', () => {
         expect(screen.getByRole('listbox')).toBeInTheDocument();
     });
 
-    it.skip('should change the button when a dropdown item is selected', () => {
+    it('should change the button when a dropdown item is selected', () => {
         const onChange = jest.fn();
         render(<Default onChange={onChange} />);
 
@@ -34,14 +53,19 @@ describe('WalletSourceSelector component', () => {
 
         fireEvent.click(screen.getByTestId('wallet-source-selector-button'));
         fireEvent.click(screen.getByTestId('option-1'));
-        expect(screen.getByText('Ethereum')).toBeInTheDocument();
-        expect(screen.getByText('120 Ethereum Available')).toBeInTheDocument();
+        expect(screen.getByText('SF Vault')).toBeInTheDocument();
+        expect(screen.getByText('4,000 WBTC')).toBeInTheDocument();
 
         expect(onChange).toBeCalledTimes(2);
-        expect(onChange).toHaveBeenLastCalledWith({
-            available: 120,
-            name: 'Ethereum',
-            symbol: 'ETH',
-        });
+        expect(onChange).toHaveBeenLastCalledWith(WalletSource.SF_VAULT);
+    });
+
+    it('should not render options which have zero balance except metamask', () => {
+        render(<Default optionList={walletSourceList} />);
+        expect(screen.getByText('0xb98b...fd6d')).toBeInTheDocument();
+        expect(screen.getByText('1,000 WBTC')).toBeInTheDocument();
+
+        fireEvent.click(screen.getByTestId('wallet-source-selector-button'));
+        expect(screen.queryByTestId('option-1')).not.toBeInTheDocument();
     });
 });
