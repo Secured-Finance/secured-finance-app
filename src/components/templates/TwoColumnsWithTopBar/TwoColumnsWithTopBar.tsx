c4caa055ce2fc44786dfd889eae32b69bcbf0fc6
Riya Kaushik <haibara@Riyas-MacBook-Pro.local>
2023-04-27 11:34:11 +0530

    fix: fix advanced screen top bar layout [SF-386]

--- src/components/templates/TwoColumnsWithTopBar/TwoColumnsWithTopBar.tsx
+++ src/components/templates/TwoColumnsWithTopBar/TwoColumnsWithTopBar.tsx
@@ -6,7 +6,7 @@ export const TwoColumnsWithTopBar = ({
     children: [React.ReactNode, React.ReactNode];
 }) => {
     return (
-        <div className='h-fit min-w-[720px]'>
+        <div className='h-fit min-w-[820px]'>
             <div className='w-full'>{topBar}</div>
             <div className='flex flex-row justify-between gap-6 pt-4'>
                 <div className='w-1/3'>{children[0]}</div>
