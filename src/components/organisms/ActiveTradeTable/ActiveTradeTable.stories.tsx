c3f763bcf6c368c027e41afb8e565f590981170a
Riya Kaushik <haibara@Riyas-MacBook-Pro.local>
2023-05-02 19:06:15 +0530

    fix: show hours and minutes when maturity is less than 24 hours [SF-339]

--- src/components/organisms/ActiveTradeTable/ActiveTradeTable.stories.tsx
+++ src/components/organisms/ActiveTradeTable/ActiveTradeTable.stories.tsx
@@ -1,9 +1,11 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { BigNumber } from 'ethers';
 import {
     withAssetPrice,
     withWalletProvider,
 } from 'src/../.storybook/decorators';
-import { aggregatedTrades } from 'src/stories/mocks/fixtures';
+import { aggregatedTrades, efilBytes32 } from 'src/stories/mocks/fixtures';
+import { Maturity } from 'src/utils/entities';
 import { ActiveTradeTable } from './ActiveTradeTable';
 
 export default {
@@ -16,6 +18,21 @@ export default {
 } as ComponentMeta<typeof ActiveTradeTable>;
 
 const Template: ComponentStory<typeof ActiveTradeTable> = args => {
+    const now = new Date();
+    const twentyTwoHoursFromNow = new Date(now.getTime() + 22 * 60 * 60 * 1000);
+    const epochTimestamp = Math.floor(twentyTwoHoursFromNow.getTime() / 1000);
+
+    args.data = [
+        ...args.data,
+        {
+            amount: BigNumber.from('500000000000000000000'),
+            currency: efilBytes32,
+            forwardValue: BigNumber.from('500000000000000000000'),
+            maturity: new Maturity(epochTimestamp).toString(),
+            averagePrice: BigNumber.from(9671),
+        },
+    ];
+
     return (
         <div className='px-24'>
             <ActiveTradeTable {...args} />
