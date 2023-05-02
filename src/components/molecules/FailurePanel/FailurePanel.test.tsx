48fd43f060d6df5f555d45210a25be75c3fed6eb
Arpit Singh <arpit@secured-finance.com>
2023-05-02 17:26:13 +0530

    fix: add new designs for error screen [SF-428]

--- src/components/molecules/FailurePanel/FailurePanel.test.tsx
+++ src/components/molecules/FailurePanel/FailurePanel.test.tsx
@@ -1,11 +1,34 @@
 import { composeStories } from '@storybook/testing-react';
-import { render } from 'src/test-utils.js';
+import { fireEvent, render, screen } from 'src/test-utils.js';
 import * as stories from './FailurePanel.stories';
 
-const { Default } = composeStories(stories);
+const { Default, LongErrorMessage } = composeStories(stories);
+
+Object.assign(navigator, {
+    clipboard: {
+        writeText: () => {},
+    },
+});
 
 describe('FailurePanel Component', () => {
     it('should render a FailurePanel', () => {
         render(<Default />);
+        jest.spyOn(navigator.clipboard, 'writeText');
+        expect(screen.getByRole('button')).toBeInTheDocument();
+        const copyButton = screen.getByRole('button');
+        fireEvent.click(copyButton);
+        expect(navigator.clipboard.writeText).toHaveBeenCalledWith(
+            'This is an error.'
+        );
+        expect(screen.getByText('This is an error.')).toBeInTheDocument();
+    });
+
+    it('should render a long message failure panel', () => {
+        render(<LongErrorMessage />);
+        expect(
+            screen.getByText(
+                'This is an example of a Long Error Message. This message should enable vertical scrollbar. This is an example of a Long Error Message. This message should enable vertical scrollbar. This is an example of a Long Error Message. This message should enable vertical scrollbar.'
+            )
+        ).toBeInTheDocument();
     });
 });
