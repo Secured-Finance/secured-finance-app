a5808f16dbff9cbd79ad89dce7be703e43dea92e
NetFreak26 <chirayugarg001999@gmail.com>
2023-05-23 01:22:22 +0530

    fix: requested changes done [SF-309]

--- src/components/organisms/CollateralOrganism/CollateralOrganism.stories.tsx
+++ src/components/organisms/CollateralOrganism/CollateralOrganism.stories.tsx
@@ -1,13 +1,13 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { withWalletProvider } from 'src/../.storybook/decorators';
 import { CollateralOrganism } from './CollateralOrganism';
-import { emptyCollateralBook } from 'src/stories/mocks/fixtures';
+import { collateralBook37 } from 'src/stories/mocks/fixtures';
 
 export default {
     title: 'Organism/CollateralOrganism',
     component: CollateralOrganism,
     args: {
-        collateralBook: emptyCollateralBook,
+        collateralBook: collateralBook37,
     },
     decorators: [withWalletProvider],
 } as ComponentMeta<typeof CollateralOrganism>;
