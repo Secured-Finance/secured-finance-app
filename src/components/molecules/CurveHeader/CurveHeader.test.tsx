ef315111566554a3015c011e15a3c3c3ef962e72
Riya Kaushik <haibara@192.168.1.6>
2023-05-23 00:30:14 +0530

    fix: remove unnecessary async [SF-446]

--- src/components/molecules/CurveHeader/CurveHeader.test.tsx
+++ src/components/molecules/CurveHeader/CurveHeader.test.tsx
@@ -9,7 +9,7 @@ const { Default } = composeStories(stories);
 describe('CurveHeader component', () => {
     const preloadedState = { ...preloadedAssetPrices };
 
-    it('should render CurveHeader', async () => {
+    it('should render CurveHeader', () => {
         render(<Default />);
     });
 
@@ -18,21 +18,21 @@ describe('CurveHeader component', () => {
         expect(screen.getByText('Filecoin')).toBeInTheDocument();
     });
 
-    it('should display the price of the asset', async () => {
+    it('should display the price of the asset', () => {
         render(<Default />, {
             preloadedState,
         });
         expect(screen.getByText('$6.00')).toBeInTheDocument();
     });
 
-    it('should display the change of the asset and round up the second decimal', async () => {
+    it('should display the change of the asset and round up the second decimal', () => {
         render(<Default />, {
             preloadedState,
         });
         expect(screen.getByText('-8.21%')).toBeInTheDocument();
     });
 
-    it('should display Total Volume (Asset) for EFIL and Total Volume (USD) when asset is EFIL', async () => {
+    it('should display Total Volume (Asset) for EFIL and Total Volume (USD) when asset is EFIL', () => {
         render(<Default />, {
             preloadedState,
         });
@@ -40,21 +40,21 @@ describe('CurveHeader component', () => {
         assertAssetVolume('300 EFIL', '$1,800.00');
     });
 
-    it('should display Total Volume (Asset) for USDC and Total Volume (USD) when asset is USDC', async () => {
+    it('should display Total Volume (Asset) for USDC and Total Volume (USD) when asset is USDC', () => {
         render(<Default asset={CurrencySymbol.USDC} />, {
             preloadedState,
         });
         assertAssetVolume('0 USDC', '$0.00');
     });
 
-    it('should display Total Volume (Asset) for ETH and Total Volume (USD) when asset is ETH', async () => {
+    it('should display Total Volume (Asset) for ETH and Total Volume (USD) when asset is ETH', () => {
         render(<Default asset={CurrencySymbol.ETH} />, {
             preloadedState,
         });
         assertAssetVolume('0 ETH', '$0.00');
     });
 
-    it('should display Total Volume (Asset) for WBTC and Total Volume (USD) when asset is WBTC', async () => {
+    it('should display Total Volume (Asset) for WBTC and Total Volume (USD) when asset is WBTC', () => {
         render(<Default asset={CurrencySymbol.WBTC} />, {
             preloadedState,
         });
