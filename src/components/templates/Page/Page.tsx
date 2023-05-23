c4ff83f5487fde83f65af170992e931e85f0a6bf
Reuven Aboulker <reuven.aboulker@gmail.com>
2023-05-23 10:23:25 +0200

    feat: move back to mobile first design [SF-522]

--- src/components/templates/Page/Page.tsx
+++ src/components/templates/Page/Page.tsx
@@ -13,11 +13,11 @@ export const Page = ({
 }) => {
     return (
         <div
-            className='mt-7 flex min-w-[1024px] flex-col gap-6 px-40 mobile:mt-0 mobile:min-w-[375px] mobile:gap-4 mobile:px-0'
+            className='mt-7 flex flex-col gap-4 px-10 desktop:gap-6 desktop:px-40'
             data-testid={name}
         >
-            <div className='flex h-16 min-w-[1024px] items-center justify-between border-b-[0.5px] border-panelStroke mobile:min-w-min mobile:justify-around'>
-                <span className='font-secondary text-lg font-light leading-7 text-white mobile:text-smd'>
+            <div className='flex h-16 items-center justify-around border-b-[0.5px] border-panelStroke tablet:justify-between'>
+                <span className='font-secondary text-smd font-light leading-7 text-white tablet:text-lg'>
                     {title}
                 </span>
                 {titleComponent ? titleComponent : null}
@@ -25,10 +25,7 @@ export const Page = ({
             <div className='flex flex-col gap-6'>
                 {React.Children.map(children, (child, index) => {
                     return (
-                        <div
-                            className='min-w-[1024px] mobile:min-w-min'
-                            key={`page-${name}-${index}`}
-                        >
+                        <div className='' key={`page-${name}-${index}`}>
                             {child}
                         </div>
                     );
