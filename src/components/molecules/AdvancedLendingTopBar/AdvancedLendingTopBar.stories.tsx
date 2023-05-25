db2d386f67a2adebb63826af66b1ca63af6d62c6
NetFreak26 <chirayugarg001999@gmail.com>
2023-05-25 11:06:56 +0530

    fix: requested changes done [SF-405]

--- src/components/molecules/AdvancedLendingTopBar/AdvancedLendingTopBar.stories.tsx
+++ src/components/molecules/AdvancedLendingTopBar/AdvancedLendingTopBar.stories.tsx
@@ -1,11 +1,10 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-import { withMidPrice } from 'src/../.storybook/decorators';
 import { currencyList, maturityOptions } from 'src/stories/mocks/fixtures';
 import { AdvancedLendingTopBar } from '.';
-import { LoanValue, Maturity } from 'src/utils/entities';
+import { LoanValue } from 'src/utils/entities';
 
-const sep24Fixture = new Maturity(1725148800);
 const lastTradePrice = 8000;
+
 export default {
     title: 'Molecules/AdvancedLendingTopBar',
     component: AdvancedLendingTopBar,
@@ -20,8 +19,11 @@ export default {
             label: maturityOptions[0].label,
             value: maturityOptions[0].value.toString(),
         },
+        lastTradeLoan: LoanValue.fromPrice(
+            lastTradePrice,
+            maturityOptions[0].value.toNumber()
+        ),
     },
-    decorators: [withMidPrice],
 } as ComponentMeta<typeof AdvancedLendingTopBar>;
 
 const Template: ComponentStory<typeof AdvancedLendingTopBar> = args => (
@@ -33,9 +35,4 @@ export const Default = Template.bind({});
 export const Values = Template.bind({});
 Values.args = {
     values: [26.16, 24.2, 894, 10000000, '23000'],
-};
-
-export const LastTradePrice = Template.bind({});
-LastTradePrice.args = {
-    lastTradeLoan: LoanValue.fromPrice(lastTradePrice, sep24Fixture.toNumber()),
 };
