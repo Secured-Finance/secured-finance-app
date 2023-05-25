4d0540b315f7e41ec6a24962b721e0284d8fcb67
Reuven Aboulker <reuven.aboulker@gmail.com>
2023-05-25 10:45:34 +0200

    feat: add styling as argument in horizontal tab [SF-522]

--- src/components/molecules/HorizontalTab/HorizontalTab.stories.tsx
+++ src/components/molecules/HorizontalTab/HorizontalTab.stories.tsx
@@ -4,12 +4,14 @@ import { HorizontalTab } from './HorizontalTab';
 export default {
     title: 'Molecules/HorizontalTab',
     component: HorizontalTab,
-    args: {},
+    args: {
+        tabTitles: ['Active Contracts', 'Trade History'],
+    },
 } as ComponentMeta<typeof HorizontalTab>;
 
-const Template: ComponentStory<typeof HorizontalTab> = () => (
+const Template: ComponentStory<typeof HorizontalTab> = args => (
     <div className='h-[400px] w-[600px] text-white-80'>
-        <HorizontalTab tabTitles={['Active Contracts', 'Trade History']}>
+        <HorizontalTab {...args}>
             <div>This is a Great Tab Content</div>
             <div className='py-12'>This is the content of the second tab</div>
         </HorizontalTab>
@@ -17,3 +19,7 @@ const Template: ComponentStory<typeof HorizontalTab> = () => (
 );
 
 export const Default = Template.bind({});
+export const WithStyling = Template.bind({});
+WithStyling.args = {
+    tabTitles: [['Test', 'px-8']],
+};
