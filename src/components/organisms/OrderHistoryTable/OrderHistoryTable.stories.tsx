fd8c190a4dcf066f6c05b9cc4a4652d271e03214
Riya Kaushik <haibara@192.168.1.8>
2023-05-31 21:15:51 +0530

    fix: fix stories and assetInformation [SF-524]

--- src/components/organisms/OrderHistoryTable/OrderHistoryTable.stories.tsx
+++ src/components/organisms/OrderHistoryTable/OrderHistoryTable.stories.tsx
@@ -1,3 +1,4 @@
+import { RESPONSIVE_PARAMETERS, VIEWPORTS } from '.storybook/constants';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { withAssetPrice } from 'src/../.storybook/decorators';
 import { orderHistoryList } from 'src/stories/mocks/fixtures';
@@ -8,6 +9,12 @@ export default {
     component: OrderHistoryTable,
     args: {
         data: orderHistoryList,
+    },
+    parameters: {
+        ...RESPONSIVE_PARAMETERS,
+        chromatic: {
+            viewports: [VIEWPORTS.MOBILE, VIEWPORTS.TABLET, VIEWPORTS.LAPTOP],
+        },
     },
     decorators: [withAssetPrice],
 } as ComponentMeta<typeof OrderHistoryTable>;
