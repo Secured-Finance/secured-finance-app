8663c648bb6c5e0f29e63cbeb3e6ab63ff428161
Riya Kaushik <haibara@192.168.1.5>
2023-06-01 18:21:42 +0530

    fix: fix horizontal tab and my transactions table [SF-524]

--- src/components/molecules/HorizontalTab/HorizontalTab.tsx
+++ src/components/molecules/HorizontalTab/HorizontalTab.tsx
@@ -1,7 +1,7 @@
-import { Tab as HeadlessTab } from '@headlessui/react';
+import { Menu, Tab as HeadlessTab } from '@headlessui/react';
 import classNames from 'classnames';
 import React, { Children, useState } from 'react';
-import { GradientBox } from 'src/components/atoms';
+import { Separator } from 'src/components/atoms';
 
 const TitleChip = ({
     title,
@@ -42,29 +42,49 @@ export const HorizontalTab = ({
                 onChange={setSelectedIndex}
             >
                 <HeadlessTab.List className='justify-start border-b border-white-10 tablet:py-3'>
-                    <div className='h-16 w-full border-white-10 bg-cardBackground/60 shadow-tab  tablet:hidden'>
-                        <GradientBox shape='rectangle'>
-                            <select
-                                value={selectedIndex}
-                                onChange={event =>
-                                    setSelectedIndex(Number(event.target.value))
-                                }
-                                className='mx-2 my-3 h-10 w-[95%] justify-start rounded-xl  bg-black-30 p-2 text-neutral-8 focus:outline-none'
-                            >
-                                {tabTitles.map((title, index) => {
-                                    return (
-                                        <option
-                                            key={title}
-                                            value={index}
-                                            className='rounded-xl bg-black-30 text-neutral-8 '
-                                            data-testid={`${title}-mobile`}
-                                        >
-                                            {title}
-                                        </option>
-                                    );
-                                })}
-                            </select>
-                        </GradientBox>
+                    <div className='w-full tablet:hidden'>
+                        <Menu as='div' className='relative'>
+                            <Menu.Button className='mx-2 my-3 h-10 w-[95%] justify-start rounded-xl  bg-black-30 p-2 text-neutral-8 focus:outline-none'>
+                                {tabTitles[selectedIndex]}
+                            </Menu.Button>
+                            <Menu.Items className='z-15 absolute mt-0.5 flex w-full flex-col rounded-lg bg-gunMetal p-2 shadow-sm'>
+                                {tabTitles.map((title, index) => (
+                                    <Menu.Item key={title}>
+                                        {({ active }) => (
+                                            <div>
+                                                <div
+                                                    className={classNames(
+                                                        'flex flex-row items-center justify-start space-x-4 rounded-lg p-2 text-white-80',
+                                                        {
+                                                            'bg-horizonBlue':
+                                                                active,
+                                                        }
+                                                    )}
+                                                >
+                                                    <button
+                                                        onClick={() =>
+                                                            setSelectedIndex(
+                                                                index
+                                                            )
+                                                        }
+                                                        className='w-full'
+                                                        data-testid={`${title}-mobile`}
+                                                    >
+                                                        {title}
+                                                    </button>
+                                                </div>
+                                                {index !==
+                                                tabTitles.length - 1 ? (
+                                                    <div className='py-2'>
+                                                        <Separator />
+                                                    </div>
+                                                ) : null}
+                                            </div>
+                                        )}
+                                    </Menu.Item>
+                                ))}
+                            </Menu.Items>
+                        </Menu>
                     </div>
                     <div className='hidden tablet:block'>
                         {tabTitles.map((title, index) => {
