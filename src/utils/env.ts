d0fd44d3f5c4dc0d20174e0014c878ec08d52b43
Reuven Aboulker <reuven.aboulker@gmail.com>
2023-06-06 12:01:52 +0200

    build: silently default to false for use commit hash [SF-548]

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -68,8 +68,6 @@ export const getUseCommitHash = () => {
     const NEXT_PUBLIC_USE_COMMIT_HASH = process.env.NEXT_PUBLIC_USE_COMMIT_HASH;
 
     if (!NEXT_PUBLIC_USE_COMMIT_HASH) {
-        // eslint-disable-next-line no-console
-        console.warn('USE_COMMIT_HASH is not set, defaulting to false');
         return false;
     }
 
