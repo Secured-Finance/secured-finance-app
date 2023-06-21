30feb1786e0da35eb3bf458f9ba00b89c6379512
Reuven Aboulker <reuven.aboulker@gmail.com>
2023-06-21 19:34:53 +0200

    test: fix mobile view [SF-553]

--- src/components/molecules/HorizontalTab/HorizontalTab.stories.tsx
+++ src/components/molecules/HorizontalTab/HorizontalTab.stories.tsx
@@ -37,5 +37,5 @@ MobileViewport.parameters = {
 };
 MobileViewport.play = async ({ canvasElement }) => {
     const canvas = within(canvasElement);
-    canvas.getByRole('button', { hidden: true }).click();
+    canvas.getByRole('button').click();
 };
