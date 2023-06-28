11e54512ad71d75d569a7dcfee5dddb1712e0d6b
Arpit Singh <arpit@secured-finance.com>
2023-06-28 22:12:14 +0530

    refactor: refactor to accomodate children and css fixes [SF-561]

--- src/components/atoms/HorizontalListItem/HorizontalListItem.tsx
+++ src/components/atoms/HorizontalListItem/HorizontalListItem.tsx
@@ -2,13 +2,21 @@ export const HorizontalListItem = ({
     label,
     value,
 }: {
-    label: string;
-    value: string;
+    label: React.ReactNode;
+    value: React.ReactNode;
 }) => {
     return (
         <div className='typography-caption flex justify-between'>
-            <span className='text-planetaryPurple'>{label}</span>
-            <span className='text-neutral-8'>{value}</span>
+            {typeof label === 'string' ? (
+                <span className='text-planetaryPurple'>{label}</span>
+            ) : (
+                label
+            )}
+            {typeof value === 'string' ? (
+                <span className='text-neutral-8'>{value}</span>
+            ) : (
+                value
+            )}
         </div>
     );
 };
