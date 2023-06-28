31a6d2b8f6ccbb3d85b4b21dad3457e443500e88
Arpit Singh <arpit@secured-finance.com>
2023-06-28 21:17:52 +0530

    fix: small css fixes [SF-561]

--- src/components/atoms/CollateralManagementConciseTab/CollateralManagementConciseTab.test.tsx
+++ src/components/atoms/CollateralManagementConciseTab/CollateralManagementConciseTab.test.tsx
@@ -27,7 +27,7 @@ describe('CollateralManagementConciseTab component', () => {
         render(<Default collateralCoverage={0} />);
         expect(screen.getByText('Threshold: 80%')).toBeInTheDocument();
         expect(screen.getByText('N/A')).toBeInTheDocument();
-        expect(screen.getByText('N/A')).toHaveClass('text-white');
+        expect(screen.getByText('N/A')).toHaveClass('text-progressBarStart');
 
         render(<Default collateralCoverage={30} />);
         expect(screen.getByText('Threshold: 50%')).toBeInTheDocument();
