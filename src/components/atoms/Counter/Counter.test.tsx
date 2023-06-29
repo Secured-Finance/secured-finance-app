a1365c20febbbd662373c9b66d39a96366a51443
Reuven Aboulker <reuven.aboulker@gmail.com>
2023-06-29 15:51:28 +0100

    test: speed up counter tests [SF-601]

--- src/components/atoms/Counter/Counter.test.tsx
+++ src/components/atoms/Counter/Counter.test.tsx
@@ -1,5 +1,5 @@
 import { composeStories } from '@storybook/testing-react';
-import { render, screen, waitFor } from 'src/test-utils.js';
+import { render, screen } from 'src/test-utils.js';
 import * as stories from './Counter.stories';
 
 const { Default, OnlyNumber, OnlyPrefix, OnlySuffix } = composeStories(stories);
@@ -7,41 +7,29 @@ const { Default, OnlyNumber, OnlyPrefix, OnlySuffix } = composeStories(stories);
 describe('test Counter component', () => {
     it('should render default counter', async () => {
         render(<Default />);
-        await waitFor(
-            () => expect(screen.getByText('$12.23M')).toBeInTheDocument(),
-            {
-                timeout: 3000,
-            }
-        );
+        expect(
+            await screen.findByText('$12.23M', undefined, { timeout: 3000 })
+        ).toBeInTheDocument();
     });
 
     it('should render only number counter', async () => {
         render(<OnlyNumber />);
-        await waitFor(
-            () => expect(screen.getByText('14.23')).toBeInTheDocument(),
-            {
-                timeout: 3000,
-            }
-        );
+        expect(
+            await screen.findByText('14.23', undefined, { timeout: 3000 })
+        ).toBeInTheDocument();
     });
 
     it('should render only prefix counter', async () => {
         render(<OnlyPrefix />);
-        await waitFor(
-            () => expect(screen.getByText('$4,000.91')).toBeInTheDocument(),
-            {
-                timeout: 3000,
-            }
-        );
+        expect(
+            await screen.findByText('$4,000.91', undefined, { timeout: 3000 })
+        ).toBeInTheDocument();
     });
 
     it('should render only suffix counter', async () => {
         render(<OnlySuffix />);
-        await waitFor(
-            () => expect(screen.getByText('40.20K')).toBeInTheDocument(),
-            {
-                timeout: 3000,
-            }
-        );
+        expect(
+            await screen.findByText('40.20K', undefined, { timeout: 3000 })
+        ).toBeInTheDocument();
     });
 });
