25d9eabb78aa5a8191235cb06e9fc22b493d21f9
Riya <kaushik.riya193@gmail.com>
2023-07-03 20:17:52 +0530

    test: add test and fix description [SF-597]

--- src/components/atoms/ErrorInfo/ErrorInfo.test.tsx
+++ src/components/atoms/ErrorInfo/ErrorInfo.test.tsx
@@ -1,11 +1,17 @@
 import { composeStories } from '@storybook/testing-react';
-import { render } from 'src/test-utils.js';
+import { render, screen } from 'src/test-utils.js';
 import * as stories from './ErrorInfo.stories';
 
 const { Default } = composeStories(stories);
 
-describe('Chip Component', () => {
+describe('ErrorInfo Component', () => {
     it('should render an error text', () => {
         render(<Default />);
+        expect(screen.getByText('This is an error')).toBeInTheDocument();
+    });
+
+    it('should not render an error text', () => {
+        render(<Default showError={false} />);
+        expect(screen.queryByText('This is an error')).not.toBeInTheDocument();
     });
 });
