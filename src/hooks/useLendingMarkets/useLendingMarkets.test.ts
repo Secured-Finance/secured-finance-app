99a23df12377f19a5026e0b54c813f2877043400
Reuven Aboulker <reuven.aboulker@gmail.com>
2023-07-07 13:46:39 +0200

    feat: subscribe to new block instead of timer [SF-591]

--- src/hooks/useLendingMarkets/useLendingMarkets.test.ts
+++ src/hooks/useLendingMarkets/useLendingMarkets.test.ts
@@ -1,3 +1,5 @@
+import { SecuredFinanceClient } from '@secured-finance/sf-client';
+import { mockUseSF } from 'src/stories/mocks/useSFMock';
 import { renderHook } from 'src/test-utils';
 import { CurrencySymbol } from 'src/utils';
 import timemachine from 'timemachine';
@@ -10,27 +12,58 @@ beforeAll(() => {
     });
 });
 
+const mock = mockUseSF();
+jest.mock('src/hooks/useSecuredFinance', () => () => mock);
+
 describe('useLendingMarkets', () => {
-    it('should return the initial state of the store if the client is not defined', async () => {
-        const { store } = renderHook(() => useLendingMarkets(undefined));
+    it('should return a function to fetch the lending markets', async () => {
+        const { result } = renderHook(() => useLendingMarkets());
+        expect(result.current.fetchLendingMarkets).toBeInstanceOf(Function);
+    });
+
+    it('should insert the lending markets in the store', async () => {
+        const { result, store } = renderHook(() => useLendingMarkets());
+        const fetchLendingMarkets = result.current.fetchLendingMarkets;
+        await fetchLendingMarkets(
+            CurrencySymbol.EFIL,
+            mock as unknown as SecuredFinanceClient
+        );
         expect(
-            Object.keys(store.getState().availableContracts.lendingMarkets)
-        ).toHaveLength(4);
+            Object.keys(
+                store.getState().availableContracts.lendingMarkets[
+                    CurrencySymbol.EFIL
+                ]
+            )
+        ).toHaveLength(2);
         expect(
             store.getState().availableContracts.lendingMarkets[
-                CurrencySymbol.ETH
+                CurrencySymbol.EFIL
             ]
         ).toEqual({
-            0: {
+            1000: {
+                isActive: true,
+                maturity: 1000,
+                name: 'ETH-1000',
+                utcOpeningDate: 1620000000,
+                midUnitPrice: 100,
+                preOpenDate: 1447200000,
+                openingUnitPrice: 9686,
+                isReady: true,
+                isOpened: true,
+                isMatured: false,
+                isPreOrderPeriod: false,
+                isItayosePeriod: false,
+            },
+            2000: {
                 isActive: false,
-                maturity: 0,
-                name: 'EMPTY',
-                utcOpeningDate: 0,
-                midUnitPrice: 0,
-                preOpenDate: 0,
-                openingUnitPrice: 0,
-                isReady: false,
-                isOpened: false,
+                maturity: 2000,
+                name: 'ETH-2000',
+                utcOpeningDate: 1720000000,
+                midUnitPrice: 100,
+                preOpenDate: 1547200000,
+                openingUnitPrice: 9786,
+                isReady: true,
+                isOpened: true,
                 isMatured: false,
                 isPreOrderPeriod: false,
                 isItayosePeriod: false,
