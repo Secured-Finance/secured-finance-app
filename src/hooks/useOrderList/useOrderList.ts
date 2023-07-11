dce6ea0f43d792b30b9d8fe15ff27eb5466e2fea
NetFreak26 <chirayugarg001999@gmail.com>
2023-07-11 15:16:21 +0530

    fix: issue fixed for portfolio management data exist after wallet is disconnected [SF-650]

--- src/hooks/useOrderList/useOrderList.ts
+++ src/hooks/useOrderList/useOrderList.ts
@@ -41,6 +41,7 @@ export const useOrderList = (account: string | null) => {
 
     const fetchOrdersList = useCallback(async () => {
         if (!securedFinance || !account) {
+            setOrderList(emptyOrderList);
             return;
         }
 
