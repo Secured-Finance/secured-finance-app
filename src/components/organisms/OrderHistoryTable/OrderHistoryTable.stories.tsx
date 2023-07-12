4c7d75701d8d6af51634c1721cda967b600066c9
Riya <kaushik.riya193@gmail.com>
2023-07-12 23:25:16 +0530

    feat: add pagination to order history table and add types [SF-632]

--- src/components/organisms/OrderHistoryTable/OrderHistoryTable.stories.tsx
+++ src/components/organisms/OrderHistoryTable/OrderHistoryTable.stories.tsx
@@ -1,7 +1,15 @@
 import { RESPONSIVE_PARAMETERS, VIEWPORTS } from '.storybook/constants';
+import { OrderStatus } from '@secured-finance/sf-graph-client/dist/graphclients/development/.graphclient';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { BigNumber, utils } from 'ethers';
+import { useState } from 'react';
 import { withAssetPrice } from 'src/../.storybook/decorators';
-import { orderHistoryList } from 'src/stories/mocks/fixtures';
+import {
+    dec22Fixture,
+    efilBytes32,
+    orderHistoryList,
+} from 'src/stories/mocks/fixtures';
+import { OrderList } from 'src/types';
 import { OrderHistoryTable } from './OrderHistoryTable';
 
 export default {
@@ -23,4 +31,43 @@ const Template: ComponentStory<typeof OrderHistoryTable> = args => (
     <OrderHistoryTable {...args} />
 );
 
+const PaginatedTemplate: ComponentStory<typeof OrderHistoryTable> = args => {
+    const [data, setData] = useState<OrderList>(args.data);
+
+    return (
+        <OrderHistoryTable
+            {...args}
+            data={data}
+            pagination={{
+                totalData: 100,
+                getMoreData: () => {
+                    const newData = Array(20)
+                        .fill(null)
+                        .map((_, index) => ({
+                            orderId: index,
+                            currency: efilBytes32,
+                            side: 1,
+                            maturity: BigNumber.from(dec22Fixture.toString()),
+                            unitPrice: BigNumber.from('9800'),
+                            filledAmount: BigNumber.from('0'),
+                            amount: BigNumber.from('1000000000000000000000'),
+                            status: 'Open' as OrderStatus,
+                            createdAt: BigNumber.from('1'),
+                            txHash: utils.formatBytes32String('hash'),
+                            lendingMarket: {
+                                id: '1',
+                                isActive: true,
+                            },
+                        }));
+
+                    const updatedData = [...data, ...newData];
+                    setData(updatedData);
+                },
+                containerHeight: 400,
+            }}
+        />
+    );
+};
+
 export const Default = Template.bind({});
+export const PaginatedOrderHistoryTable = PaginatedTemplate.bind({});
