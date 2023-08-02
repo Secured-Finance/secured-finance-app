42e3a31ae6810db59eaeb98f6ac5aa35f09250c8
Reuven Aboulker <reuven.aboulker@gmail.com>
2023-08-02 19:00:42 +0100

    feat: add gitbook links [SF-687]

--- src/components/molecules/MenuPopover/MenuPopover.test.tsx
+++ src/components/molecules/MenuPopover/MenuPopover.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen, waitFor } from 'src/test-utils.js';
+import { fireEvent, render, screen } from 'src/test-utils.js';
 
 import { composeStories } from '@storybook/testing-react';
 import * as stories from './MenuPopover.stories';
@@ -13,21 +13,9 @@ describe('MenuPopover component', () => {
     });
     it('should render when clicked on the More... button', async () => {
         render(<Default />);
-
-        expect(screen.queryByText('Official Site')).toBeNull();
-        expect(screen.queryByText('Documentation')).toBeNull();
-        expect(screen.queryByText('Follow us on Twitter')).toBeNull();
-        expect(screen.queryByText('Join us on Discord')).toBeNull();
+        expect(screen.queryByRole('menu')).not.toBeInTheDocument();
         fireEvent.click(screen.getByRole('button'));
-        await waitFor(() => {
-            expect(screen.queryByText('Official Site')).toBeInTheDocument();
-            expect(screen.queryByText('Documentation')).toBeInTheDocument();
-            expect(
-                screen.queryByText('Follow us on Twitter')
-            ).toBeInTheDocument();
-            expect(
-                screen.queryByText('Join us on Discord')
-            ).toBeInTheDocument();
-        });
+        expect(await screen.findByRole('menu')).toBeInTheDocument();
+        expect(screen.queryAllByRole('menuitem')).toHaveLength(5);
     });
 });
