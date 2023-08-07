d8712fe4cd07df4869dcf988769962d6b0aeda0d
Arpit Singh <arpit@secured-finance.com>
2023-08-07 20:12:04 +0530

    feat: add use currencies for orders hook [SF-678]

---
+++ src/hooks/useCurrenciesForOrders/useCurrenciesForOrders.ts
@@ -0,0 +1,30 @@
+import { Currency } from '@secured-finance/sf-core';
+import { useQuery } from '@tanstack/react-query';
+import useSF from 'src/hooks/useSecuredFinance';
+import { hexToCurrencySymbol, toCurrency } from 'src/utils';
+
+export const useCurrenciesForOrders = (account: string) => {
+    const securedFinance = useSF();
+
+    return useQuery({
+        queryKey: ['getUsedCurrenciesForOrders', account],
+        queryFn: async () => {
+            const currencies = await securedFinance?.getUsedCurrenciesForOrders(
+                account
+            );
+            return currencies ?? [];
+        },
+        initialData: [],
+        select: currencies =>
+            currencies
+                .map(ccy => {
+                    const symbol = hexToCurrencySymbol(ccy);
+                    const convertedCurrency = symbol
+                        ? toCurrency(symbol)
+                        : null;
+                    return convertedCurrency;
+                })
+                .filter((ccy): ccy is Currency => ccy !== null),
+        enabled: !!securedFinance,
+    });
+};
