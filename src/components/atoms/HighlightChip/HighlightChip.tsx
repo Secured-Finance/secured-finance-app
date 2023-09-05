7ecd996dbe634aa5e0470b5f1259080ad5dce06b
Riya <kaushik.riya193@gmail.com>
2023-09-05 23:43:58 +0530

    fix: minor fix [SF-702]

--- src/components/atoms/HighlightChip/HighlightChip.tsx
+++ src/components/atoms/HighlightChip/HighlightChip.tsx
@@ -9,11 +9,11 @@ export const HighlightChip = ({
 }) => (
     <div
         className={classNames(
-            'flex items-center justify-center rounded-3xl bg-starBlue text-neutral-8',
+            'flex items-center justify-center rounded-3xl bg-starBlue ',
             {
-                'typography-dropdown-selection-label h-5 w-10 font-semibold':
+                'typography-dropdown-selection-label h-5 w-10 font-semibold text-white':
                     size === 'large',
-                'typography-caption-3 h-4 w-8': size === 'small',
+                'typography-caption-3 h-4 w-8 text-neutral-8': size === 'small',
             }
         )}
     >
