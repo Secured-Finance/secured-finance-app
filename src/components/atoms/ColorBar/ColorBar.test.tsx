a0d89765a22c43467f27cb7b7e2f8cfda4624950
Reuven Aboulker <reuven.aboulker@gmail.com>
2023-09-20 14:33:59 +0100

    fix: use max amount as reference for color bar [SF-767]

--- src/components/atoms/ColorBar/ColorBar.test.tsx
+++ src/components/atoms/ColorBar/ColorBar.test.tsx
@@ -10,11 +10,11 @@ describe('ColorBar Component', () => {
         render(<Default />);
     });
 
-    it('should have a width of at least 20% when value is not null', () => {
+    it('should have a width of at least 5% when value is not null', () => {
         render(
             <Default value={BigNumber.from(1)} total={BigNumber.from(1000)} />
         );
-        expect(screen.getByTestId('color-bar')).toHaveStyle('width: 20%');
+        expect(screen.getByTestId('color-bar')).toHaveStyle('width: 5%');
     });
 
     it('should have a width of 2px when value is zero', () => {
@@ -24,10 +24,17 @@ describe('ColorBar Component', () => {
         expect(screen.getByTestId('color-bar')).toHaveStyle('width: 2px');
     });
 
-    it('should have a width of 300% when value is equal to total', () => {
+    it('should have a width of 308% when value is equal to total', () => {
         render(
             <Default value={BigNumber.from(100)} total={BigNumber.from(100)} />
         );
-        expect(screen.getByTestId('color-bar')).toHaveStyle('width: 300%');
+        expect(screen.getByTestId('color-bar')).toHaveStyle('width: 308%');
+    });
+
+    it('should have a width of 5% when value is 1 and total is 1000', () => {
+        render(
+            <Default value={BigNumber.from(1)} total={BigNumber.from(1000)} />
+        );
+        expect(screen.getByTestId('color-bar')).toHaveStyle('width: 5%');
     });
 });
