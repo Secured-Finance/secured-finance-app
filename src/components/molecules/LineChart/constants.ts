083e655372fa52875043cfd6f5c1c10f1708ce2c
Arpit Singh <arpit@secured-finance.com>
2023-09-20 18:26:35 +0530

    fix: ticks now have min and max one decimal place [SF-774]

--- src/components/molecules/LineChart/constants.ts
+++ src/components/molecules/LineChart/constants.ts
@@ -134,7 +134,7 @@ export const options: ChartOptions<'line'> = {
             display: true,
             ticks: {
                 callback: function (value: string | number) {
-                    return percentFormat(Number(value));
+                    return percentFormat(Number(value), 100, 1, 1);
                 },
                 color: 'rgba(255, 255, 255, 0.6)',
                 font: {
