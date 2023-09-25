0b2bb25b283efe619128af50c49afb4a012bd0d6
Riya <kaushik.riya193@gmail.com>
2023-09-25 18:01:17 +0530

    fix: make active position table scrollable [SF-790]

--- src/components/organisms/ActiveTradeTable/ActiveTradeTable.tsx
+++ src/components/organisms/ActiveTradeTable/ActiveTradeTable.tsx
@@ -185,6 +185,11 @@ export const ActiveTradeTable = ({ data }: { data: Position[] }) => {
                 options={{
                     name: 'active-trade-table',
                     stickyColumns: new Set<number>([6]),
+                    pagination: {
+                        containerHeight: 150,
+                        getMoreData: () => {},
+                        totalData: data.length,
+                    },
                 }}
             />
             <div className='typography-dropdown-selection-label mt-16 w-full rounded-xl bg-cardBackground/60 text-justify text-secondary7 '>
