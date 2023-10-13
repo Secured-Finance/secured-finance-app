8dd050b1f8f34695351b2ab25b135565a4568afc
Reuven Aboulker <reuven.aboulker@gmail.com>
2023-10-13 09:35:20 +0100

    fix: use right version of smart contract [SF-585]

--- src/hooks/useCurrencyDelistedStatus/useCurrencyDelistedStatus.ts
+++ src/hooks/useCurrencyDelistedStatus/useCurrencyDelistedStatus.ts
@@ -1,4 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
+import { useMemo } from 'react';
 import { QueryKeys } from 'src/hooks/queries';
 import useSF from 'src/hooks/useSecuredFinance';
 import { CurrencySymbol, getCurrencyMapAsList, toCurrency } from 'src/utils';
@@ -12,11 +13,14 @@ export const defaultDelistedStatusMap: Record<CurrencySymbol, boolean> = {
 
 export const useCurrencyDelistedStatus = () => {
     const securedFinance = useSF();
+    const currencies = useMemo(
+        () => getCurrencyMapAsList().map(c => c.symbol),
+        []
+    );
 
     return useQuery({
         queryKey: [QueryKeys.CURRENCY_EXISTS],
         queryFn: async () => {
-            const currencies = getCurrencyMapAsList().map(c => c.symbol);
             const currencyExistList = await Promise.all(
                 currencies.map(symbol =>
                     securedFinance?.currencyExists(toCurrency(symbol))
