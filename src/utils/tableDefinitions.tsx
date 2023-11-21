7e80092dc0c98c153752bfd7d43dce60adeefde6
Reuven Aboulker <reuven.aboulker@gmail.com>
2023-11-21 16:41:31 +0000

    feat: clean up table design [SF-877]

--- src/utils/tableDefinitions.tsx
+++ src/utils/tableDefinitions.tsx
@@ -334,7 +334,7 @@ export const contractColumnDefinition = <
                     : OrderSide.LEND;
             return (
                 <div
-                    className={classNames('flex', {
+                    className={classNames('flex px-3 tablet:px-0', {
                         'justify-start': alignCell === 'left',
                         'justify-center': alignCell === 'center',
                         'justify-end': alignCell === 'right',
