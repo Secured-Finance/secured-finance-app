09e41c548acf5f908a67ad84e328f1bd18e41349
Reuven Aboulker <reuven.aboulker@gmail.com>
2023-11-23 18:35:23 +0000

    fix: fix length of tab [SF-877]

--- src/components/molecules/Tab/Tab.tsx
+++ src/components/molecules/Tab/Tab.tsx
@@ -26,38 +26,36 @@ export const Tab: React.FC<TabProps> = ({ tabDataArray, children }) => {
     const util = tabDataArray[selectedIndex].util;
 
     return (
-        <div className='h-full w-full min-w-fit rounded-b-2xl border border-white-10 bg-cardBackground/60 shadow-tab'>
-            <HeadlessTab.Group
-                selectedIndex={selectedIndex}
-                onChange={setSelectedIndex}
-            >
-                <div className='grid w-full grid-cols-1 border-b border-white-10 tablet:grid-cols-2'>
-                    <HeadlessTab.List className='col-span-1 flex h-[60px] w-full'>
-                        {tabDataArray.map((tabData, index) => {
-                            return (
-                                <HeadlessTab
-                                    key={tabData.text}
-                                    className='h-full w-full focus:outline-none tablet:w-fit'
-                                    disabled={tabData.disabled}
-                                    data-testid={tabData.text}
-                                >
-                                    <NavTab
-                                        text={tabData.text}
-                                        active={selectedIndex === index}
-                                        highlight={tabData.highlight}
-                                    ></NavTab>
-                                </HeadlessTab>
-                            );
-                        })}
-                    </HeadlessTab.List>
-                    {util && (
-                        <div className='col-span-1 h-full w-full'>{util}</div>
-                    )}
-                </div>
-                <HeadlessTab.Panels className='h-full min-h-[25vh] rounded-b-2xl bg-[#161E2E] pb-2'>
-                    {arrayChildren[selectedIndex]}
-                </HeadlessTab.Panels>
-            </HeadlessTab.Group>
-        </div>
+        <HeadlessTab.Group
+            selectedIndex={selectedIndex}
+            onChange={setSelectedIndex}
+            as='div'
+            className='h-full w-full rounded-b-2xl border border-white-10 bg-[#161E2E] shadow-tab '
+        >
+            <div className='grid w-full grid-cols-1 border-b border-white-10 bg-cardBackground/60 tablet:grid-cols-2'>
+                <HeadlessTab.List className='col-span-1 flex h-[60px] w-full'>
+                    {tabDataArray.map((tabData, index) => {
+                        return (
+                            <HeadlessTab
+                                key={tabData.text}
+                                className='h-full w-full focus:outline-none tablet:w-fit'
+                                disabled={tabData.disabled}
+                                data-testid={tabData.text}
+                            >
+                                <NavTab
+                                    text={tabData.text}
+                                    active={selectedIndex === index}
+                                    highlight={tabData.highlight}
+                                ></NavTab>
+                            </HeadlessTab>
+                        );
+                    })}
+                </HeadlessTab.List>
+                {util && <div className='col-span-1 h-full w-full'>{util}</div>}
+            </div>
+            <HeadlessTab.Panels className='h-full min-h-[25vh] rounded-b-2xl pb-2'>
+                {arrayChildren[selectedIndex]}
+            </HeadlessTab.Panels>
+        </HeadlessTab.Group>
     );
 };
