a9a9af91ec65e93f4792cb4c6dd50e1353667172
Reuven Aboulker <reuven.aboulker@gmail.com>
2023-11-25 19:03:02 +0000

    fix: fix moving layout [SF-877]

--- src/components/templates/TwoColumns/TwoColumns.test.tsx
+++ src/components/templates/TwoColumns/TwoColumns.test.tsx
@@ -18,20 +18,20 @@ describe('TwoColumns Component', () => {
     it('should display the two columns with the first one being the bigger one', () => {
         render(<Default />);
         expect(screen.getByText('Column 1').parentElement).toHaveClass(
-            'w-full laptop:w-[70%]'
+            'w-full laptop:w-70%-gap-3'
         );
         expect(screen.getByText('Column 2').parentElement).toHaveClass(
-            'w-full laptop:w-[30%]'
+            'w-full laptop:w-30%-gap-3'
         );
     });
 
     it('should display the two columns with the first one being the smaller one when narrowFirstColumn is true', () => {
         render(<Default narrowFirstColumn />);
         expect(screen.getByText('Column 1').parentElement).toHaveClass(
-            'w-full laptop:w-[30%]'
+            'w-full laptop:w-30%-gap-3'
         );
         expect(screen.getByText('Column 2').parentElement).toHaveClass(
-            'w-full laptop:w-[70%]'
+            'w-full laptop:w-70%-gap-3'
         );
     });
 });
