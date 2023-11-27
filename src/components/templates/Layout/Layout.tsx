57cf79b542220572c133631c2e64e1fdd81dbeb9
andy00614 <33732843@qq.com>
2023-11-27 19:56:18 +0800

    fix: navbar to top of the page to maintain visibility on scroll [SF-918]

--- src/components/templates/Layout/Layout.tsx
+++ src/components/templates/Layout/Layout.tsx
@@ -1,3 +1,8 @@
+import classNames from 'classnames';
+import { useSelector } from 'react-redux';
+import { RootState } from 'src/store/types';
+import { getMainnetChainId } from 'src/utils';
+
 export const Layout = ({
     navBar,
     children,
@@ -7,14 +12,30 @@ export const Layout = ({
     children: React.ReactNode;
     footer: React.ReactNode;
 }) => {
+    const chainError = useSelector(
+        (state: RootState) => state.blockchain.chainError
+    );
+    const currentChainId = useSelector(
+        (state: RootState) => state.blockchain.chainId
+    );
+    const mainnetChainId = getMainnetChainId();
+    const isShowHeaderMessage =
+        currentChainId && (chainError || currentChainId !== mainnetChainId);
+    // HeaderMessage is 22px,after calculation, the top margin of the main section is 7.875rem with the HeaderMessage, and 6.5rem without the HeaderMessage.
+    const marginTop = isShowHeaderMessage ? 'mt-header-top' : 'mt-26';
     return (
         <div
             className='flex h-screen w-screen flex-col justify-between gap-8'
             data-testid='wrapper-div'
         >
             <div className='w-full'>
-                <header className='w-full'>{navBar}</header>
-                <main className='w-full'>{children}</main>
+                <header className='fixed top-0 z-50 w-full bg-universeBlue'>
+                    {navBar}
+                </header>
+                {/* The header has a height of h-20, and the main section has a top margin of mt-6. However, since the header is fixed, the actual top margin of the main section is mt-26 after calculation. */}
+                <main className={classNames('w-full', marginTop)}>
+                    {children}
+                </main>
             </div>
             <footer className='w-full'>{footer}</footer>
         </div>
