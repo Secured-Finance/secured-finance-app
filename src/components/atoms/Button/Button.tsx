4ef0845c2e5c4c3018a6e28a2f0fbf24c5506f90
Reuven Aboulker <reuven.aboulker@gmail.com>
2023-11-27 09:25:23 +0000

    feat: align buttons and reduce their size [SF-877]

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -14,7 +14,7 @@ export const Button = ({
     React.AnchorHTMLAttributes<HTMLAnchorElement> & {
         fullWidth?: boolean;
         href?: string;
-        size?: 'sm' | 'md';
+        size?: 'xs' | 'sm' | 'md';
         variant?: 'solid' | 'outlined';
     } & {
         StartIcon?: SvgIcon;
@@ -40,6 +40,7 @@ export const Button = ({
                 'enabled:hover:bg-gradient-to-t enabled:hover:from-black-20 enabled:hover:via-black-20 enabled:hover:to-starBlue',
                 'disabled:bg-gradient-to-t disabled:from-black/25 disabled:via-black/25 disabled:to-starBlue disabled:opacity-50',
                 {
+                    'h-8 px-3 py-2': size === 'xs',
                     'h-10 px-4 py-3': size === 'sm',
                     'h-12 px-6 py-4': size === 'md',
                     'w-full': fullWidth,
@@ -57,6 +58,7 @@ export const Button = ({
             )}
             <p
                 className={classNames('whitespace-nowrap text-neutral-8', {
+                    'typography-button-2 text-xs': size === 'xs',
                     'typography-button-2': size === 'sm',
                     'typography-button-1': size === 'md',
                 })}
