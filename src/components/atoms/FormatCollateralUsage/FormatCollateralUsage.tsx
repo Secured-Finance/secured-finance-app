31448609ef65776a3594744327bf0cff178ba50e
Riya <kaushik.riya193@gmail.com>
2023-11-28 11:44:02 +0530

    fix: fix ZC usage [SF-898]

---
+++ src/components/atoms/FormatCollateralUsage/FormatCollateralUsage.tsx
@@ -0,0 +1,29 @@
+import { getLiquidationInformation } from 'src/components/atoms';
+import { formatCollateralRatio } from 'src/utils';
+
+export const FormatCollateralUsage = ({
+    initialValue,
+    finalValue,
+    maxValue = Number.MAX_VALUE,
+}: {
+    initialValue: number;
+    finalValue: number;
+    maxValue?: number;
+}) => {
+    const initialColor = getLiquidationInformation(initialValue / 100).color;
+    const finalColor = getLiquidationInformation(
+        Math.min(finalValue, maxValue) / 100
+    ).color;
+
+    return (
+        <div className='flex flex-row gap-1'>
+            <span className={`${initialColor}`}>
+                {formatCollateralRatio(initialValue)}
+            </span>
+            <span className='text-neutral-8'>&#8594;</span>
+            <span className={`${finalColor}`}>
+                {formatCollateralRatio(finalValue)}
+            </span>
+        </div>
+    );
+};
