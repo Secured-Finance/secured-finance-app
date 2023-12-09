2b4c174715d850422b3f8092ae220496e00f46f4
Arpit Singh <arpit@secured-finance.com>
2023-12-10 01:38:39 +0530

    fix: use next image for the sphere [SF-932]

--- src/components/organisms/ConnectWalletCard/ConnectWalletCard.tsx
+++ src/components/organisms/ConnectWalletCard/ConnectWalletCard.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { useDispatch } from 'react-redux';
 import SFLogo from 'src/assets/img/logo.svg';
 import SFSphere from 'src/assets/img/sf-sphere.png';
@@ -11,12 +12,13 @@ export const ConnectWalletCard = () => {
         <div className='h-fit w-full'>
             <GradientBox>
                 <div className='flex h-full flex-col items-center gap-4 px-6 pb-6 pt-5'>
-                    {/* eslint-disable-next-line @next/next/no-img-element */}
-                    <img
-                        src={SFSphere.src}
-                        className='h-[188px] w-[210px]'
-                        alt='SF Logo'
-                    ></img>
+                    <Image
+                        src={SFSphere}
+                        alt='SF-Logo'
+                        width={210}
+                        height={188}
+                        quality={100}
+                    />
                     <SFLogo className='h-5 w-[208px]'></SFLogo>
                     <div className='typography-caption h-[92px] w-[230px] pb-5 text-center text-[#718096]'>
                         Welcome to the future of DeFi. Secured Finance brings
