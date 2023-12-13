283bb1789575e7c8ffa23046d66f37cf4c786fe7
Arpit Singh <arpit@secured-finance.com>
2023-12-13 11:30:40 +0530

    feat: add use itayose estimation hook [SF-896]

---
+++ src/hooks/useItayoseEstimation/useItayoseEstimation.ts
@@ -0,0 +1,28 @@
+import { useQuery } from '@tanstack/react-query';
+import { QueryKeys } from 'src/hooks/queries';
+import useSF from 'src/hooks/useSecuredFinance';
+import { CurrencySymbol, ZERO_BI, toCurrency } from 'src/utils';
+
+export const emptyOrderEstimation = {
+    openingUnitPrice: ZERO_BI,
+    lastLendUnitPrice: ZERO_BI,
+    lastBorrowUnitPrice: ZERO_BI,
+    totalOffsetAmount: ZERO_BI,
+};
+
+export const useItayoseEstimation = (ccy: CurrencySymbol, maturity: number) => {
+    const securedFinance = useSF();
+
+    return useQuery({
+        queryKey: [QueryKeys.ITAYOSE_ESTIMATION, ccy, maturity],
+        queryFn: async () => {
+            const itayoseEstimation =
+                await securedFinance?.getItayoseEstimation(
+                    toCurrency(ccy),
+                    maturity
+                );
+            return itayoseEstimation ?? emptyOrderEstimation;
+        },
+        enabled: !!securedFinance && !!ccy && !!maturity,
+    });
+};
