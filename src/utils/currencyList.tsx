33241fce52bc5d59591fbd892669afd2ae130b7a
Reuven Aboulker <reuven.aboulker@gmail.com>
2023-12-14 22:21:15 +0000

    feat: change axelar name to axelar FIL [SF-958]

--- src/utils/currencyList.tsx
+++ src/utils/currencyList.tsx
@@ -126,7 +126,7 @@ export const currencyMap: Readonly<
     [CurrencySymbol.axlFIL]: {
         index: 3,
         symbol: CurrencySymbol.axlFIL,
-        name: 'Axelar Wrapped FIL',
+        name: 'Axelar WFIL',
         icon: FilIcon,
         coinGeckoId: 'filecoin',
         isCollateral: false,
