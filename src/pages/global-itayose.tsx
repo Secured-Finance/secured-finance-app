3464cf724c43a2d1f7e34c3ba67c8988d89853f1
Riya <kaushik.riya193@gmail.com>
2024-01-12 14:03:46 +0530

    fix: fix global itayose on mainnet issue [SF-996]

--- src/pages/global-itayose.tsx
+++ src/pages/global-itayose.tsx
@@ -1,6 +1,18 @@
+import Router from 'next/router';
 import { GlobalItayose as GlobalItayoseComponent } from 'src/components/pages';
+import { useIsGlobalItayose } from 'src/hooks';
 
 const GlobalItayose = () => {
+    const { data: isGlobalItayose, isLoading: isLoadingGlobalItayose } =
+        useIsGlobalItayose();
+
+    if (isLoadingGlobalItayose) {
+        return null;
+    }
+
+    if (!isGlobalItayose) {
+        Router.push('/');
+    }
     return <GlobalItayoseComponent />;
 };
 
