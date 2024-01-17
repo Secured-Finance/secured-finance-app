39668cee70c1f7c9907e045dfa8c47da93a2f58a
Arpit Singh <arpit@secured-finance.com>
2024-01-18 01:02:09 +0530

    feat: restrict available networks depending on environment [SF-979]

--- src/hooks/useEtherscanUrl/useEtherscanUrl.ts
+++ src/hooks/useEtherscanUrl/useEtherscanUrl.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import useSF from 'src/hooks/useSecuredFinance';
-import { supportedNetworks } from 'src/utils';
+import { getSupportedNetworks } from 'src/utils';
 
 export const useEtherscanUrl = () => {
     const securedFinance = useSF();
@@ -12,8 +12,9 @@ export const useEtherscanUrl = () => {
         }
         const network = securedFinance.config?.network ?? 'unknown';
         const baseUrl =
-            supportedNetworks.find(n => n.name.toLocaleLowerCase() === network)
-                ?.blockExplorers?.etherscan?.url || 'https://etherscan.io';
+            getSupportedNetworks().find(
+                n => n.name.toLocaleLowerCase() === network
+            )?.blockExplorers?.etherscan?.url || 'https://etherscan.io';
 
         setEtherscanUrl(baseUrl);
     }, [securedFinance, securedFinance?.config?.network]);
