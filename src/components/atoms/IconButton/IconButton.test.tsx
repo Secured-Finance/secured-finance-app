4029e89b85703e6e61929434abcf4b4ddc44b056
Hadi Lee <leeshuzheng@gmail.com>
2024-02-13 15:21:36 +0800

    feat: add tests for button variants [SF-1029]

--- src/components/atoms/IconButton/IconButton.test.tsx
+++ src/components/atoms/IconButton/IconButton.test.tsx
@@ -2,7 +2,8 @@ import { composeStories } from '@storybook/react';
 import { fireEvent, render, screen } from 'src/test-utils.js';
 import * as stories from './IconButton.stories';
 
-const { Primary } = composeStories(stories);
+const { Primary, Secondary, SecondaryBuy, SecondarySell } =
+    composeStories(stories);
 
 describe('IconButton component', () => {
     it('should render a button', () => {
@@ -16,5 +17,20 @@ describe('IconButton component', () => {
 
         fireEvent.click(screen.getByRole('button'));
         expect(onClick).toHaveBeenCalled();
+    });
+
+    it('should render a grey background when variant is secondary', async () => {
+        render(<Secondary />);
+        expect(screen.getByRole('button')).toHaveClass('bg-neutral-800');
+    });
+
+    it('should render a green background when variant is secondary-buy', async () => {
+        render(<SecondaryBuy />);
+        expect(screen.getByRole('button')).toHaveClass('bg-success-700');
+    });
+
+    it('should render a red background when variant is secondary-sell', async () => {
+        render(<SecondarySell />);
+        expect(screen.getByRole('button')).toHaveClass('bg-error-700');
     });
 });
