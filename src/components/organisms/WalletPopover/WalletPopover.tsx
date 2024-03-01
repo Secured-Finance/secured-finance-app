b735625f6363f9931f2cc906d7920e0d1ce35c95
Hadi Lee <leeshuzheng@gmail.com>
2024-03-01 18:36:57 +0800

    feat: add large button for 1440px and above, and tweak background color of buttons [SF-1064]

--- src/components/organisms/WalletPopover/WalletPopover.tsx
+++ src/components/organisms/WalletPopover/WalletPopover.tsx
@@ -100,7 +100,7 @@ export const WalletPopover = ({
                         data-cy='popover-button'
                         aria-label='Wallet Popover Button'
                         className={clsx(
-                            'flex items-center gap-x-1.5 rounded-[6px] bg-transparent px-3 py-2 ring-1 hover:bg-white-10 hover:ring-white-10 focus:outline-none tablet:gap-x-4 tablet:rounded-xl tablet:px-4 tablet:py-3 tablet:ring-[1.5px]',
+                            'flex items-center gap-x-1.5 rounded-[6px] bg-neutral-800 px-3 py-2 ring-1 hover:bg-white-10 hover:ring-white-10 focus:outline-none tablet:gap-x-4 tablet:rounded-xl tablet:px-4 tablet:py-3 tablet:ring-[1.5px]',
                             {
                                 'bg-white-10 ring-white-10': open,
                                 'ring-neutral-500': !open,
