3a850910642b8a2af3f57f1330ad0dc86bb1d7ea
Hadi Lee <leeshuzheng@gmail.com>
2024-03-14 14:58:50 +0800

    feat: add radio buttons for selecting time interval [SF-946]

---
+++ src/components/atoms/RadioButton/RadioButton.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta, StoryFn } from '@storybook/react';
+import { useState } from 'react';
+
+import { RadioButton } from './RadioButton';
+
+export default {
+    title: 'Atoms/RadioButton',
+    component: RadioButton,
+    args: {
+        options: [
+            { label: 'Option 1', value: 'option1' },
+            { label: 'Option 2', value: 'option2' },
+            { label: 'Option 3', value: 'option3' },
+        ],
+        value: 'option1',
+    },
+    argTypes: {},
+} as Meta<typeof RadioButton>;
+
+const StatefulTemplate: StoryFn<typeof RadioButton> = args => {
+    const [value, setValue] = useState(args.value);
+
+    const handleChange = (newValue: string) => {
+        setValue(newValue);
+        args.onChange(newValue);
+    };
+
+    return <RadioButton {...args} value={value} onChange={handleChange} />;
+};
+
+export const Default = StatefulTemplate.bind({});
