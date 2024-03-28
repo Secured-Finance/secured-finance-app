c55506ab205632f7fcb8feaa2e630d91f748e2dd
Arpit Singh <arpit@secured-finance.com>
2024-03-29 00:47:08 +0530

    ci: dummy change to test turbo snap on chromatic [SF-1115]

--- src/components/atoms/InputBase/InputBase.tsx
+++ src/components/atoms/InputBase/InputBase.tsx
@@ -64,7 +64,7 @@ export const InputBase = ({
     return (
         <NumericFormat
             className={clsx(
-                'bg-transparent placeholder-opacity-50 focus:outline-none',
+                'bg-red placeholder-opacity-50 focus:outline-none',
                 className,
                 fontSizeClass
             )}
