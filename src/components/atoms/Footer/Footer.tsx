e8f8c0bdacaece4373e05c26331bfa1833f971a2
Arpit Singh <arpit@secured-finance.com>
2024-04-15 13:35:46 +0530

    fix: revert changes for wallet connectivity status on footer [SF-1131]

--- src/components/atoms/Footer/Footer.tsx
+++ src/components/atoms/Footer/Footer.tsx
@@ -1,10 +1,6 @@
-import clsx from 'clsx';
 import packageJson from 'package.json';
-import { useSelector } from 'react-redux';
-import { RootState } from 'src/store/types';
 import { getCommitHash, getEnvShort, getUsePackageVersion } from 'src/utils';
 import { isChipVisibleForEnv } from 'src/utils/displayUtils';
-import { useAccount } from 'wagmi';
 
 const getVersion = () => {
     if (getUsePackageVersion() && getCommitHash() !== '.storybook') {
@@ -15,23 +11,12 @@ const getVersion = () => {
 };
 
 export const Footer = () => {
-    const chainError = useSelector(
-        (state: RootState) => state.blockchain.chainError
-    );
-    const { isConnected } = useAccount();
-
     return (
         <div
             data-cy='footer'
             className='flex h-9 w-full flex-row items-center gap-3 border-t border-neutral-1 bg-black-20 px-3'
         >
-            <span
-                className={clsx('h-6px w-6px rounded-full', {
-                    'bg-green': isConnected && !chainError,
-                    'bg-red': !isConnected || chainError,
-                })}
-                data-testid='connection-status'
-            ></span>
+            <span className='h-6px w-6px rounded-full bg-green'></span>
             <div className='typography-caption-2 text-planetaryPurple'>
                 <span>{`Secured Finance v${getVersion()} `}</span>
                 {isChipVisibleForEnv() && (
