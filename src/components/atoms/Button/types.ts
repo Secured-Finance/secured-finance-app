3e3507e6c9ea5be07aa6ce0f1cf80730f6196d90
Hadi Lee <leeshuzheng@gmail.com>
2024-04-15 11:49:38 +0800

    fix: remove button variants [SF-1029]

--- src/components/atoms/Button/types.ts
+++ src/components/atoms/Button/types.ts
@@ -2,9 +2,4 @@ export enum ButtonVariants {
     primary = 'primary',
     secondary = 'secondary',
     tertiary = 'tertiary',
-    primaryBuy = 'primary-buy',
-    primarySell = 'primary-sell',
-    secondaryNeutral = 'secondary-neutral',
-    tertiaryBuy = 'tertiary-buy',
-    tertiarySell = 'tertiary-sell',
 }
