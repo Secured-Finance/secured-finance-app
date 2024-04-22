e1928cf8d0dd2476430681aa1058c3d9a87a3e09
Hadi Lee <hadi@secured-finance.com>
2024-04-22 11:08:24 +0800

    fix: change color and add suffix [SF-1058]

--- src/components/atoms/Tab/Tab.stories.tsx
+++ src/components/atoms/Tab/Tab.stories.tsx
@@ -1,3 +1,4 @@
+import { FireIcon } from '@heroicons/react/24/solid';
 import type { Meta, StoryFn } from '@storybook/react';
 import { Tab, TabVariant } from '.';
 
@@ -45,8 +46,28 @@ Neutral.args = {
     variant: TabVariant.Neutral,
 };
 
+export const WithSuffix = Template.bind({});
+WithSuffix.args = {
+    text: 'With Suffix',
+    suffixEle: (
+        <span
+            className='bg-warning-300/35 flex items-center gap-1 rounded-[5px] border border-warning-300 px-1.5 py-0.5 text-2xs leading-[1.3] text-warning-300'
+            data-testid='tab-suffix'
+        >
+            New <FireIcon className='h-2.5 w-2.5' />
+        </span>
+    ),
+};
+
 export const Inactive = Template.bind({});
 Inactive.args = {
     text: 'Inactive',
     active: false,
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+    text: 'Disabled',
+    active: true,
+    disabled: true,
 };
