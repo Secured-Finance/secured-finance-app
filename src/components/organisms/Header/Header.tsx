b8f1032045d7c37eac7d7057a3a0397c173bf82e
Hadi Lee <hadi@secured-finance.com>
2024-04-24 12:54:49 +0800

    feat: import header as client-side component [SF-1144]

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -81,7 +81,7 @@ const HeaderMessage = ({
     return <></>;
 };
 
-export const Header = ({ showNavigation }: { showNavigation: boolean }) => {
+const Header = ({ showNavigation }: { showNavigation: boolean }) => {
     const dispatch = useDispatch();
     const isMobile = useBreakpoint('tablet');
     const { address, isConnected } = useAccount();
@@ -162,7 +162,7 @@ export const Header = ({ showNavigation }: { showNavigation: boolean }) => {
                         </>
                     ) : (
                         <Button
-                            size={!!isMobile ? 'sm' : 'md'}
+                            size={isMobile ? 'sm' : undefined}
                             data-cy='wallet'
                             data-testid='connect-wallet'
                             onClick={() => dispatch(setWalletDialogOpen(true))}
@@ -215,3 +215,5 @@ const ItemLink = ({
         </Link>
     );
 };
+
+export default Header;
