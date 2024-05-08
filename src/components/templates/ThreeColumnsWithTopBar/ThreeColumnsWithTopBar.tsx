fa5765969e78323de767034f03525b511734e246
Hadi Lee <leeshuzheng@gmail.com>
2024-05-08 13:46:34 +0700

    feat: handle min and max width for advanced lending order card [SF-1138]

--- src/components/templates/ThreeColumnsWithTopBar/ThreeColumnsWithTopBar.tsx
+++ src/components/templates/ThreeColumnsWithTopBar/ThreeColumnsWithTopBar.tsx
@@ -8,18 +8,18 @@ export const ThreeColumnsWithTopBar = ({
     return (
         <div
             className='grid h-fit grid-cols-1 place-items-stretch gap-x-3
-            tablet:grid-cols-2 laptop:grid-cols-12 laptop:gap-y-4'
+            tablet:grid-cols-2 laptop:flex laptop:grid-cols-12 laptop:flex-wrap laptop:gap-y-4'
         >
-            <div className='order-1 tablet:col-span-2 laptop:col-span-12'>
+            <div className='order-1 tablet:col-span-2 laptop:w-full'>
                 {topBar}
             </div>
-            <div className='order-2 mb-4 block tablet:col-span-2 laptop:mb-0 laptop:hidden'>
+            <div className='order-2 mb-4 block tablet:col-span-2 laptop:hidden'>
                 {children[0]}
             </div>
-            <div className='order-4 grid grid-cols-12 gap-3 tablet:col-span-2 laptop:col-span-9 laptop:flex laptop:flex-wrap'>
+            <div className='order-4 grid grid-cols-12 gap-3 tablet:col-span-2 laptop:flex laptop:w-1/2 laptop:grow laptop:flex-wrap'>
                 {children[1]}
             </div>
-            <div className='order-3 tablet:col-span-2 laptop:order-4 laptop:col-span-3'>
+            <div className='order-3 tablet:col-span-2 laptop:order-4 laptop:w-[25%] laptop:min-w-[339px] laptop:max-w-[447px]'>
                 {children[2]}
             </div>
         </div>
