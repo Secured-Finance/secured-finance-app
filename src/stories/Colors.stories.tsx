3beb160a43679fb99efdadf5ba0f4d9b3172ccbd
Arpit Singh <arpit@secured-finance.com>
2024-05-16 01:04:54 +0530

    fix: fix colors story [SF-1113]

--- src/stories/Colors.stories.tsx
+++ src/stories/Colors.stories.tsx
@@ -2,75 +2,78 @@ import { Meta, Story } from '@storybook/react';
 
 export default {
     title: 'Design System/Colors',
+    parameters: {
+        layout: 'fullscreen',
+    },
 } as Meta;
 
 export const Colors: Story = () => (
-    <div className='inline-grid grid-cols-1 gap-4'>
-        <div className='flex-col items-start justify-start rounded-3xl bg-white p-10'>
-            <div className='flex flex-col items-start justify-start space-y-6'>
-                <p className='text-lg font-semibold leading-10 text-black-80'>
-                    Primary
-                </p>
+    <div className='inline-grid w-full grid-cols-1 gap-4 bg-[#010316] p-10'>
+        <div className='flex-col items-start justify-start'>
+            <div className='flex flex-col items-start justify-start space-y-4'>
+                <p className='typography-mobile-h-5 text-neutral-50'>Neutral</p>
                 <div className='inline-flex items-center justify-start space-x-6'>
-                    <div className='h-10 w-10 rounded-lg bg-neutral-900' />
-                    <div className='h-10 w-10 rounded-lg bg-starBlue-10' />
-                    <div className='h-10 w-10 rounded-lg bg-teal' />
-                    <div className='h-10 w-10 rounded-lg bg-green' />
-                    <div className='h-10 w-10 rounded-lg bg-orange' />
-                    <div className='h-10 w-10 rounded-lg bg-red' />
+                    <div className='h-10 w-32 rounded-lg bg-neutral-50' />
+                    <div className='h-10 w-32 rounded-lg bg-neutral-100' />
+                    <div className='h-10 w-32 rounded-lg bg-neutral-200' />
+                    <div className='h-10 w-32 rounded-lg bg-neutral-300' />
+                    <div className='h-10 w-32 rounded-lg bg-neutral-400' />
                 </div>
                 <div className='inline-flex items-center justify-start space-x-6'>
-                    <div className='h-10 w-10 rounded-lg bg-purple' />
-                    <div className='h-10 w-10 rounded-lg bg-gunMetal' />
+                    <div className='h-10 w-32 rounded-lg bg-neutral-500' />
+                    <div className='h-10 w-32 rounded-lg bg-neutral-600' />
+                    <div className='h-10 w-32 rounded-lg bg-neutral-700' />
+                    <div className='h-10 w-32 rounded-lg bg-neutral-800' />
+                    <div className='h-10 w-32 rounded-lg bg-neutral-900' />
                 </div>
             </div>
         </div>
-        <div className='inline-flex flex-col items-start justify-start space-y-6 rounded-3xl bg-white p-10'>
-            <p className='text-lg font-semibold leading-10 text-black-80'>
-                Secondary
-            </p>
+        <div className='inline-flex flex-col items-start justify-start space-y-4'>
+            <p className='typography-mobile-h-5 text-neutral-50'>Primary</p>
             <div className='inline-flex items-center justify-start space-x-6'>
-                <div className='h-10 w-10 rounded-lg bg-galacticOrange' />
-                <div className='h-10 w-10 rounded-lg bg-planetaryPurple' />
-                <div className='h-10 w-10 rounded-lg bg-nebulaTeal' />
-                <div className='h-10 w-10 rounded-lg bg-moonGrey' />
-                <div className='h-10 w-10 rounded-lg bg-slateGray' />
+                <div className='h-10 w-32 rounded-lg bg-primary-300' />
+                <div className='h-10 w-32 rounded-lg bg-primary-500' />
+                <div className='h-10 w-32 rounded-lg bg-primary-700' />
             </div>
         </div>
-        <div className='inline-flex flex-col items-start justify-start rounded-3xl bg-white p-10'>
-            <div className='flex flex-col items-start justify-start space-y-6'>
-                <p className='text-lg font-semibold leading-10 text-black-80'>
-                    Shades
-                </p>
-                <div className='inline-flex items-center justify-start space-x-6'>
-                    <div className='h-10 w-10 rounded-lg bg-starBlue-80' />
-                    <div className='h-10 w-10 rounded-lg bg-starBlue-60' />
-                    <div className='h-10 w-10 rounded-lg bg-starBlue-40' />
-                    <div className='h-10 w-10 rounded-lg bg-starBlue-20' />
-                    <div className='h-10 w-10 rounded-lg bg-starBlue-10' />
-                </div>
-                <div className='inline-flex items-center justify-start space-x-6'>
-                    <div className='h-10 w-10 rounded-lg bg-black-90' />
-                    <div className='h-10 w-10 rounded-lg bg-black-80' />
-                    <div className='h-10 w-10 rounded-lg bg-black-70' />
-                    <div className='h-10 w-10 rounded-lg bg-black-60' />
-                    <div className='h-10 w-10 rounded-lg bg-black-50' />
-                    <div className='h-10 w-10 rounded-lg bg-black-40' />
-                    <div className='h-10 w-10 rounded-lg bg-black-30' />
-                    <div className='h-10 w-10 rounded-lg bg-black-20' />
-                    <div className='h-10 w-10 rounded-lg bg-black-10' />
-                </div>
-                <div className='inline-flex items-center justify-start space-x-6'>
-                    <div className='h-10 w-10 rounded-lg border-2 bg-white-90' />
-                    <div className='h-10 w-10 rounded-lg border-2 bg-white-80' />
-                    <div className='h-10 w-10 rounded-lg border-2 bg-white-70' />
-                    <div className='h-10 w-10 rounded-lg border-2 bg-white-60' />
-                    <div className='h-10 w-10 rounded-lg border-2 bg-white-50' />
-                    <div className='h-10 w-10 rounded-lg border-2 bg-white-40' />
-                    <div className='h-10 w-10 rounded-lg border-2 bg-white-30' />
-                    <div className='h-10 w-10 rounded-lg border-2 bg-white-20' />
-                    <div className='h-10 w-10 rounded-lg border-2 bg-white-10' />
-                </div>
+        <div className='inline-flex flex-col items-start justify-start space-y-4'>
+            <p className='typography-mobile-h-5 text-neutral-50'>Secondary</p>
+            <div className='inline-flex items-center justify-start space-x-6'>
+                <div className='h-10 w-32 rounded-lg bg-secondary-300' />
+                <div className='h-10 w-32 rounded-lg bg-secondary-500' />
+                <div className='h-10 w-32 rounded-lg bg-secondary-700' />
+            </div>
+        </div>
+        <div className='inline-flex flex-col items-start justify-start space-y-4'>
+            <p className='typography-mobile-h-5 text-neutral-50'>Tertiary</p>
+            <div className='inline-flex items-center justify-start space-x-6'>
+                <div className='h-10 w-32 rounded-lg bg-tertiary-300' />
+                <div className='h-10 w-32 rounded-lg bg-tertiary-500' />
+                <div className='h-10 w-32 rounded-lg bg-tertiary-700' />
+            </div>
+        </div>
+        <div className='inline-flex flex-col items-start justify-start space-y-4'>
+            <p className='typography-mobile-h-5 text-neutral-50'>Success</p>
+            <div className='inline-flex items-center justify-start space-x-6'>
+                <div className='h-10 w-32 rounded-lg bg-success-300' />
+                <div className='h-10 w-32 rounded-lg bg-success-500' />
+                <div className='h-10 w-32 rounded-lg bg-success-700' />
+            </div>
+        </div>
+        <div className='inline-flex flex-col items-start justify-start space-y-4'>
+            <p className='typography-mobile-h-5 text-neutral-50'>Warning</p>
+            <div className='inline-flex items-center justify-start space-x-6'>
+                <div className='h-10 w-32 rounded-lg bg-warning-300' />
+                <div className='h-10 w-32 rounded-lg bg-warning-500' />
+                <div className='h-10 w-32 rounded-lg bg-warning-700' />
+            </div>
+        </div>
+        <div className='inline-flex flex-col items-start justify-start space-y-4'>
+            <p className='typography-mobile-h-5 text-neutral-50'>Error</p>
+            <div className='inline-flex items-center justify-start space-x-6'>
+                <div className='h-10 w-32 rounded-lg bg-error-300' />
+                <div className='h-10 w-32 rounded-lg bg-error-500' />
+                <div className='h-10 w-32 rounded-lg bg-error-700' />
             </div>
         </div>
     </div>
