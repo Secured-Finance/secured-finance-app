561864dfe36ba80eae1a638c63b36edbf1931261
Arpit Singh <arpit@secured-finance.com>
2024-05-16 00:37:11 +0530

    feature: add latest typography [SF-1113]

--- src/stories/Typography.stories.tsx
+++ src/stories/Typography.stories.tsx
@@ -1,84 +1,72 @@
-import { RESPONSIVE_PARAMETERS } from '.storybook/constants';
 import { Meta, Story } from '@storybook/react';
 
 export default {
     title: 'Design System/Typography',
     parameters: {
-        ...RESPONSIVE_PARAMETERS,
+        layout: 'fullscreen',
     },
 } as Meta;
 
 export const Typography: Story = () => (
     <div className='grid grid-cols-2 place-items-stretch gap-y-4 text-white'>
-        <div className='border-b'>Class Name:</div>
-        <div className='border-b'>Usage</div>
+        <div className='border-b'>Desktop</div>
+        <div className='border-b'>Mobile</div>
 
-        <div>Hero</div>
-        <div className='typography-hero'>Hero</div>
+        <div className='typography-desktop-display-xxl'>Secured - XXL</div>
+        <div className='typography-mobile-display-xxl'>Secured - XXL</div>
 
-        <div>Headline 1</div>
-        <div className='typography-headline-1'>Headline 1</div>
+        <div className='typography-desktop-display-xl'>Secured - XL</div>
+        <div className='typography-mobile-display-xl'>Secured - XL</div>
 
-        <div>Headline 2</div>
-        <div className='typography-headline-2'>Headline 2</div>
+        <div className='typography-desktop-h-1'>Secured Finance - H1</div>
+        <div className='typography-mobile-h-1'>Secured Finance - H1</div>
 
-        <div>Headline 3</div>
-        <div className='typography-headline-3'>Headline 3</div>
+        <div className='typography-desktop-h-2'>Secured Finance - H2</div>
+        <div className='typography-mobile-h-2'>Secured Finance - H2</div>
 
-        <div>Big Body Bold</div>
-        <div className='typography-big-body-bold'>Big Body Bold</div>
+        <div className='typography-desktop-h-3'>Secured Finance - H3</div>
+        <div className='typography-mobile-h-3'>Secured Finance - H3</div>
 
-        <div>Modal Title</div>
-        <div className='typography-modal-title'>Modal Title</div>
+        <div className='typography-desktop-h-4'>Secured Finance - H4</div>
+        <div className='typography-mobile-h-4'>Secured Finance - H4</div>
 
-        <div>Body 1</div>
-        <div className='typography-body-1'>
-            Body text large for big paragraphs
-        </div>
+        <div className='typography-desktop-h-5'>Secured Finance - H5</div>
+        <div className='typography-mobile-h-5'>Secured Finance - H5</div>
 
-        <div>Body 2</div>
-        <div className='typography-body-2'>
-            Body text normal, used for most readable content
-        </div>
+        <div className='typography-desktop-h-6'>Secured Finance - H6</div>
+        <div className='typography-mobile-h-6'>Secured Finance - H6</div>
 
-        <div>Caption</div>
-        <div className='typography-caption'>
-            Caption text, used for very small paragraphs.
-        </div>
+        <div className='typography-desktop-sh-7'>Secured Finance - SH7</div>
+        <div className='typography-mobile-sh-7'>Secured Finance - SH7</div>
 
-        <div>Caption 2</div>
-        <div className='typography-caption-2'>
-            Caption 2 text regular, could be used for secondary links or
-            navigation elements.
-        </div>
+        <div className='typography-desktop-sh-8'>Secured Finance - SH8</div>
+        <div className='typography-mobile-sh-8'>Secured Finance - SH8</div>
 
-        <div>Caption 3</div>
-        <div className='typography-caption-3'>Caption 3</div>
+        <div className='typography-desktop-sh-9'>Secured Finance - SH9</div>
+        <div className='typography-mobile-sh-9'>Secured Finance - SH9</div>
 
-        <div>Hairline1</div>
-        <div className='typography-hairline-1'>Hairline Large</div>
+        <div className='typography-desktop-body-1'>Body 1</div>
+        <div className='typography-mobile-body-1'>Body 1</div>
 
-        <div>Hairline2</div>
-        <div className='typography-hairline-2'>Hairline Small</div>
+        <div className='typography-desktop-body-2'>Body 2</div>
+        <div className='typography-mobile-body-2'>Body 2</div>
 
-        <div>Button 1</div>
-        <div className='typography-button-1'>Nav Menu Default</div>
+        <div className='typography-desktop-body-3'>Body 3</div>
+        <div className='typography-mobile-body-3'>Body 3</div>
 
-        <div>Button 2</div>
-        <div className='typography-button-2'>Button Small</div>
+        <div className='typography-desktop-body-4'>Body 4</div>
+        <div className='typography-mobile-body-4'>Body 4</div>
 
-        <div>Button 3</div>
-        <div className='typography-button-3'>Tooltip Large</div>
+        <div className='typography-desktop-body-5'>Body 5</div>
+        <div className='typography-mobile-body-5'>Body 5</div>
 
-        <div>Nav Menu Default</div>
-        <div className='typography-nav-menu-default'>Navigation Menu</div>
+        <div className='typography-desktop-body-6'>Body 6</div>
+        <div className='typography-mobile-body-6'>Body 6</div>
 
-        <div>Pill Label</div>
-        <div className='typography-pill-label'>Pill Label</div>
+        <div className='typography-desktop-cta-1'>CTA BODY 1</div>
+        <div className='typography-mobile-body-1'>CTA BODY 2</div>
 
-        <div>Dropdown Selection Label</div>
-        <div className='typography-dropdown-selection-label'>
-            Dropdown Selection Label
-        </div>
+        <div className='typography-desktop-cta-2'>CTA BODY 2</div>
+        <div className='typography-mobile-body-2'>CTA BODY 2</div>
     </div>
 );
