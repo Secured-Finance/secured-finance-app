c5ac6c011ae28ab17de4e8d245296bd7c83edd9f
Hadi Lee <leeshuzheng@gmail.com>
2024-06-02 23:32:45 +0700

    fix: remove portalContainer [SF-1098]

--- src/components/molecules/Tooltip/Tooltip.tsx
+++ src/components/molecules/Tooltip/Tooltip.tsx
@@ -1,6 +1,6 @@
 import { Tooltip as NextTooltip, TooltipPlacement } from '@nextui-org/tooltip';
 import clsx from 'clsx';
-import { cloneElement, useEffect, useRef, useState } from 'react';
+import { cloneElement } from 'react';
 import { Alignment } from 'src/types';
 import { modeStyles, tooltipOptions } from './constants';
 import { TooltipMode } from './types';
@@ -20,54 +20,41 @@ export const Tooltip = ({
     placement?: TooltipPlacement;
     disabled?: boolean;
 }) => {
-    const tooltipRef = useRef<HTMLDivElement | null>(null);
-
-    const [portalContainer, setPortalContainer] = useState<Element | undefined>(
-        undefined
-    );
-
-    useEffect(() => {
-        setPortalContainer(tooltipRef.current || undefined);
-    }, []);
-
     return (
-        <div ref={tooltipRef} className='flex'>
-            <NextTooltip
-                isDisabled={disabled}
-                showArrow={true}
-                offset={0}
-                placement={placement}
-                portalContainer={portalContainer}
-                content={
-                    <div
-                        className={clsx(
-                            'typography-desktop-body-6 laptop:typography-desktop-body-5 relative flex w-fit max-w-[240px] gap-2.5 overflow-hidden whitespace-normal rounded-lg border px-2 py-1 text-left text-neutral-50 shadow-dropdown laptop:px-3',
-                            modeStyles[mode],
-                            {
-                                'left-24': align === 'right',
-                            }
-                        )}
-                    >
-                        {children}
-                    </div>
-                }
-                role='tooltip'
-                {...tooltipOptions}
-            >
+        <NextTooltip
+            isDisabled={disabled}
+            showArrow={true}
+            offset={0}
+            placement={placement}
+            content={
                 <div
                     className={clsx(
-                        'pointer-events-auto flex items-start focus:outline-none',
+                        'typography-desktop-body-6 laptop:typography-desktop-body-5 relative flex w-fit max-w-[240px] gap-2.5 overflow-hidden whitespace-normal rounded-lg border px-2 py-1 text-left text-neutral-50 shadow-dropdown laptop:px-3',
+                        modeStyles[mode],
                         {
-                            'cursor-pointer': !disabled,
+                            'left-24': align === 'right',
                         }
                     )}
                 >
-                    {cloneElement(
-                        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                        iconElement as React.ReactElement<any>
-                    )}
+                    {children}
                 </div>
-            </NextTooltip>
-        </div>
+            }
+            role='tooltip'
+            {...tooltipOptions}
+        >
+            <div
+                className={clsx(
+                    'pointer-events-auto flex items-start focus:outline-none',
+                    {
+                        'cursor-pointer': !disabled,
+                    }
+                )}
+            >
+                {cloneElement(
+                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                    iconElement as React.ReactElement<any>
+                )}
+            </div>
+        </NextTooltip>
     );
 };
