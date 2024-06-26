c5722d16572e5593ae1ef297dd78381e99f3ca70
Hadi Lee <leeshuzheng@gmail.com>
2024-06-26 17:12:15 +0800

    fix: fix tests [SF-1135]

--- src/components/molecules/AdvancedLendingTopBar/AdvancedLendingTopBar.test.tsx
+++ src/components/molecules/AdvancedLendingTopBar/AdvancedLendingTopBar.test.tsx
@@ -1,25 +1,28 @@
 import { composeStories } from '@storybook/react';
 import { render, screen } from 'src/test-utils.js';
+import { formatTimeStampWithTimezone } from 'src/utils';
+import { LoanValue } from 'src/utils/entities';
 import * as stories from './AdvancedLendingTopBar.stories';
 
 const { Default, Values } = composeStories(stories);
 
 describe('AdvancedLendingTopBar Component', () => {
     it('should render a AdvancedLendingTopBar without the values', () => {
-        render(<Default />, {
-            apolloMocks: Default.parameters?.apolloClient.mocks,
-        });
+        render(<Default />);
 
         expect(
             screen.getByRole('button', { name: 'WFIL-DEC2022' })
         ).toBeInTheDocument();
         expect(screen.getByText('Maturity Dec 1, 2022')).toBeInTheDocument();
+        expect(screen.getByText('80.00')).toBeInTheDocument();
+        expect(screen.getByText('25.03% APR')).toBeInTheDocument();
+        expect(
+            screen.getByText(formatTimeStampWithTimezone(1646920200))
+        ).toBeInTheDocument();
     });
 
     it('should render a AdvancedLendingTopBar with the values', () => {
-        render(<Values />, {
-            apolloMocks: Default.parameters?.apolloClient.mocks,
-        });
+        render(<Values />);
 
         expect(screen.getByText('24h High')).toBeInTheDocument();
         expect(screen.getByText('24h Low')).toBeInTheDocument();
@@ -34,9 +37,7 @@ describe('AdvancedLendingTopBar Component', () => {
     });
 
     it('should render source link for the selected asset', () => {
-        render(<Default />, {
-            apolloMocks: Default.parameters?.apolloClient.mocks,
-        });
+        render(<Default />);
 
         expect(
             screen.getByRole('button', { name: 'WFIL-DEC2022' })
@@ -49,30 +50,46 @@ describe('AdvancedLendingTopBar Component', () => {
         );
     });
 
-    // describe('Current Market', () => {
-    //     it('should show that the current market is an opening price if indicated', () => {
-    //         render(<Default />);
-    //         expect(screen.getByText('Opening Price')).toBeInTheDocument();
-    //         expect(
-    //             screen.queryByText(`${formatTimeStampWithTimezone(1643713200)}`)
-    //         ).not.toBeInTheDocument();
-    //     });
+    describe('Current Market', () => {
+        it('should show that the current market is an opening price if indicated', () => {
+            render(
+                <Default
+                    currentMarket={{
+                        value: LoanValue.fromPrice(8000, 1643713200),
+                        time: 1643713200,
+                        type: 'opening',
+                    }}
+                />
+            );
+            expect(screen.getByText('Opening Price')).toBeInTheDocument();
+            expect(
+                screen.queryByText(`${formatTimeStampWithTimezone(1643713200)}`)
+            ).not.toBeInTheDocument();
+        });
 
-    //     it('should show that the current market is a last block price if indicated', () => {
-    //         render(<Default />);
-    //         expect(
-    //             screen.getByText(`${formatTimeStampWithTimezone(1643713200)}`)
-    //         ).toBeInTheDocument();
-    //     });
+        it('should show that the current market is a last block price if indicated', () => {
+            render(
+                <Default
+                    currentMarket={{
+                        value: LoanValue.fromPrice(8000, 1643713200),
+                        time: 1643713200,
+                        type: 'block',
+                    }}
+                />
+            );
+            expect(
+                screen.getByText(`${formatTimeStampWithTimezone(1643713200)}`)
+            ).toBeInTheDocument();
+        });
 
-    //     it('should show the current market box in gray if there is no current market', () => {
-    //         render(<Default />);
-    //         expect(
-    //             screen.queryByLabelText('Current Market')
-    //         ).toBeInTheDocument();
-    //         expect(
-    //             screen.queryByLabelText('Current Market')?.children[0]
-    //         ).toHaveClass('text-slateGray');
-    //     });
-    // });
+        it('should show the current market box in gray if there is no current market', () => {
+            render(<Default currentMarket={undefined} />);
+            expect(
+                screen.queryByLabelText('Current Market')
+            ).toBeInTheDocument();
+            expect(
+                screen.queryByLabelText('Current Market')?.children[0]
+            ).toHaveClass('text-slateGray');
+        });
+    });
 });
