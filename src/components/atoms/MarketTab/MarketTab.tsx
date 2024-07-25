c95311bc17102320433fe912ebb88d50f97e9fec
Hadi Lee <leeshuzheng@gmail.com>
2024-07-25 13:13:11 +0800

    fix: handle countdown width [SF-1136]

--- src/components/atoms/MarketTab/MarketTab.tsx
+++ src/components/atoms/MarketTab/MarketTab.tsx
@@ -6,49 +6,19 @@ export interface MarketTabProps {
     name: string;
     value?: number | React.ReactNode;
     source?: string;
-    variant?: 'default' | 'green-name' | 'gray-name';
     label?: string;
 }
 
-export const MarketTab = ({
-    name,
-    value,
-    source,
-    variant = 'default',
-    label,
-}: MarketTabProps) => {
+export const MarketTab = ({ name, value, source, label }: MarketTabProps) => {
     return (
         <section
-            className={clsx('flex h-fit flex-grow flex-col', {
-                'gap-[1.5px]': variant === 'green-name',
-            })}
+            className={clsx('flex h-fit flex-grow flex-col')}
             aria-label={label ?? name}
         >
-            <span
-                className={clsx({
-                    'whitespace-nowrap text-base font-semibold leading-[1.57] text-nebulaTeal laptop:text-smd':
-                        variant === 'green-name',
-                    'typography-button-3 whitespace-nowrap leading-8 text-slateGray':
-                        variant === 'gray-name',
-                    'laptop:typography-caption-2 whitespace-nowrap text-[11px] text-neutral-400':
-                        variant === 'default',
-                })}
-            >
+            <span className='laptop:typography-caption-2 whitespace-nowrap text-[11px] text-neutral-400'>
                 {name}
             </span>
-            <span
-                className={clsx(
-                    {
-                        'laptop:typography-caption whitespace-nowrap text-[11px] text-white':
-                            variant === 'green-name',
-                        'typography-caption whitespace-nowrap text-slateGray':
-                            variant === 'gray-name',
-                        'typography-caption leading-4 text-neutral-50 desktop:leading-6':
-                            variant === 'default',
-                    },
-                    'flex items-center'
-                )}
-            >
+            <span className='typography-caption flex items-center leading-4 text-neutral-50 desktop:leading-6'>
                 {typeof value === 'number'
                     ? ordinaryFormat(value, 0, 4)
                     : value}
