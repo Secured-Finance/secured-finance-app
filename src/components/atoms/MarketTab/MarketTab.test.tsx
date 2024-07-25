c418291d59e9ab4ccb4138f33b6bef8819d64b19
Hadi Lee <leeshuzheng@gmail.com>
2024-07-25 13:13:11 +0800

    fix: handle countdown width [SF-1136]

--- src/components/atoms/MarketTab/MarketTab.test.tsx
+++ src/components/atoms/MarketTab/MarketTab.test.tsx
@@ -2,7 +2,7 @@ import { composeStories } from '@storybook/react';
 import { render, screen } from 'src/test-utils.js';
 import * as stories from './MarketTab.stories';
 
-const { Default, GreenMarketTab, GrayMarketTab } = composeStories(stories);
+const { Default } = composeStories(stories);
 
 describe('test Market Tab component', () => {
     it('should render Market Tab', () => {
@@ -15,32 +15,6 @@ describe('test Market Tab component', () => {
         expect(screen.getByText('10,000')).toBeInTheDocument();
         expect(screen.getByText('10,000')).toHaveClass(
             'typography-caption leading-4 text-neutral-50 desktop:leading-6 flex items-center'
-        );
-    });
-
-    it('should render Green Market Tab', () => {
-        render(<GreenMarketTab />);
-        expect(screen.getByLabelText('Green Market Tab')).toBeInTheDocument();
-        expect(screen.getByText('7977.00')).toBeInTheDocument();
-        expect(screen.getByText('7977.00')).toHaveClass(
-            'whitespace-nowrap text-base font-semibold leading-[1.57] text-nebulaTeal'
-        );
-        expect(screen.getByText('25.00% APR')).toBeInTheDocument();
-        expect(screen.getByText('25.00% APR')).toHaveClass(
-            'whitespace-nowrap text-[11px] text-white flex items-center'
-        );
-    });
-
-    it('should render Gray Market Tab', () => {
-        render(<GrayMarketTab />);
-        expect(screen.getByLabelText('Gray Market Tab')).toBeInTheDocument();
-        expect(screen.getByText('7977.00')).toBeInTheDocument();
-        expect(screen.getByText('7977.00')).toHaveClass(
-            'typography-button-3 whitespace-nowrap leading-8 text-slateGray'
-        );
-        expect(screen.getByText('25.00% APR')).toBeInTheDocument();
-        expect(screen.getByText('25.00% APR')).toHaveClass(
-            'typography-caption text-slateGray'
         );
     });
 
--- src/components/molecules/AdvancedLendingTopBar/CountdownTimer.tsx
+++ src/components/molecules/AdvancedLendingTopBar/CountdownTimer.tsx
@@ -31,7 +31,11 @@ export const CountdownTimer = ({ maturity }: { maturity: number }) => {
     return (
         <MarketTab
             name='Countdown'
-            value={formatRemainingTime(remainingTime)}
+            value={
+                <span className='tabular-nums'>
+                    {formatRemainingTime(remainingTime)}
+                </span>
+            }
         />
     );
 };
