b7ba57cb7eacf1f17dbef8f3cce830210028c72d
Arpit Singh <arpit@secured-finance.com>
2024-08-14 04:21:57 +0530

    feat: add more sizes [SF-1059]

--- src/components/atoms/Checkbox/constants.ts
+++ src/components/atoms/Checkbox/constants.ts
@@ -4,7 +4,7 @@ export const FIGMA_STORYBOOK_LINK =
     'https://www.figma.com/design/YIBsk1ihFbPlDb8XldGj1T/SF-Design-System?node-id=1922-5591&m=dev';
 
 export const sizeStyle: { [key in CheckboxSizes]: string } = {
-    [CheckboxSizes.sm]: 'px-1 text-[0.5rem] leading-[0.625rem] rounded',
+    [CheckboxSizes.sm]: 'h-3 w-3 rounded-[3px] border',
     [CheckboxSizes.md]: 'h-3.5 w-3.5 rounded-[3.5px] border-[1.2px]',
-    [CheckboxSizes.lg]: 'px-2 text-xs leading-4 rounded-md',
+    [CheckboxSizes.lg]: 'h-4 w-4 rounded border-[1.5px]',
 };
