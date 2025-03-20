ddec2d82370e822a2f7c8519a8fb4093235ac4bd
vijayeth <vijay@secured-finance.com>
2025-03-20 12:13:55 +0530

    feat: orderbook info tooltip [SF-1163]

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -27,6 +27,7 @@ export * from './InputBase';
 export * from './MarketTab';
 export * from './MenuItem';
 export * from './OrderBookIcon';
+export * from './OrderBookInfo';
 export * from './OrderDisplayBox';
 export * from './OrderInputBox';
 export * from './PercentageTab';
