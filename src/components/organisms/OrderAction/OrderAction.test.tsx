3f99169e8d6cf00d20567a44d541c51873062781
Arpit Singh <arpit@secured-finance.com>
2025-04-29 02:50:34 +0530

    fix: rename modal and button titles [SF-1377]

--- src/components/organisms/OrderAction/OrderAction.test.tsx
+++ src/components/organisms/OrderAction/OrderAction.test.tsx
@@ -56,14 +56,14 @@ describe('OrderAction component', () => {
         expect(
             await screen.findByTestId('deposit-collateral-button')
         ).toBeInTheDocument();
-        expect(screen.getByText('Deposit collateral')).toBeInTheDocument();
+        expect(screen.getByText('Deposit')).toBeInTheDocument();
         const button = screen.getByTestId('deposit-collateral-button');
         fireEvent.click(button);
         expect(track).toHaveBeenCalledWith(
             ButtonEvents.DEPOSIT_COLLATERAL_BUTTON
         );
         expect(
-            screen.getByRole('dialog', { name: 'Deposit Collateral' })
+            screen.getByRole('dialog', { name: 'Deposit' })
         ).toBeInTheDocument();
     });
 
@@ -85,7 +85,7 @@ describe('OrderAction component', () => {
         expect(
             await screen.findByTestId('deposit-collateral-button')
         ).toBeInTheDocument();
-        expect(screen.getByText('Deposit collateral')).toBeInTheDocument();
+        expect(screen.getByText('Deposit')).toBeInTheDocument();
         expect(screen.getByTestId('deposit-collateral-button')).toBeDisabled();
     });
 
