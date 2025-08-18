f2d9c2d07eb156df47a4a0b842b498b4b96bed5b
vijayeth <vijay@secured-finance.com>
2025-08-18 16:16:06 +0530

    fix: feeCalculator implementation [SF-026-refactor]

--- src/utils/fee.ts
+++ src/utils/fee.ts
@@ -3,8 +3,17 @@ import { percentFormat } from 'src/utils';
 
 const SECONDS_IN_YEAR = 365 * 24 * 60 * 60;
 
-export const calculateFee = (maturity: number, annualFee: number) => {
-    const diff = dayjs.unix(maturity).diff(Date.now(), 'second');
-    const fee = Math.max((diff * annualFee) / SECONDS_IN_YEAR, 0);
-    return percentFormat(fee);
-};
+export class FeeCalculator {
+    static calculateTransactionFees(
+        maturity: number,
+        annualFee: number
+    ): string {
+        const diff = dayjs.unix(maturity).diff(Date.now(), 'second');
+        const fee = Math.max((diff * annualFee) / SECONDS_IN_YEAR, 0);
+        return percentFormat(fee);
+    }
+
+    static calculateProtocolFee(feeRate: number): number {
+        return feeRate / 100;
+    }
+}
