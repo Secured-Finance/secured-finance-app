544fb5d550c2e454d6e63473ccc8cf04ee44ed7b
vijayeth <vijay@secured-finance.com>
2025-09-03 20:24:41 +0530

    fix: update Hex [SF-034-refactor]

--- src/utils/hexConverter.ts
+++ src/utils/hexConverter.ts
@@ -1,28 +1,30 @@
 import {
+    Hex,
     hexToBigInt,
     hexToNumber,
     hexToString,
     numberToHex,
     toHex,
 } from 'viem';
+
 export class HexConverter {
     static hexToNumber(hex: string): number {
-        return hexToNumber(hex as `0x${string}`);
+        return hexToNumber(hex as Hex);
     }
 
-    static numberToHex(num: number): `0x${string}` {
+    static numberToHex(num: number): Hex {
         return numberToHex(num);
     }
 
     static hexToBigInt(hex: string): bigint {
-        return hexToBigInt(hex as `0x${string}`);
+        return hexToBigInt(hex as Hex);
     }
 
-    static toHex(value: string | number): `0x${string}` {
+    static toHex(value: string | number): Hex {
         return toHex(value);
     }
 
     static hexToString(hex: string, size = 32): string {
-        return hexToString(hex as `0x${string}`, { size });
+        return hexToString(hex as Hex, { size });
     }
 }
