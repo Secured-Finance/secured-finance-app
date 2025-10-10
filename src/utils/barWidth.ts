fcddf626ac3ae17efab6ac212ba8f67a3ae51af1
vijayeth <vijay@secured-finance.com>
2025-10-10 17:55:43 +0530

    chore: colorBar fix [SF-035-refactor]

--- src/utils/barWidth.ts
+++ src/utils/barWidth.ts
@@ -1,10 +1,9 @@
+import { CollateralCalculator } from './collateral';
 import { divide, multiply } from './currencyList';
 
 type InputValue = bigint | number | string;
 
 const PERCENTAGE_BASE = 100;
-const ZERO = 0;
-const ZERO_BI = BigInt(0);
 
 export const calculateBarWidth = (
     value: InputValue,
@@ -12,11 +11,9 @@ export const calculateBarWidth = (
     maxWidth: number,
     minWidth: number
 ): number => {
-    const vBig = BigInt(value || ZERO);
-    const tBig = BigInt(total || ZERO);
-    const percentage =
-        tBig === ZERO_BI ? ZERO : multiply(divide(vBig, tBig), PERCENTAGE_BASE);
-    const ratio = divide(percentage, PERCENTAGE_BASE);
-    const targetWidth = multiply(ratio, maxWidth);
-    return Math.min(Math.max(targetWidth, minWidth), maxWidth);
+    const percentage = CollateralCalculator.calculatePercentage(value, total);
+    const ratio = divide(Number(percentage), PERCENTAGE_BASE);
+    const calculatedWidth = multiply(ratio, maxWidth);
+
+    return Math.min(Math.max(calculatedWidth, minWidth), maxWidth);
 };
