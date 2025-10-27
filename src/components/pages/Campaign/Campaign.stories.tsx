ef0ee1d6b6387029cdb7e4948cf8eb6d5b40a9ce
vijayeth <vijay@secured-finance.com>
2025-10-27 13:38:52 +0530

    fix: storybook error [SF-065-refactor]

--- src/components/pages/Campaign/Campaign.stories.tsx
+++ src/components/pages/Campaign/Campaign.stories.tsx
@@ -1,5 +1,4 @@
 import { RESPONSIVE_PARAMETERS, VIEWPORTS } from '.storybook/constants';
-import { GetUserDocument } from 'src/generated/points';
 import type { Meta, StoryFn } from '@storybook/react';
 import {
     withAppLayout,
@@ -8,39 +7,12 @@ import {
 } from 'src/../.storybook/decorators';
 import { Campaign } from './Campaign';
 
-const user = [
-    {
-        request: {
-            query: GetUserDocument,
-        },
-        result: {
-            data: {
-                user: {
-                    id: '1',
-                    walletAddress: '0x123',
-                    point: 150,
-                    rank: 1,
-                    joindAt: '2024-05-30T13:39:49.165Z',
-                    referralCode: 'ABCDEFG123',
-                    pointDetails: {
-                        deposit: 100,
-                        referral: 50,
-                    },
-                },
-            },
-        },
-    },
-];
-
 export default {
     title: 'Pages/Campaign',
     component: Campaign,
     chromatic: { pauseAnimationAtEnd: true, viewports: [1024, 1440] },
     args: {},
     parameters: {
-        apolloClient: {
-            mocks: [...user],
-        },
         ...RESPONSIVE_PARAMETERS,
         chromatic: {
             delay: 3000,
