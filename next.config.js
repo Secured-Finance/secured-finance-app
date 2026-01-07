0ace5b9e79dd41189895b7018d39552dfb3da395
Arpit Singh <arpit@secured-finance.com>
2026-01-08 01:38:05 +0530

    ci: use a less restrictive CSP [SF-1481]

--- next.config.js
+++ next.config.js
@@ -45,12 +45,13 @@ module.exports = withBundleAnalyzer({
                     {
                         key: 'Content-Security-Policy',
                         value: [
-                            "default-src 'self'",
+                            "default-src 'self' https://*.storacha.link https://*.w3s.link https://*.nftstorage.link https://*.dweb.link https://ipfs.io/ipfs/ https://cloudflare-eth.com",
                             "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://cdn.amplitude.com https://api.amplitude.com",
                             "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+                            "style-src-elem 'self' 'unsafe-inline' https://fonts.googleapis.com",
                             "font-src 'self' https://fonts.gstatic.com",
                             "img-src 'self' data: https: blob:",
-                            "connect-src 'self' https://*.secured.finance https://api.amplitude.com https://*.amplitude.com https://www.google-analytics.com https://rpc.ankr.com https://*.alchemy.com https://*.alchemyapi.io https://*.walletconnect.org https://*.walletconnect.com wss: https:",
+                            "connect-src 'self' https://*.secured.finance https://api.amplitude.com https://*.amplitude.com https://www.google-analytics.com https://rpc.ankr.com https://*.alchemy.com https://*.alchemyapi.io https://*.walletconnect.org https://*.walletconnect.com https://api.web3modal.com https://pulse.walletconnect.com https://cloudflare-eth.com wss: https:",
                             "frame-src 'self' https://studio.squidrouter.com https://verify.walletconnect.org https://verify.walletconnect.com",
                             "object-src 'none'",
                             "base-uri 'self'",
