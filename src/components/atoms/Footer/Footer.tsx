6031e2cf3786a51dcaf3bac233f178245306f53b
Arpit Singh <arpit@secured-finance.com>
2026-01-14 20:43:07 +0530

    ci: check new build [SF-1490]

--- src/components/atoms/Footer/Footer.tsx
+++ src/components/atoms/Footer/Footer.tsx
@@ -8,7 +8,7 @@ const STATUS_PAGE_LINK = 'https://secured-finance.statuspage.io/';
 
 const getVersion = () => {
     if (getUsePackageVersion() && getCommitHash() !== '.storybook') {
-        return '2-13/01/2026';
+        return '1-14/01/2026';
     }
 
     return getCommitHash();
